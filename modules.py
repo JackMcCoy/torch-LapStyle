90ee47cfa5295174f94934e440434b5738e9b9f2
JackMcCoy <fdrdavegill@gmail.com>
2021-09-26 19:24:04 -0400

    fix Conv2d invocation

--- modules.py
+++ modules.py
@@ -5,10 +5,10 @@ class ResBlock(nn.Module):
     def __init__(self, dim):
         super(ResBlock, self).__init__()
         self.conv_block = nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
-                                        nn.Conv2d(dim, dim, 3),
+                                        nn.Conv2d(dim, dim, kernel_size=3),
                                         nn.ReLU(),
                                         nn.ReflectionPad2d((1, 1, 1, 1)),
-                                        nn.Conv2d(dim, dim, 3))
+                                        nn.Conv2d(dim, dim, kernel_size=3))
 
     def forward(self, x):
         out = x + self.conv_block(x)
@@ -19,7 +19,7 @@ class ConvBlock(nn.Module):
     def __init__(self, dim1, dim2,noise=0):
         super(ConvBlock, self).__init__()
         self.conv_block = nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
-                                        nn.Conv2d(dim1, dim2, 3),
+                                        nn.Conv2d(dim1, dim2, kernel_size=3),
                                         nn.ReLU())
 
     def forward(self, x):
