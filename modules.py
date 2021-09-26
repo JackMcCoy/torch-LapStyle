9d342e953454cf1c6aa83678c33a6010bb602057
JackMcCoy <fdrdavegill@gmail.com>
2021-09-26 19:10:40 -0400

    fix inint

--- modules.py
+++ modules.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class ResBlock(nn.Module):
     def __init__(self, dim):
-        super(ResnetBlock, self).__init__()
+        super(ResBlock, self).__init__()
         self.conv_block = nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
                                         nn.Conv2D(dim, dim, (3, 3)), nn.ReLU(),
                                         nn.ReflectionPad2d((1, 1, 1, 1)),
