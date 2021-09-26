686f66165a059c4167093ef9e17492d7177ce987
JackMcCoy <fdrdavegill@gmail.com>
2021-09-26 19:19:35 -0400

    fix Conv2d invocation

--- net.py
+++ net.py
@@ -8,97 +8,97 @@ from losses import CalcContentLoss, CalcContentReltLoss, CalcStyleEmdLoss, CalcS
 decoder_1 = nn.Sequential(
     ResBlock(nn.Sequential(
         nn.ReflectionPad2d((1, 1, 1, 1)),
-        nn.Conv2d(512, 512, 3),
+        nn.Conv2d(512, 512, kernel_size=3),
         nn.ReLU(),
-        nn.Conv2d(512, 512, 1),
+        nn.Conv2d(512, 512, kernel_size=1),
     )),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 256, 3),
+    nn.Conv2d(512, 256, kernel_size=3),
     nn.ReLU(),
 )
 decoder_2 = nn.Sequential(
     ResBlock(nn.Sequential(
         nn.ReflectionPad2d((1, 1, 1, 1)),
-        nn.Conv2d(256, 256, 3),
+        nn.Conv2d(256, 256, kernel_size=3),
         nn.ReLU(),
-        nn.Conv2d(256, 256, 1),
+        nn.Conv2d(256, 256, kernel_size=1),
     )),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 128, 3),
+    nn.Conv2d(256, 128, kernel_size=3),
     nn.ReLU(),
 
 )
 decoder_3 = nn.Sequential(
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 128, 3),
+    nn.Conv2d(128, 128, kernel_size=3),
     nn.ReLU(),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 64, 3),
+    nn.Conv2d(128, 64, kernel_size=3),
     nn.ReLU(),
 
 )
 decoder_4 = nn.Sequential(
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 64, 3),
+    nn.Conv2d(64, 64, kernel_size=3),
     nn.ReLU(),
     nn.Upsample(scale_factor=2, mode='nearest'),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 3, 3),
+    nn.Conv2d(64, 3, kernel_size=3),
 )
 
 vgg = nn.Sequential(
-    nn.Conv2d(3, 3, 1),
+    nn.Conv2d(3, 3, kernel_size=1),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(3, 64, 3),
+    nn.Conv2d(3, 64, kernel_size=3),
     nn.ReLU(),  # relu1-1
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 64, 3),
+    nn.Conv2d(64, 64, kernel_size=3),
     nn.ReLU(),  # relu1-2
     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(64, 128, 3),
+    nn.Conv2d(64, 128, kernel_size=3),
     nn.ReLU(),  # relu2-1
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 128, 3),
+    nn.Conv2d(128, 128, kernel_size=3),
     nn.ReLU(),  # relu2-2
     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(128, 256, 3),
+    nn.Conv2d(128, 256, kernel_size=3),
     nn.ReLU(),  # relu3-1
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, 3),
+    nn.Conv2d(256, 256, kernel_size=3),
     nn.ReLU(),  # relu3-2
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, (3, 3)),
+    nn.Conv2d(256, 256, kernel_size=3),
     nn.ReLU(),  # relu3-3
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 256, 3),
+    nn.Conv2d(256, 256, kernel_size=3),
     nn.ReLU(),  # relu3-4
     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(256, 512, 3),
+    nn.Conv2d(256, 512, kernel_size=3),
     nn.ReLU(),  # relu4-1, this is the last layer used
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, 3),
+    nn.Conv2d(512, 512, kernel_size=3),
     nn.ReLU(),  # relu4-2
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, 3),
+    nn.Conv2d(512, 512, kernel_size=3),
     nn.ReLU(),  # relu4-3
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, 3),
+    nn.Conv2d(512, 512, kernel_size=3),
     nn.ReLU(),  # relu4-4
     nn.MaxPool2d((2, 2), (2, 2), (0, 0), ceil_mode=True),
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, 3),
+    nn.Conv2d(512, 512, kernel_size=3),
     nn.ReLU(),  # relu5-1
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, 3),
+    nn.Conv2d(512, 512, kernel_size=3),
     nn.ReLU(),  # relu5-2
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, 3),
+    nn.Conv2d(512, 512, kernel_size=3),
     nn.ReLU(),  # relu5-3
     nn.ReflectionPad2d((1, 1, 1, 1)),
-    nn.Conv2d(512, 512, 3),
+    nn.Conv2d(512, 512, kernel_size=3),
     nn.ReLU()  # relu5-4
 )
 
