cfb88c0181271a97c3f0f1c292b274e8f34efa83
JackMcCoy <fdrdavegill@gmail.com>
2021-09-27 05:10:42 -0400

    definitely buggy vqgan

--- vqgan.py
+++ vqgan.py
@@ -85,8 +85,8 @@ class VQGANLayers(nn.Module):
         self.context_mod = self.context_mod[:32]
         self.z_mod = self.z_mod[:32]
 
-        self.quantize_4_z = VectorQuantize(24, 1024)
-        self.quantize_4_s = VectorQuantize(24, 1024)
+        self.quantize_4_z = VectorQuantize(1024, 512)
+        self.quantize_4_s = VectorQuantize(1024, 512)
         self.transformer_4 = GPT(1024, 512, 24, 16, 1024)
 
     def forward(self, ci, si, training=True):
