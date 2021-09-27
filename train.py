a974f7e79cc1e7ba46a876041af82be6cb38c412
JackMcCoy <fdrdavegill@gmail.com>
2021-09-27 19:27:20 -0400

    add eps to avoid div by 0 in content_emd loss

--- train.py
+++ train.py
@@ -143,7 +143,7 @@ if args.train_model=='drafting':
         losses = calc_losses(stylized, ci, si, cF, sF, enc_, dec_, calc_identity=True)
         loss_c, loss_s, loss_r, loss_ss, l_identity1, l_identity2, l_identity3, l_identity4, mdog = losses
         loss = loss_c * args.content_weight + loss_s * args.style_weight +\
-                    l_identity1 * 50 + l_identity2 * 1 + l_identity3 * 25 + l_identity4 * .5 +\
+                    l_identity1 * 50 + l_identity2 * 1 + l_identity3 * 50 + l_identity4 * 1 +\
                     loss_r * 16 + 10*loss_ss + mdog
         loss.backward()
         optimizer.step()
