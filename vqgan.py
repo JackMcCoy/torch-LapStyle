286e349aed792120fe200015ea0629c074868743
JackMcCoy <fdrdavegill@gmail.com>
2021-09-27 23:53:50 -0400

    debug

--- vqgan.py
+++ vqgan.py
@@ -86,7 +86,7 @@ class VQGANLayers(nn.Module):
         self.context_mod = self.context_mod[:32]
         self.z_mod = self.z_mod[:32]
 
-        embed_dim = 16
+        embed_dim = 256
         z_channels = 512
         codebook_size = 640
         self.quantize_4_z = VectorQuantize(embed_dim, codebook_size)
