5d6e216c39749756173129f7aca5c33270bc1d48
JackMcCoy <fdrdavegill@gmail.com>
2021-09-28 14:38:25 -0400

    axial attention only

--- vqgan.py
+++ vqgan.py
@@ -64,7 +64,6 @@ class VectorQuantize(nn.Module):
                                             reversible=True,
                                             shift_tokens = True,
                                             attend_axially = True)
-            self.pos_embedding = nn.Parameter(torch.randn(1, 256, 512))
         elif transformer_size==2:
             self.transformer = Transformer(dim = 256,
                                             heads = 16,
@@ -73,7 +72,6 @@ class VectorQuantize(nn.Module):
                                             reversible=True,
                                             shift_tokens = True,
                                             attend_axially = True)
-            self.pos_embedding = nn.Parameter(torch.randn(1, 1024, 256))
         elif transformer_size==3:
             self.transformer = Transformer(dim = 512,
                                             heads = 16,
@@ -82,7 +80,6 @@ class VectorQuantize(nn.Module):
                                             max_seq_len = 512,
                                             shift_tokens = True,
                                             attend_axially = True)
-            self.pos_embedding = nn.Parameter(torch.randn(1, 1024, 512))
         elif transformer_size==4:
             self.transformer = Transformer(dim = 256,
                                             heads = 16,
@@ -91,7 +88,6 @@ class VectorQuantize(nn.Module):
                                             max_seq_len = 4096,
                                             shift_tokens = True,
                                             attend_axially = True)
-            self.pos_embedding = nn.Parameter(torch.randn(1, 4096, 256))
             self.rearrange=Rearrange('b c (h p1) (w p2) -> b (h w) (c p1 p2)', p1 = 2, p2 = 2)
             self.decompose_axis=Rearrange('b (h w) (c e d) -> b c (h e) (w d)',h=64,w=64,d=2,e=2)
 
@@ -104,7 +100,7 @@ class VectorQuantize(nn.Module):
         dtype = input.dtype
         quantize = self.rearrange(input)
         b, n, _ = quantize.shape
-        quantize = self.transformer(self.pos_embedding[:,:(n)]+quantize)
+        quantize = self.transformer(quantize)
         quantize = self.decompose_axis(quantize)
 
         quantize = input + (quantize - input).detach()
