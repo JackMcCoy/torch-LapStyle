d76a962696c5decff4cb41d12fbf749d3f64cfae
JackMcCoy <fdrdavegill@gmail.com>
2021-09-28 00:25:09 -0400

    debug

--- train.py
+++ train.py
@@ -68,8 +68,7 @@ def adjust_learning_rate(optimizer, iteration_count,args):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-def warmup_lr_adjust(optimizer, iteration_count, warmup_start=6.5e-7, warmup_iters=1000,
-                         max_lr = 5e-4, decay=5e-5):
+def warmup_lr_adjust(optimizer, iteration_count, warmup_start=6.5e-7, warmup_iters=1000, max_lr = 5e-4, decay=5e-5):
     """Imitating the original implementation"""
     warmup_step = (max_lr - warmup_start) / warmup_iters
     if iteration_count < warmup_iters:
@@ -78,7 +77,6 @@ def warmup_lr_adjust(optimizer, iteration_count, warmup_start=6.5e-7, warmup_ite
         lr = max_lr / (1.0 + decay * (iteration_count - warmup_iters))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
-    return lr
 
 
 parser = argparse.ArgumentParser()
