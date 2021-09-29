1a6b04e10be96b0362346d4e446d920b911d73fa
JackMcCoy <fdrdavegill@gmail.com>
2021-09-28 23:47:43 -0400

    change default lr

--- train.py
+++ train.py
@@ -66,7 +66,7 @@ def adjust_learning_rate(optimizer, iteration_count,args):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-def warmup_lr_adjust(optimizer, iteration_count, warmup_start=6.5e-7, warmup_iters=5000, max_lr = 8e-5, decay=5e-5):
+def warmup_lr_adjust(optimizer, iteration_count, warmup_start=6.5e-7, warmup_iters=1000, max_lr = 1e-4, decay=5e-5):
     """Imitating the original implementation"""
     warmup_step = (max_lr - warmup_start) / warmup_iters
     if iteration_count < warmup_iters:
