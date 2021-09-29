8a464ebca744fde4d86035f0eb039aadb48f898d
JackMcCoy <fdrdavegill@gmail.com>
2021-09-29 05:06:56 -0400

    no identity 3+4

--- vqgan.py
+++ vqgan.py
@@ -29,7 +29,7 @@ class VectorQuantize(nn.Module):
         self,
         dim,
         codebook_size,
-        decay = 0.8,
+        decay = 1,
         commitment = 1.,
         eps = 1e-5,
         n_embed = None,
@@ -118,7 +118,7 @@ class VectorQuantize(nn.Module):
             embed_normalized = self.embed_avg / cluster_size.unsqueeze(0)
             self.embed.data.copy_(embed_normalized)
 
-        loss = F.mse_loss(quantize.detach(), input, norm=True) * self.commitment
+        loss = F.mse_loss(quantize.detach(), input) * self.commitment
         quantize = input + (quantize - input).detach()
         return quantize, embed_ind, loss
 
