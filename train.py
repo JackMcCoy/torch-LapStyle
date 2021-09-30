5da809cb3582c93100da1173f5253fa0a8605448
JackMcCoy <fdrdavegill@gmail.com>
2021-09-30 06:31:19 -0400

    seperate step for codebook

--- train.py
+++ train.py
@@ -171,7 +171,7 @@ if args.train_model=='drafting':
         losses = calc_losses(stylized, ci, si, cF, sF, enc_, dec_, disc_, calc_identity=True, disc_loss=True)
         loss_c, loss_s, loss_r, loss_ss, l_identity1, l_identity2, l_identity3, l_identity4, mdog, codebook_loss, loss_Gp_GAN, debug_cX = losses
         loss = loss_c * args.content_weight + loss_s * args.style_weight +\
-                    l_identity1 * 50 + l_identity2 * 1 +l_identity3 * 50 + l_identity4 * 1 +\
+                    l_identity1 * 50 + l_identity2 * 1 +l_identity3 * 25 + l_identity4 * .5 +\
                     loss_r * 9 + 16*loss_ss + mdog * .33 + loss_Gp_GAN + l
         loss.backward()
         optimizer.step()
