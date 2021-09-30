7ef10a4d4ea5805579e2aabc72e0a342287eafb7
JackMcCoy <fdrdavegill@gmail.com>
2021-09-29 20:06:31 -0400

    reverted

--- train.py
+++ train.py
@@ -31,9 +31,7 @@ def train_transform(load_size, crop_size):
     transform_list = [
         transforms.Resize(size=(load_size, load_size)),
         transforms.RandomCrop(crop_size),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225]),
+        transforms.ToTensor()
     ]
     return transforms.Compose(transform_list)
 
