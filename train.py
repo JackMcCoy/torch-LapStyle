3dd8eec0f6cf85365bc243f47ddc2301dcb2fe15
JackMcCoy <fdrdavegill@gmail.com>
2021-09-30 06:14:43 -0400

    mdog=.33

--- train.py
+++ train.py
@@ -172,7 +172,7 @@ if args.train_model=='drafting':
         loss_c, loss_s, loss_r, loss_ss, l_identity1, l_identity2, l_identity3, l_identity4, mdog, codebook_loss, loss_Gp_GAN, debug_cX = losses
         loss = loss_c * args.content_weight + loss_s * args.style_weight +\
                     l_identity1 * 50 + l_identity2 * 1 +l_identity3 * 50 + l_identity4 * 1 +\
-                    loss_r * 9 + 16*loss_ss + mdog * .5 + l + codebook_loss + loss_Gp_GAN
+                    loss_r * 9 + 16*loss_ss + mdog * .33 + l + codebook_loss + loss_Gp_GAN
         loss.backward()
         optimizer.step()
 
