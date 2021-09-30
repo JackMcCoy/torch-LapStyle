68ef0951d783efbb49c59459a05643a004863af7
JackMcCoy <fdrdavegill@gmail.com>
2021-09-29 20:03:45 -0400

    try normalizing for vgg

--- train.py
+++ train.py
@@ -31,9 +31,9 @@ def train_transform(load_size, crop_size):
     transform_list = [
         transforms.Resize(size=(load_size, load_size)),
         transforms.RandomCrop(crop_size),
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225]),
-        transforms.ToTensor()
+                            std=[0.229, 0.224, 0.225]),
     ]
     return transforms.Compose(transform_list)
 
