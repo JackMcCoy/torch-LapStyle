29a37f2dd37b4e3f81effcf745fd03e223d5b576
JackMcCoy <fdrdavegill@gmail.com>
2021-10-01 21:25:28 -0400

    remd loss in quantizer codebook loss

--- losses.py
+++ losses.py
@@ -17,7 +17,7 @@ class CalcStyleEmdLoss():
         CX_M = calc_emd_loss(pred, target)
         m1, _ = CX_M.min(2)
         m2, _ = CX_M.min(1)
-        loss_remd = torch.max(torch.mean(m1),torch.mean(m2)).unsqueeze(0)
+        loss_remd = torch.max(torch.mean(m1),torch.mean(m2))
         return loss_remd
 
 def calc_emd_loss(pred, target):
