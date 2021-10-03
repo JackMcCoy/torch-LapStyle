9a2debe3d387cce0b5098e92347f6a7c1343a8d2
JackMcCoy <fdrdavegill@gmail.com>
2021-10-02 22:05:17 -0400

    transformer at end, with style context

--- net.py
+++ net.py
@@ -302,7 +302,7 @@ content_loss = CalcContentLoss()
 style_loss = CalcStyleLoss()
 
 def identity_loss(i, F, encoder, decoder):
-    Icc = decoder(F, F)
+    Icc = decoder(F, F, i)
     l_identity1 = content_loss(Icc, i)
     Fcc = encoder(Icc)
     l_identity2 = 0
