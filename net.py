74508cdad986f3daef03bd3a1bcd1f3415d50eaf
JackMcCoy <fdrdavegill@gmail.com>
2021-10-03 08:27:31 -0400

    identity losses

--- net.py
+++ net.py
@@ -311,7 +311,7 @@ content_loss = CalcContentLoss()
 style_loss = CalcStyleLoss()
 
 def identity_loss(i, F, encoder, decoder):
-    Icc = decoder(F, F, i, i)
+    Icc = decoder(F, F)
     l_identity1 = content_loss(Icc, i)
     Fcc = encoder(Icc)
     l_identity2 = 0
