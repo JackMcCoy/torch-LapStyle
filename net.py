e911fa9a57cafa0b8df3531cd210ae6c62317bc1
JackMcCoy <fdrdavegill@gmail.com>
2021-10-02 22:09:12 -0400

    identity loss 3, 4

--- net.py
+++ net.py
@@ -314,8 +314,7 @@ def calc_losses(stylized, ci, si, cF, sF, encoder, decoder, disc_= None, calc_id
     stylized_feats = encoder(stylized)
     if calc_identity==True:
         l_identity1, l_identity2 = identity_loss(ci, cF, encoder, decoder)
-        #l_identity3, l_identity4, cb = identity_loss(si, sF, encoder, decoder)
-        #cb_loss = cb.data
+        l_identity3, l_identity4 = identity_loss(si, sF, encoder, decoder)
     else:
         l_identity1 = None
         l_identity2 = None
@@ -353,5 +352,5 @@ def calc_losses(stylized, ci, si, cF, sF, encoder, decoder, disc_= None, calc_id
     else:
         loss_Gp_GAN = 0
 
-    return loss_c, loss_s, remd_loss, loss_ss, l_identity1, l_identity2, mxdog_losses, loss_Gp_GAN
+    return loss_c, loss_s, remd_loss, loss_ss, l_identity1, l_identity2, l_identity3, l_identity4, mxdog_losses, loss_Gp_GAN
 
