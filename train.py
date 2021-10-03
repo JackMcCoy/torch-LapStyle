e12e3225046bfac5944685eb294fbe8029d150ed
JackMcCoy <fdrdavegill@gmail.com>
2021-10-03 02:45:14 -0400

    no discriminator

--- train.py
+++ train.py
@@ -143,7 +143,6 @@ if args.train_model=='drafting':
     for i in tqdm(range(args.max_iter)):
         warmup_lr_adjust(optimizer, i)
 
-        warmup_lr_adjust(opt_D, i)
         ci = next(content_iter).to(device)
         si = next(style_iter).to(device)
         cF = enc_(ci)
