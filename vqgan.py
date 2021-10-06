a2740dc31f25a07a01ce023876f6a2686eac508d
JackMcCoy <fdrdavegill@gmail.com>
2021-10-06 10:53:20 -0400

    k, v = style

--- vqgan.py
+++ vqgan.py
@@ -119,7 +119,7 @@ class VectorQuantize(nn.Module):
     def forward(self, cF, sF):
         target = adain(cF, sF)
         inputs = []
-        for i in [cF,sF, target]:
+        for i in [cF,sF]:
             quantize = self.normalize(i)
             quantize = self.rearrange(quantize)
             b, n, _ = quantize.shape
@@ -128,7 +128,7 @@ class VectorQuantize(nn.Module):
             position_embeddings = self.pos_embedding(self.position_ids.detach())
             quantize = quantize + position_embeddings
             inputs.append(quantize)
-        quantize = self.transformer(inputs[2],context=inputs[1])
+        quantize = self.transformer(inputs[0],context=inputs[1])
         quantize = self.decompose_axis(quantize)
 
         flatten = quantize.reshape(-1, self.dim)
