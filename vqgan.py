23fcba2e049ff32e1fa2cec5842517385cedf79c
JackMcCoy <fdrdavegill@gmail.com>
2021-10-06 14:17:35 -0400

    mdog = .1

--- vqgan.py
+++ vqgan.py
@@ -64,7 +64,7 @@ class VectorQuantize(nn.Module):
                                             receives_context=True)
             self.rearrange = Rearrange('b c h w -> b (h w) c')
             self.decompose_axis = Rearrange('b (h w) c -> b c h w',h=8,w=8)
-            self.normalize = nn.InstanceNorm2d(512, affine = True)
+            self.normalize = nn.InstanceNorm2d(512)
         if transformer_size==1:
             self.transformer = Transformer(dim = 512,
                                             heads = 16,
@@ -75,7 +75,7 @@ class VectorQuantize(nn.Module):
                                             receives_context=True)
             self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (c p1 p2)',p1=1,p2=1)
             self.decompose_axis = Rearrange('b (h w) (c e d) -> b c (h e) (w d)',h=16,w=16, e=1,d=1)
-            self.normalize = nn.InstanceNorm2d(512, affine = True)
+            self.normalize = nn.InstanceNorm2d(512)
         elif transformer_size==2:
             self.transformer = Transformer(dim = 1024,
                                             heads = 16,
@@ -120,8 +120,8 @@ class VectorQuantize(nn.Module):
         target = adain(cF, sF)
         inputs = []
         for i in [cF,sF]:
-            #quantize = self.normalize(i)
-            quantize = self.rearrange(i)
+            quantize = self.normalize(i)
+            quantize = self.rearrange(quantize)
             b, n, _ = quantize.shape
             if not self.embeddings_set:
                 self.set_embeddings(b,n,_)
