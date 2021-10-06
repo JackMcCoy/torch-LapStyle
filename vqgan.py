ce996431c44e27c01d4446f45a142e3bdd29b0c6
JackMcCoy <fdrdavegill@gmail.com>
2021-10-06 06:13:47 -0400

    no affine transform

--- vqgan.py
+++ vqgan.py
@@ -152,7 +152,7 @@ class VectorQuantize(nn.Module):
             embed_normalized = self.embed_avg / cluster_size.unsqueeze(0)
             self.embed.data.copy_(embed_normalized)
 
-        loss = self.perceptual_loss(quantize.detach(), target) * self.commitment
+        loss = F.mse_loss(quantize.detach(), target) * self.commitment
 
         quantize = target + (quantize.detach() - target)
 
