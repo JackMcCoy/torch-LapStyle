76d1a0f3edf741f8797df3aea9a7cdaa33591a0e
JackMcCoy <fdrdavegill@gmail.com>
2021-10-07 16:48:09 -0400

    instance norm

--- train.py
+++ train.py
@@ -167,9 +167,9 @@ if args.train_model=='drafting':
         dec_.zero_grad()
         optimizer.zero_grad()
         losses = calc_losses(stylized, ci, si, cF, sF, enc_, dec_, disc_, calc_identity=True, disc_loss=True, mdog_losses=True)
-        loss_c, loss_s, loss_r, loss_ss, l_identity1, l_identity2, l_identity3, l_identity4,mdog, loss_Gp_GAN, cb = losses
+        loss_c, loss_s, loss_r, loss_ss, l_identity1, l_identity2, l_identity3, l_identity4,mdog, loss_Gp_GAN, cb_i = losses
         loss = cb + loss_c * args.content_weight + loss_s * args.style_weight+\
-                    loss_r * 10 + 18*loss_ss + l_identity1*50 + l_identity2 * 1 + l_identity3 * 50 + l_identity4 * 1 + mdog * .65 + cb + loss_Gp_GAN * 2.5
+                    loss_r * 10 + 18*loss_ss + l_identity1*50 + l_identity2 * 1 + l_identity3 * 50 + l_identity4 * 1 + mdog * .65 + loss_Gp_GAN * 2.5
         loss.backward()
         optimizer.step()
 
