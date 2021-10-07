feb763a8a128a071253ff3f9fef3ad820de6d90a
JackMcCoy <fdrdavegill@gmail.com>
2021-10-07 16:43:23 -0400

    instance norm

--- vqgan.py
+++ vqgan.py
@@ -132,7 +132,7 @@ class VectorQuantize(nn.Module):
             position_embeddings = self.pos_embedding(self.position_ids.detach())
             quantize = quantize + position_embeddings
             inputs.append(quantize)
-        quantize = self.transformer(inputs[2], context=inputs[0])
+        quantize = self.transformer(inputs[0], context=inputs[1])
         quantize = self.decompose_axis(quantize)
 
         flatten = quantize.reshape(-1, self.dim)
