2d309b9aaf08fce29a05e66a23f8dc7525a0db2d
JackMcCoy <fdrdavegill@gmail.com>
2021-10-07 23:47:01 -0400

    three quantization layers

--- net.py
+++ net.py
@@ -172,8 +172,8 @@ class DecoderVQGAN(nn.Module):
 
         #self.quantize_5 = VectorQuantize(8, 320, transformer_size=0, **rc)
         self.quantize_4 = VectorQuantize(16, 64, transformer_size=1, **rc)
-        #self.quantize_3 = VectorQuantize(32, 640, transformer_size=2, **rc)
-        #self.quantize_2 = VectorQuantize(64, 1280, transformer_size=3, **rc)
+        self.quantize_3 = VectorQuantize(32, 128, transformer_size=2, **rc)
+        self.quantize_2 = VectorQuantize(64, 128, transformer_size=3, **rc)
         #self.quantize_1 = VectorQuantize(128, 640, transformer_size=4, **rc)
         '''
         self.vit = Transformer(192, 4, 256, 16, 192, shift_tokens=True,
@@ -229,14 +229,14 @@ class DecoderVQGAN(nn.Module):
         quantized, idx, codebook_loss = self.quantize_4(cF['r4_1'], sF['r4_1'])
         t = self.decoder_1(quantized)
         t = self.upsample(t)
-        t += adain(cF['r3_1'], sF['r3_1']).data
-        #codebook_loss += cbloss.data
-        #t += quantized.data
+        quantized, idx, cbloss = self.quantize_3(cF['r3_1'], sF['r3_1'])
+        codebook_loss += cbloss.data
+        t += quantized.data
         t = self.decoder_2(t)
         t = self.upsample(t)
-        t += adain(cF['r2_1'], sF['r2_1'])
-        #codebook_loss += cbloss.data
-        #t += quantized.data
+        quantized, idx, cbloss = self.quantize_2(cF['r2_1'], sF['r2_1'])
+        codebook_loss += cbloss.data
+        t += quantized.data
         t = self.decoder_3(t)
         t = self.upsample(t)
         t = self.decoder_4(t)
