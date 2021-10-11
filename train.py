71a1ed2c022d7ff0bbcff904ca2d6f038c1ac892
JackMcCoy <fdrdavegill@gmail.com>
2021-10-11 13:20:33 -0400

    use identity loss 3+4, mdog weight =.5

--- train.py
+++ train.py
@@ -170,7 +170,7 @@ if args.train_model=='drafting':
         loss_c, loss_s, loss_r, loss_ss, l_identity1, l_identity2, mdog, loss_Gp_GAN = losses
         loss = loss_c * args.content_weight + loss_s * args.style_weight+\
                     loss_r * 16 + 16*loss_ss + l_identity1*50 + l_identity2 * 1 +\
-                    l_identity3*50 + l_identity4 * 1mdog * .5 + loss_Gp_GAN * 5
+                    l_identity3*50 + l_identity4 * 1 + mdog * .5 + loss_Gp_GAN * 5
         loss.backward()
         optimizer.step()
 
