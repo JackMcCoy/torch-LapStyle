819883cced029e296b074c7492c41be93afd23d5
JackMcCoy <fdrdavegill@gmail.com>
2021-10-11 18:30:05 -0400

    no context

--- vqgan.py
+++ vqgan.py
@@ -37,10 +37,9 @@ class TransformerOnly(nn.Module):
                                            max_seq_len=64,
                                            shift_tokens=True,
                                            reversible=True,
-                                           receives_context=True)
+                                           receives_context=False)
             self.rearrange = Rearrange('b c h w -> b (h w) c')
             self.decompose_axis = Rearrange('b (h w) c -> b c h w', h=8, w=8)
-            self.normalize = nn.InstanceNorm2d(512)
         if transformer_size == 1:
             self.transformer = Transformer(dim=512,
                                            heads=16,
@@ -48,10 +47,9 @@ class TransformerOnly(nn.Module):
                                            max_seq_len=256,
                                            shift_tokens=True,
                                            reversible=True,
-                                           receives_context=True)
+                                           receives_context=False)
             self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (c p1 p2)', p1=1, p2=1)
             self.decompose_axis = Rearrange('b (h w) (c e d) -> b c (h e) (w d)', h=16, w=16, e=1, d=1)
-            self.normalize = nn.InstanceNorm2d(512)
         elif transformer_size==2:
             self.transformer = Transformer(dim = 1024,
                                             heads = 16,
@@ -59,10 +57,9 @@ class TransformerOnly(nn.Module):
                                             max_seq_len = 256,
                                             shift_tokens = True,
                                             reversible=True,
-                                            receives_context=True)
+                                            receives_context=False)
             self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (c p1 p2)',p1=2,p2=2)
             self.decompose_axis = Rearrange('b (h w) (c e d) -> b c (h e) (w d)',h=16,w=16, e=2,d=2)
-            self.normalize = nn.InstanceNorm2d(256, affine=False)
         elif transformer_size==3:
             self.transformer = Transformer(dim = 2048,
                                             heads = 16,
@@ -70,10 +67,9 @@ class TransformerOnly(nn.Module):
                                             max_seq_len = 256,
                                             shift_tokens = True,
                                             reversible = True,
-                                            receives_context=True)
+                                            receives_context=False)
             self.rearrange = Rearrange('b c (h p1) (w p2) -> b (h w) (c p1 p2)',p1=4,p2=4)
             self.decompose_axis = Rearrange('b (h w) (c e d) -> b c (h e) (w d)',h=16,w=16, e=4,d=4)
-            self.normalize = nn.InstanceNorm2d(128, affine=False)
         elif transformer_size==4:
             self.transformer = Transformer(dim = 1024,
                                             heads = 16,
@@ -81,7 +77,7 @@ class TransformerOnly(nn.Module):
                                             max_seq_len = 1024,
                                             reversible = True,
                                             shift_tokens = True,
-                                            receives_context=True)
+                                            receives_context=False)
 
             self.rearrange=Rearrange('b c (h p1) (w p2) -> b (h w) (c p1 p2)', p1 = 4, p2 = 4)
             self.decompose_axis=Rearrange('b (h w) (c e d) -> b c (h e) (w d)',h=32,w=32,d=4,e=4)
@@ -95,18 +91,15 @@ class TransformerOnly(nn.Module):
 
     def forward(self, cF, sF):
         target = adain(cF, sF)
-        quantize = self.normalize(cF)
-        inputs = []
-        for i in [quantize, sF]:
-            quantize = self.rearrange(i)
-            b, n, _ = quantize.shape
-            if not self.embeddings_set:
-                self.set_embeddings(b, n, _)
-            position_embeddings = self.pos_embedding(self.position_ids.detach())
-            quantize = quantize + position_embeddings
-            inputs.append(quantize)
 
-        quantize = self.transformer(inputs[0],context=inputs[1])
+        quantize = self.rearrange(target)
+        b, n, _ = quantize.shape
+        if not self.embeddings_set:
+            self.set_embeddings(b, n, _)
+        position_embeddings = self.pos_embedding(self.position_ids.detach())
+        quantize = quantize + position_embeddings
+
+        quantize = self.transformer(quantize)
         quantize = self.decompose_axis(quantize)
         quantize = target + (quantize - target)
         return quantize, None, 0
