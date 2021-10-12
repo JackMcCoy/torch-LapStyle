1435945ba6f68b1bab3e30eed14edc5d0b7dca9e
JackMcCoy <fdrdavegill@gmail.com>
2021-10-11 21:37:21 -0400

    vector quantize

--- net.py
+++ net.py
@@ -171,9 +171,9 @@ class DecoderVQGAN(nn.Module):
         rc = dict(receives_ctx=True)
 
         #self.quantize_5 = VectorQuantize(8, 320, transformer_size=0, **rc)
-        self.quantize_4 = VectorQuantize(transformer_size=1)
-        self.quantize_3 = VectorQuantize(transformer_size=2)
-        self.quantize_2 = VectorQuantize(transformer_size=3)
+        self.quantize_4 = VectorQuantize(16, 640, transformer_size=1)
+        self.quantize_3 = VectorQuantize(32, 640, transformer_size=2)
+        self.quantize_2 = VectorQuantize(64, 1280, transformer_size=3)
         #self.quantize_1 = VectorQuantize(128, 640, transformer_size=4, **rc)
         '''
         self.vit = Transformer(192, 4, 256, 16, 192, shift_tokens=True,
