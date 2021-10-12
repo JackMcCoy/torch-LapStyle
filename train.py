27851542c778cc56108b623f74996b5113327c78
JackMcCoy <fdrdavegill@gmail.com>
2021-10-12 10:41:37 -0400

    adjust weights to match paddle implementation

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ if args.train_model=='drafting':
         if (i + 1) % 10 == 0:
             print(f'{loss.item():.2f}')
             print(f'disc: {loss_D.item():.4f} gan_loss: {loss_Gp_GAN.item():.3f}, c: {loss_c.item():.3f} s: {loss_s.item():.3f} \
-            r: {loss_r.item():.3f} ss: {loss_ss.item():.3f} \
+            style_remd: {style_remd.item():.3f} content_relt: {content_relt.item():.3f} \
             mdog_loss: {mdog.item():.3f}')
 
         writer.add_scalar('loss_content', loss_c.item(), i + 1)
