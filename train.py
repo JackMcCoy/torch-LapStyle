e33a3f74b550f12ff568c70b2419d9dfdc9c65a5
JackMcCoy <fdrdavegill@gmail.com>
2021-10-12 11:17:49 -0400

    lr schedule changes

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ def adjust_learning_rate(optimizer, iteration_count,args):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-def warmup_lr_adjust(optimizer, iteration_count, warmup_start=6.5e-8, warmup_iters=1500, max_lr = 1e-4, decay=5e-5):
+def warmup_lr_adjust(optimizer, iteration_count, warmup_start=1e-8, warmup_iters=1000, max_lr = 1e-3, decay=5e-5):
     """Imitating the original implementation"""
     warmup_step = (max_lr - warmup_start) / warmup_iters
     if iteration_count < warmup_iters:
@@ -145,7 +145,7 @@ if args.train_model=='drafting':
     disc_.to(device)
 
     optimizer = torch.optim.Adam(dec_.parameters(), lr=args.lr)
-    opt_D = torch.optim.Adam(disc_.parameters(),lr=args.lr)
+    opt_D = torch.optim.Adam(disc_.parameters(),lr=args.lr, weight_decay = .1)
     for i in tqdm(range(args.max_iter)):
         warmup_lr_adjust(optimizer, i)
 
