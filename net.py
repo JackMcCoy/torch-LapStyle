bbc49c07a63405bc8722b33bedbb49d3f0893558
JackMcCoy <fdrdavegill@gmail.com>
2021-10-15 10:47:42 -0400

    quantize_1

--- net.py
+++ net.py
@@ -174,7 +174,7 @@ class DecoderVQGAN(nn.Module):
         self.quantize_4 = VectorQuantize(16, 860, transformer_size=1)
         self.quantize_3 = VectorQuantize(32, 860, transformer_size=2)
         self.quantize_2 = VectorQuantize(64, 1280, transformer_size=3)
-        #self.quantize_1 = VectorQuantize(128, 860, transformer_size=4, **rc)
+        self.quantize_1 = VectorQuantize(128, 860, transformer_size=4, **rc)
 
         self.vit = Transformer(192, 4, 256, 16, 192, shift_tokens=True,
                                reversible=True,
@@ -249,9 +249,9 @@ class DecoderVQGAN(nn.Module):
         cb_loss += cb.data
         t = self.decoder_3(t)
         t = self.upsample(t)
-        #quantized, idx, cb = self.quantize_1(cF['r1_1'], sF['r1_1'])
-        #t += quantized.data
-        #cb_loss += cb.data
+        quantized, idx, cb = self.quantize_1(cF['r1_1'], sF['r1_1'])
+        t += quantized.data
+        cb_loss += cb.data
         t = self.decoder_4(t)
 
         quantized = self.rearrange(t)
