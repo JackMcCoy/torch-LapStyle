2a9c5270e12903e9c7011219d1a99d94553a2731
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 11:47:28 -0400

    multiply instead of add

--- net.py
+++ net.py
@@ -213,13 +213,13 @@ class DecoderAdaConv(nn.Module):
         x = self.kernel_1(style, cF['r4_1'])
         x = self.decoder_1(x)
         x = self.upsample(x)
-        x *= self.kernel_2(style, cF['r3_1']).data
+        x += self.kernel_2(style, cF['r3_1']).data
         x = self.decoder_2(x)
         x = self.upsample(x)
-        x *= self.kernel_3(style, cF['r2_1']).data
+        x += self.kernel_3(style, cF['r2_1']).data
         x = self.decoder_3(x)
         x = self.upsample(x)
-        x *= self.kernel_4(style, cF['r1_1']).data
+        x += self.kernel_4(style, cF['r1_1']).data
         x = self.decoder_4(x)
         return x
 
