4f1a76a09864bf8d2b7b454cccae0f14be158ac0
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 11:49:24 -0400

    multiply instead of add

--- net.py
+++ net.py
@@ -404,10 +404,14 @@ def calc_losses(stylized, ci, si, cF, sF, encoder, decoder, disc_= None, calc_id
     loss_s = 0
     for key in style_layers:
         loss_s += style_loss(stylized_feats[key], sF[key]).data
-    content_relt = content_emd_loss(stylized_feats['r3_1'], cF['r3_1']) +\
-        content_emd_loss(stylized_feats['r4_1'], cF['r4_1'])
-    style_remd = style_remd_loss(stylized_feats['r3_1'], sF['r3_1']) +\
-        style_remd_loss(stylized_feats['r4_1'], sF['r4_1'])
+    if calc_remd:
+        content_relt = content_emd_loss(stylized_feats['r3_1'], cF['r3_1']) +\
+            content_emd_loss(stylized_feats['r4_1'], cF['r4_1'])
+        style_remd = style_remd_loss(stylized_feats['r3_1'], sF['r3_1']) +\
+            style_remd_loss(stylized_feats['r4_1'], sF['r4_1'])
+    else:
+        content_relt = 0
+        style_remd = 0
 
     if mdog_losses:
         cX,_ = xdog(ci.detach(),gaus_1,gaus_2,morph,gamma=.9,morph_cutoff=8.85,morphs=1)
--- train.py
+++ train.py
@@ -196,8 +196,7 @@ if args.train_model=='drafting':
 
         if (i + 1) % 10 == 0:
             print(f'{loss.item():.2f}')
-            print(f'c: {loss_c.item():.3f} s: {loss_s.item():.3f} \
-            style_remd: {style_remd.item():.3f} content_relt: {content_relt.item():.3f}')
+            print(f'c: {loss_c.item():.3f} s: {loss_s.item():.3f}')
 
         writer.add_scalar('loss_content', loss_c.item(), i + 1)
         writer.add_scalar('loss_style', loss_s.item(), i + 1)
