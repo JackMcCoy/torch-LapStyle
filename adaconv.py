efa9fbeeb54b9c2a2cffd09322c676eb35136aed
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 04:01:15 -0400

    relu on kernel prediction

--- adaconv.py
+++ adaconv.py
@@ -32,10 +32,16 @@ class KernelPredictor(nn.Module):
         self.pointwise_groups = c_out//p
         self.c_out = c_out
         self.c_in = c_in
-        self.depthwise_kernel_conv = nn.Conv2d(512, self.c_in//self.n_groups, 2)
+        self.depthwise_kernel_conv = nn.Sequential(
+            nn.Conv2d(512, self.c_in//self.n_groups, 2),
+            nn.ReLU())
         self.pointwise_avg_pool = nn.AvgPool2d(4)
-        self.pw_cn_kn = nn.Conv2d(512, self.c_out//self.pointwise_groups, 1)
-        self.pw_cn_bias = nn.Conv2d(512, c_out, 1)
+        self.pw_cn_kn = nn.Sequential(
+            nn.Conv2d(512, self.c_out//self.pointwise_groups, 1),
+            nn.ReLU())
+        self.pw_cn_bias = nn.Sequential(
+            nn.Conv2d(512, c_out, 1),
+            nn.ReLU())
 
     def forward(self, style_encoding):
         N = style_encoding.shape[0]
