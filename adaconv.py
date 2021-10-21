e0b190e945edf22d5119b8053b8180afefecdc91
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 09:52:42 -0400

    dw kn prediction change

--- adaconv.py
+++ adaconv.py
@@ -36,10 +36,10 @@ class KernelPredictor(nn.Module):
         self.c_out = c_out
         self.c_in = c_in
         self.depthwise_kernel_conv = nn.Sequential(
-            nn.Conv2d(512, self.c_in*(self.c_out//self.n_groups), 2, groups = 512),
+            nn.Conv2d(512, self.c_in*(self.c_in//self.n_groups), 2, groups = 512),
             nn.LeakyReLU())
         self.pointwise_avg_pool = nn.AvgPool2d(4)
-        self.pw_cn_kn = nn.Sequential(nn.Conv2d(512, self.c_out*self.pointwise_groups, 1),
+        self.pw_cn_kn = nn.Sequential(nn.Conv2d(512, self.c_out*(self.c_out//self.pointwise_groups), 1),
                                       nn.LeakyReLU())
         self.pw_cn_bias = nn.Sequential(nn.Conv2d(512, c_out, 1),
                                         nn.LeakyReLU())
