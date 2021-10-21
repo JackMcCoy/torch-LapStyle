ab46c06e702e3957ffb6f9479fd439cba9132973
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 17:32:12 -0400

    relu

--- adaconv.py
+++ adaconv.py
@@ -13,12 +13,13 @@ class AdaConv(nn.Module):
         depthwise, pointwise_kn, pointwise_bias = self.kernel_predictor(style_encoding)
         spatial_conv_out = []
         N = style_encoding.shape[0]
-        size = content_in.size()
-        content_mean, content_std = calc_mean_std(content_in)
+        if self.kernel_predictor.c_in==512:
+            size = content_in.size()
+            content_mean, content_std = calc_mean_std(content_in)
 
-        normalized_feat = (content_in - content_mean.expand(
-            size)) / content_std.expand(size)
-        predicted = self.pad(normalized_feat)
+            content_in = (content_in - content_mean.expand(
+                size)) / content_std.expand(size)
+        predicted = self.pad(content_in)
         for i in range(N):
             depth = nn.functional.conv2d(predicted[i, :, :, :].unsqueeze(0),
                                          weight=depthwise[i],
