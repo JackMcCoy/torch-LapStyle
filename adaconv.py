b80dec10263df91102a8b64cd8296e73e2a08abd
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 15:56:15 -0400

    blend content feats and network

--- adaconv.py
+++ adaconv.py
@@ -20,7 +20,8 @@ class AdaConv(nn.Module):
                                          groups=self.kernel_predictor.n_groups)
             spatial_conv_out.append(self.relu(nn.functional.conv2d(depth,
                                                          weight=pointwise_kn[i],
-                                                         bias=pointwise_bias[i])))
+                                                         bias=pointwise_bias[i],
+                                                         groups=self.kernel_predictor.pointwise_groups)))
         predicted = torch.cat(spatial_conv_out,0)
         if type(feats) != bool:
             feat_conv = []
