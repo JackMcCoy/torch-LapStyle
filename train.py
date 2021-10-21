b0403651c82724fc9865ae2f85ba7cc6c6c32f10
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 12:00:07 -0400

    multiply instead of add

--- train.py
+++ train.py
@@ -174,7 +174,7 @@ if args.train_model=='drafting':
 
         with autocast(enabled=ac_enabled):
         '''
-        losses = calc_losses(stylized, ci.detach(), si.detach(), cF, sF, enc_, dec_, calc_identity=False, disc_loss=False, mdog_losses=False)
+        losses = calc_losses(stylized, ci, si, cF, sF, enc_, dec_, calc_identity=False, disc_loss=False, mdog_losses=False)
         loss_c, loss_s, style_remd, content_relt, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN = losses
         loss = loss_c * args.content_weight + args.style_weight * loss_s
         loss.backward()
@@ -183,7 +183,7 @@ if args.train_model=='drafting':
         cF = enc_(ci)
         sF = enc_(si)
         stylized = dec_(sF, cF)
-        losses = calc_losses(stylized, ci.detach(), si.detach(), cF, sF, enc_, dec_, calc_identity=False, disc_loss=False,
+        losses = calc_losses(stylized, ci, si, cF, sF, enc_, dec_, calc_identity=False, disc_loss=False,
                              mdog_losses=False)
         loss_c, loss_s, style_remd, content_relt, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN = losses
         loss = loss_c * args.content_weight + args.style_weight * loss_s
