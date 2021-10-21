852237079c2d37d9ede08cc88db4f8a6f9746a03
JackMcCoy <fdrdavegill@gmail.com>
2021-10-21 00:17:06 -0400

    one conv

--- adaconv.py
+++ adaconv.py
@@ -14,13 +14,10 @@ class AdaConv(nn.Module):
         N = style_encoding.shape[0]
         content_in = self.pad(content_in)
         for i in range(N):
-            depth = nn.functional.conv2d(content_in[i,:,:,:].unsqueeze(0),
-                                       weight = depthwise[i],
-                                       groups = self.kernel_predictor.n_groups)
-            spatial_conv_out.append(self.relu(nn.functional.conv2d(depth,
-                                                         weight = pointwise_kn[i],
-                                                         bias = pointwise_bias[i],
-                                                         groups = self.kernel_predictor.pointwise_groups)))
+            spatial_conv_out.append(nn.functional.conv2d(content_in[i,:,:,:].unsqueeze(0),
+                                       weight = depthwise[i] * pointwise_kn[i],
+                                       bias = pointwise_bias[i],
+                                       groups = self.kernel_predictor.n_groups))
         return torch.cat(spatial_conv_out,0)
 
 
