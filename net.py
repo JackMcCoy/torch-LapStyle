2f1601f5ee186776fe5f71081da0577c1d495cca
JackMcCoy <fdrdavegill@gmail.com>
2021-10-22 03:05:48 -0400

    remd

--- net.py
+++ net.py
@@ -224,8 +224,8 @@ class DecoderAdaConv(nn.Module):
 
     def forward(self, sF, cF):
         b, n, h, w = sF['r4_1'].shape
-        style, indices, commit_loss = self.vq(sF['r4_1'].detach()).flatten(1)
-        style = self.style_projection(style).reshape(b, 512, 4, 4)
+        style, indices, commit_loss = self.vq(sF['r4_1'].detach())
+        style = self.style_projection(style.flatten(1)).reshape(b, 512, 4, 4)
         x = self.kernel_1(style, cF['r4_1'])
         x = self.decoder_1(x)
         x = self.upsample(x)
