bf4b62cd9f4f2d3bf4a12a0268f4389a13fbc438
JackMcCoy <fdrdavegill@gmail.com>
2021-10-22 16:44:00 -0400

    no relt or remd

--- adaconv.py
+++ adaconv.py
@@ -52,7 +52,7 @@ class KernelPredictor(nn.Module):
         self.c_in = c_in
         self.style_groups = 64//p
         self.depthwise_kernel_conv = nn.Sequential(
-            nn.Conv2d(64, 64*(self.c_in//self.n_groups), 2, groups = self.style_groups),
+            nn.Conv2d(64, (self.c_in//self.n_groups), 2, groups = self.style_groups),
             nn.ReLU())
         self.pointwise_avg_pool = nn.AvgPool2d(4)
         self.pw_cn_kn = nn.Sequential(
