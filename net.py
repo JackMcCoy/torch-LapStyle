ee14c93a362789909fb52760570d4c158a350589
JackMcCoy <fdrdavegill@gmail.com>
2021-10-22 04:14:02 -0400

    no amp

--- net.py
+++ net.py
@@ -180,7 +180,7 @@ class DecoderAdaConv(nn.Module):
         super(DecoderAdaConv, self).__init__()
         self.vq = VectorQuantize(
             dim = 4,
-            codebook_size = 860,
+            codebook_size = 480,
             kmeans_init=True,
             kmeans_iters=10,
             use_cosine_sim=True,
