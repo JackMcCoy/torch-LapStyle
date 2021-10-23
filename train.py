5ff3d3c50b29c7dbbdb7fd85164c6518baa89124
JackMcCoy <fdrdavegill@gmail.com>
2021-10-22 22:22:39 -0400

    no warmup

--- train.py
+++ train.py
@@ -186,7 +186,7 @@ if args.train_model=='drafting':
             optimizer.zero_grad()
             losses = calc_losses(stylized, ci.detach(), si.detach(), cF, sF, enc_, dec_, disc_, calc_identity=False, disc_loss=True, mdog_losses=False)
             loss_c, loss_s, style_remd, content_relt, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN = losses
-            loss = loss_c * args.content_weight + args.style_weight * (loss_s + 3*style_remd) +\
+            loss = loss_c * args.content_weight + args.style_weight * (loss_s + 2*style_remd) +\
                         content_relt*16 + l_identity1*50 + l_identity2 * 1 +\
                         l_identity3* 25 + l_identity4 * .5 + mdog * .65 + loss_Gp_GAN * 5 + cb_loss
         scaler.scale(loss).backward()
