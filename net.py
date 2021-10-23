999d13ec970b1d8141b2c3ebbe088e36a0808061
JackMcCoy <fdrdavegill@gmail.com>
2021-10-22 23:21:11 -0400

    no warmup

--- net.py
+++ net.py
@@ -222,7 +222,7 @@ class DecoderAdaConv(nn.Module):
         b, n, h, w = sF['r4_1'].shape
         style = self.style_encoding(sF['r4_1'].detach())
         style, indices, commit_loss = self.vq(style)
-        style = self.style_projection(style.flatten(1)).reshape(b, 64, 4, 4)
+        style = self.style_projection(style.flatten(1)).reshape(b, 512, 4, 4)
         x = self.kernel_1(style, cF['r4_1'])
         x = self.decoder_1(x)
         x = self.upsample(x)
