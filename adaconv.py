32543fe2172151b7d23f4a7737bb91d5221230f2
JackMcCoy <fdrdavegill@gmail.com>
2021-10-23 21:28:45 -0400

    no warmup

--- adaconv.py
+++ adaconv.py
@@ -9,6 +9,7 @@ class AdaConv(nn.Module):
         self.kernel_predictor = KernelPredictor(ch_in, ch_in, p, s_d = s_d)
         self.pad = nn.ReflectionPad2d((1, 1, 1, 1))
         self.relu = nn.LeakyReLU()
+        self.tanh = nn.Tanh()
 
     def forward(self, style_encoding, content_in, feats=False):
         depthwise, pointwise_kn, pointwise_bias = self.kernel_predictor(style_encoding)
@@ -24,10 +25,10 @@ class AdaConv(nn.Module):
             depth = nn.functional.conv2d(predicted[i, :, :, :].unsqueeze(0),
                                          weight=depthwise[i],
                                          groups=self.kernel_predictor.n_groups)
-            spatial_conv_out.append(nn.functional.conv2d(depth,
+            spatial_conv_out.append(self.tanh(nn.functional.conv2d(depth,
                                                          weight=pointwise_kn[i],
                                                          bias=pointwise_bias[i],
-                                                         groups=self.kernel_predictor.pointwise_groups))
+                                                         groups=self.kernel_predictor.pointwise_groups)))
         predicted = torch.cat(spatial_conv_out,0)
         return predicted
 
