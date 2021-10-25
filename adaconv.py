e96643b877ee27a6370befb66875b7b7ce7c3f0e
JackMcCoy <fdrdavegill@gmail.com>
2021-10-25 17:26:22 -0400

    use adaconv out as

--- adaconv.py
+++ adaconv.py
@@ -41,9 +41,9 @@ class KernelPredictor(nn.Module):
         self.c_out = c_out
         self.c_in = c_in
         self.style_groups = (s_d//p)
-        self.depthwise_kernel_conv = nn.Conv2d(s_d, self.c_out * (self.c_in//self.n_groups), 2)
+        self.depthwise_kernel_conv = nn.Conv2d(s_d, self.c_out * (self.c_in//self.n_groups), 2, groups=self.n_groups)
         self.pointwise_avg_pool = nn.AvgPool2d(4)
-        self.pw_cn_kn = nn.Conv2d(s_d, self.c_out*(self.c_out//self.n_groups), 1)
+        self.pw_cn_kn = nn.Conv2d(s_d, self.c_out*(self.c_out//self.n_groups), 1, groups=self.n_groups)
         self.pw_cn_bias = nn.Conv2d(s_d, c_out, 1)
         self.apply(self._init_weights)
 
