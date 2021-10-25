b431ca89bffeedded363f8457778d789c859284e
JackMcCoy <fdrdavegill@gmail.com>
2021-10-24 20:25:51 -0400

    use adaconv out as

--- adaconv.py
+++ adaconv.py
@@ -22,9 +22,9 @@ class AdaConv(nn.Module):
                 size)) / content_std.expand(size)
         predicted = self.pad(content_in)
         for i in range(N):
-            depth = self.relu(nn.functional.conv2d(predicted[i, :, :, :].unsqueeze(0),
+            depth = nn.functional.conv2d(predicted[i, :, :, :].unsqueeze(0),
                                          weight=depthwise[i],
-                                         groups=self.kernel_predictor.n_groups))
+                                         groups=self.kernel_predictor.n_groups)
             spatial_conv_out.append(self.relu(nn.functional.conv2d(depth,
                                                          weight=pointwise_kn[i],
                                                          bias=pointwise_bias[i],
