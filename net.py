0d7a1919f0ae24118905327aa5cb74a661b59249
JackMcCoy <fdrdavegill@gmail.com>
2021-10-25 11:51:00 -0400

    use adaconv out as

--- net.py
+++ net.py
@@ -411,8 +411,8 @@ def identity_loss(i, F, encoder, decoder):
         l_identity2 = l_identity2 + content_loss(Fcc[key], F[key]).data
     return l_identity1, l_identity2
 
-content_layers = ['r1_1','r2_1','r3_1','r4_1', 'r5_1']
-style_layers = ['r1_1','r2_1','r3_1','r4_1', 'r5_1']
+content_layers = ['r1_1','r2_1','r3_1','r4_1']
+style_layers = ['r1_1','r2_1','r3_1','r4_1']
 
 def calc_losses(stylized, ci, si, adaconv_out, cF, sF, encoder, decoder, disc_= None, calc_identity=True, mdog_losses = True, disc_loss=True):
     stylized_feats = encoder(stylized)
