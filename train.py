2d4827452b0ce1929e145a037d3024521acafad5
JackMcCoy <fdrdavegill@gmail.com>
2021-10-25 20:47:53 -0400

    use adaconv out as

--- train.py
+++ train.py
@@ -206,8 +206,8 @@ if args.train_model=='drafting':
 
         if (i + 1) % 10 == 0:
             print(f'{loss.item():.2f}')
-            print(f'c: {loss_c.item():.3f} s: {loss_s.item():.3f} '
-              f'style_relt: {style_remt.item():.3f} content_relt: {content_relt.item():.3f')
+            print(f'c: {loss_c.item():.3f} s: {loss_s.item():.3f} \
+              style_relt: {style_remd.item():.3f} content_relt: {content_relt.item():.3f}')
 
         writer.add_scalar('loss_content', loss_c.item(), i + 1)
         writer.add_scalar('loss_style', loss_s.item(), i + 1)
