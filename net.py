5f73b3dabf3f12b1229b430a3be0ecb6cd1a0adc
JackMcCoy <fdrdavegill@gmail.com>
2021-11-01 17:24:48 -0400

    single-sample kernel estimation loop

--- net.py
+++ net.py
@@ -408,7 +408,7 @@ content_loss = CalcContentLoss()
 style_loss = CalcStyleLoss()
 
 def identity_loss(i, F, encoder, decoder):
-    Icc = decoder(F, F, train_loop = True)
+    Icc, ada, cb = decoder(F, F)
     l_identity1 = content_loss(Icc, i)
     Fcc = encoder(Icc)
     l_identity2 = 0
