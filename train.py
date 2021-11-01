feb435595be63cbe7bfc5cb4dbff64fe1906915f
JackMcCoy <fdrdavegill@gmail.com>
2021-11-01 14:29:48 -0400

    longer warmup

--- train.py
+++ train.py
@@ -149,17 +149,17 @@ if args.train_model=='drafting':
         set_requires_grad(enc_, False)
         enc_.train(False)
         dec_ = net.DecoderAdaConv()
-        disc_ = net.Discriminator(depth=9, num_channels=64)
+        #disc_ = net.Discriminator(depth=9, num_channels=64)
         init_weights(dec_)
-        init_weights(disc_)
+        #init_weights(disc_)
         dec_.train()
-        disc_.train()
+        #disc_.train()
         enc_.to(device)
         dec_.to(device)
-        disc_.to(device)
+        #disc_.to(device)
 
     optimizer = torch.optim.Adam(dec_.parameters(), lr=args.lr, weight_decay = .1)
-    opt_D = torch.optim.Adam(disc_.parameters(),lr=args.lr, weight_decay = .1)
+    #opt_D = torch.optim.Adam(disc_.parameters(),lr=args.lr, weight_decay = .1)
     '''
     content_iter = iter(data.DataLoader(
         content_dataset, batch_size=args.batch_size,
@@ -183,7 +183,7 @@ if args.train_model=='drafting':
             cF = enc_(ci)
             sF = enc_(si)
             stylized, adaconv_out, cb_loss = dec_(sF, cF)
-
+            '''
             opt_D.zero_grad()
             set_requires_grad(disc_, True)
             loss_D = disc_.losses(si.detach(),stylized.detach())
@@ -194,8 +194,9 @@ if args.train_model=='drafting':
         set_requires_grad(disc_,False)
 
         with autocast(enabled=ac_enabled):
+            '''
             optimizer.zero_grad()
-            losses = calc_losses(stylized, ci.detach(), si.detach(), adaconv_out, cF, sF, enc_, dec_, disc_, calc_identity=False, disc_loss=True, mdog_losses=False)
+            losses = calc_losses(stylized, ci.detach(), si.detach(), adaconv_out, cF, sF, enc_, dec_, calc_identity=False, disc_loss=False, mdog_losses=False)
             loss_c, loss_s, style_remd, content_relt, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN = losses
             loss = loss_c * args.content_weight + args.style_weight * (loss_s + 3 * style_remd) +\
                         content_relt * 22 + l_identity1*50 + l_identity2 * 1 +\
