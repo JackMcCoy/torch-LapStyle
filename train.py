0acfaf9b4c6999a516f9d0cb32df1b6644b06cc1
JackMcCoy <fdrdavegill@gmail.com>
2021-11-02 15:12:14 -0400

    mdog weight = .5, disc weight = 2.5

--- train.py
+++ train.py
@@ -198,7 +198,7 @@ if args.train_model=='drafting':
             optimizer.zero_grad()
             losses = calc_losses(stylized, ci.detach(), si.detach(), cF, sF, enc_, dec_, disc_, calc_identity=False, disc_loss=True, mdog_losses=True)
             loss_c, loss_s, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN = losses
-            loss = loss_c * args.content_weight + args.style_weight * loss_s + loss_Gp_GAN + mdog
+            loss = loss_c * args.content_weight + args.style_weight * loss_s + loss_Gp_GAN * 2.5 + mdog * .5
             #            content_relt * 25 + l_identity1*50 + l_identity2 * 1 +\
             #            l_identity3* 25 + l_identity4 * .5 + mdog * .33 + loss_Gp_GAN * 5 + cb_loss
         scaler.scale(loss).backward()
