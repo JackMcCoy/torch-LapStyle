428e155638a23998133753b65235dfca348fd195
JackMcCoy <fdrdavegill@gmail.com>
2021-11-09 11:08:39 -0500

    first revision network

--- train.py
+++ train.py
@@ -262,6 +262,7 @@ elif args.train_model=='revision':
         dec_.to(device)
         disc_.to(device)
         rev_.to(device)
+    print(rev_.parameters())
     optimizer = torch.optim.Adam(rev_.parameters(), lr=args.lr)
     opt_D = torch.optim.Adam(disc_.parameters(), lr=args.lr, weight_decay=.1)
     for i in tqdm(range(args.max_iter)):
