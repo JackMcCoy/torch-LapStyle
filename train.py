fec4969c35d54e63c9b27c5f9ea8dd49b05ce0fe
JackMcCoy <fdrdavegill@gmail.com>
2021-11-09 14:55:55 -0500

    AMP

--- train.py
+++ train.py
@@ -306,8 +306,9 @@ elif args.train_model=='revision':
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * 27 + style_remd * 24 + loss_Gp_GAN * 2.5
         if ac_enabled:
-            scaler.scale(loss).backward()
-            scaler.step(optimizer)
+            if i > 0:
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
             scaler.update()
         else:
             loss.backward()
