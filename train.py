92de5a3e3a96c943f3cd2a8e44898ead74920117
JackMcCoy <fdrdavegill@gmail.com>
2021-11-10 04:17:34 -0500

    spectral disc simple

--- train.py
+++ train.py
@@ -253,7 +253,7 @@ elif args.train_model=='revision':
         dec_ = net.DecoderAdaConv()
         dec_.load_state_dict(torch.load(args.load_model))
         rev_ = net.Revisors(levels = args.revision_depth)
-        disc_ = net.SpectralDiscriminator(depth=args.disc_depth, num_channels=16, relgan=True)
+        disc_ = net.SpectralDiscriminator(depth=args.disc_depth, num_channels=16, relgan=False)
         dec_.train(False)
         init_weights(disc_)
         init_weights(rev_)
