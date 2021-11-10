f169639e0e28a93b90074d718eb90213f588cb5d
JackMcCoy <fdrdavegill@gmail.com>
2021-11-09 23:15:08 -0500

    spectral linear

--- net.py
+++ net.py
@@ -31,8 +31,8 @@ def max_singular_value(W, u=None, Ip=1):
         u = torch.normal(mean=0,std=1,shape=(1, W.shape(0)))
     _u = u
     for _ in range(Ip):
-        _v = _l2normalize(_u * W.transpose(1,0), eps=1e-12)
-        _u = _l2normalize(_v * W, eps=1e-12)
+        _v = _l2normalize(torch.matmul(_u, W.transpose(1,0)), eps=1e-12)
+        _u = _l2normalize(torch.matmul(_v, W), eps=1e-12)
     sigma = torch.sum(nn.functional.linear(_u, W.transpose(1, 0)) * _v)
     return sigma, _u
 
@@ -613,7 +613,7 @@ class SpectralDiscriminator(nn.Module):
         self.relu = nn.LeakyReLU(0.1)
         self.ganloss = GANLoss('lsgan')
         self.relgan = relgan
-        self.fc=nn.Sequential(SNLinear(65536, 1),nn.Sigmoid())
+        self.fc=nn.Sequential(nn.Linear(65536, 1),nn.Sigmoid())
 
     def losses(self, real, fake):
         pred_real = self(real)
