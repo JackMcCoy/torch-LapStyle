f1298d9a02ad014f3cddbfe02f5be1bf9556dfb6
JackMcCoy <fdrdavegill@gmail.com>
2021-11-10 05:10:29 -0500

    continue training decoder

--- train.py
+++ train.py
@@ -254,7 +254,7 @@ elif args.train_model=='revision':
         dec_.load_state_dict(torch.load(args.load_model))
         rev_ = net.Revisors(levels = args.revision_depth)
         disc_ = net.SpectralDiscriminator(depth=args.disc_depth, num_channels=16, relgan=False)
-        dec_.train(False)
+        dec_.train()
         init_weights(disc_)
         init_weights(rev_)
         rev_.train()
@@ -264,7 +264,7 @@ elif args.train_model=='revision':
         disc_.to(device)
         rev_.to(device)
     scaler = GradScaler(init_scale=1024, growth_interval=1000)
-    optimizer = torch.optim.Adam(rev_.parameters(), lr=args.lr)
+    optimizer = torch.optim.Adam(list(rev_.parameters())+list(dec_.parameters()), lr=args.lr)
     opt_D = torch.optim.Adam(disc_.parameters(), lr=args.lr, weight_decay=.1)
     for i in tqdm(range(args.max_iter)):
         with autocast(enabled=ac_enabled):
@@ -304,7 +304,7 @@ elif args.train_model=='revision':
             sF = enc_(si[-1])
             losses = calc_losses(rev_stylized, ci[-1].detach(), si[-1].detach(), cF, sF, enc_, dec_, disc_, calc_identity=False, disc_loss=True, mdog_losses=False, content_all_layers=False)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN = losses
-            loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * 22 + style_remd * 22 + loss_Gp_GAN * 2.5
+            loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * 22 + style_remd * 22 + loss_Gp_GAN * 2.5 + cb_loss
         if ac_enabled:
             scaler.scale(loss).backward()
             scaler.step(optimizer)
