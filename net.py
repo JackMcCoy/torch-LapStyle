ea10ed1b845e87b542f70ad42795fa59b1c4d416
JackMcCoy <fdrdavegill@gmail.com>
2021-11-15 08:41:01 -0500

    pred kernel in revision net

--- net.py
+++ net.py
@@ -156,9 +156,9 @@ class RevisionNet(nn.Module):
         out = self.DownBlock(input)
         print(out.shape)
         print(style.shape)
-        out = self.relu(self.adaconv_pre_res(out, style, norm=False))
+        out = self.relu(self.adaconv_pre_res(style, out, norm=False))
         out = self.resblock(out)
-        out = self.relu(self.adaconv_post_res(out, style, norm=False))
+        out = self.relu(self.adaconv_post_res(style, out, norm=False))
         res_block = out.clone()
         out = self.UpBlock(out)
         return out, res_block
