496e7983c9f7878d2e5237c60ba032dc1955e4ad
JackMcCoy <fdrdavegill@gmail.com>
2021-11-20 09:37:18 -0500

    remove detach() call on patch_loss calc

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ Image.MAX_IMAGE_PIXELS = None  # Disable DecompressionBombError
 # Disable OSError: image file is truncated
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-ac_enabled = False
+ac_enabled = True
 
 def train_transform(load_size, crop_size):
     transform_list = [
