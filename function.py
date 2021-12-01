edf53b5b049b021f63d20e043e89514c3e6e52a9
JackMcCoy <fdrdavegill@gmail.com>
2021-11-30 21:36:54 -0500

    jit revisors

--- function.py
+++ function.py
@@ -1,12 +1,12 @@
 import torch
 
 
-def calc_mean_std(feat, eps):
+def calc_mean_std(feat):
     # eps is a small value added to the variance to avoid divide-by-zero.
     size = feat.size()
     assert (len(size) == 4)
     N, C = size[:2]
-    feat_var = feat.view(N, C, -1).var(dim=2) + eps
+    feat_var = feat.view(N, C, -1).var(dim=2) + 1e-5
     feat_std = feat_var.sqrt().view(N, C, 1, 1)
     feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
     return feat_mean, feat_std
