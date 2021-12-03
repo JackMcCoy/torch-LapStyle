13af4b7c6c99e6ea1a8e868512294256ea0aa671
JackMcCoy <fdrdavegill@gmail.com>
2021-12-02 20:07:39 -0500

    use 4_1 style encoding

--- net.py
+++ net.py
@@ -474,7 +474,7 @@ class Style_Guided_Discriminator(nn.Module):
         super(Style_Guided_Discriminator, self).__init__()
         self.head = nn.Sequential(
             nn.Conv2d(3,num_channels,3,stride=1,padding=1, padding_mode='reflect'),
-            nn.BatchNorm2d(num_channels, track_running_stats=False),
+            nn.BatchNorm2d(num_channels),
             nn.LeakyReLU(0.2)
             )
         self.body = nn.ModuleList([])
@@ -483,9 +483,9 @@ class Style_Guided_Discriminator(nn.Module):
 
 
         for i in range(depth - 2):
-            self.body.append(AdaConv(64, 1, s_d = 256, norm=False))
+            self.body.append(AdaConv(512, 8, s_d = self.s_d, norm=False))
             self.norms.append(nn.Sequential(
-                nn.BatchNorm2d(num_channels, track_running_stats=False),
+                nn.BatchNorm2d(num_channels),
                 nn.LeakyReLU(0.2)
             ))
         self.tail = nn.Conv2d(num_channels,
