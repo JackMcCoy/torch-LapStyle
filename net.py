bb83ca69a6dc3c9009c2d49da39e3c35d467dc2e
JackMcCoy <fdrdavegill@gmail.com>
2021-12-03 09:52:40 -0500

    adanorm=true; batchnorm track stats

--- net.py
+++ net.py
@@ -519,6 +519,7 @@ class Style_Guided_Discriminator(nn.Module):
         self.default_cl = torch.Tensor([0]).to(device)
 
     def losses(self, real, fake, style):
+        b, n, h, w = style.shape
         idx = 0
         style = self.style_encoding(style.detach())
         if self.quantize:
