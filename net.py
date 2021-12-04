5f815e478883f84e6b8a1d03558ba52154f94eac
JackMcCoy <fdrdavegill@gmail.com>
2021-12-04 01:50:05 -0500

    jit encoder for drafting

--- net.py
+++ net.py
@@ -312,9 +312,9 @@ class DecoderAdaConv(nn.Module):
             *style_encoder_block(512),
             *style_encoder_block(512)
         )
-        self.s_d = 320
+        self.s_d = 256
         self.style_projection = nn.Sequential(
-            nn.Linear(8192, self.s_d*16)
+            nn.Linear(8192, self.s_d*25)
         )
         self.kernel_1 = AdaConv(512, 8, s_d = self.s_d)
         self.decoder_1 = nn.Sequential(
