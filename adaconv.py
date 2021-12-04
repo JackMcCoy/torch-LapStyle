e3863626d9c2e49c7dedb24c6d0442f6eef047e3
JackMcCoy <fdrdavegill@gmail.com>
2021-12-04 10:24:59 -0500

    leakyrelu

--- adaconv.py
+++ adaconv.py
@@ -42,13 +42,10 @@ class KernelPredictor(nn.Module):
         self.c_out = c_out
         self.c_in = c_in
         self.style_groups = (s_d//p)
-        dw_groups = self.c_out * (self.c_in//self.n_groups)
         self.depthwise_kernel_conv = nn.Sequential(
-            nn.Conv2d(s_d, dw_groups, padding=1,padding_mode='reflect',kernel_size=3, stride=2),
-            nn.LeakyReLU(),
-            nn.Conv2d(dw_groups,dw_groups, padding=1,padding_mode='reflect', kernel_size=3, stride=2),
-            nn.LeakyReLU())
-        self.pointwise_avg_pool = nn.AvgPool2d(12)
+            nn.Conv2d(s_d, self.c_out * (self.c_in//self.n_groups), kernel_size=3, stride=2),
+            nn.ReLU(),)
+        self.pointwise_avg_pool = nn.AvgPool2d(5)
         self.pw_cn_kn = nn.Conv2d(s_d, self.c_out*(self.c_out//self.n_groups), 1)
         self.pw_cn_bias = nn.Conv2d(s_d, c_out, 1)
         self.apply(self._init_weights)
--- net.py
+++ net.py
@@ -309,11 +309,12 @@ class DecoderAdaConv(nn.Module):
 
         self.style_encoding = nn.Sequential(
             *style_encoder_block(512),
+            *style_encoder_block(512),
             *style_encoder_block(512)
         )
         self.s_d = 256
         self.style_projection = nn.Sequential(
-            nn.Linear(8192, self.s_d*12*12)
+            nn.Linear(8192, self.s_d*25)
         )
         self.kernel_1 = AdaConv(512, 8, s_d = self.s_d)
         self.decoder_1 = nn.Sequential(
@@ -342,7 +343,7 @@ class DecoderAdaConv(nn.Module):
         adaconv_out = {}
         style = self.style_encoding(sF['r4_1'].detach())
         style = self.style_projection(style.flatten(1))
-        style = style.reshape(b, self.s_d, 12, 12)
+        style = style.reshape(b, self.s_d, 5, 5)
         adaconv_out['r4_1'] = self.kernel_1(style, cF['r4_1'], norm=True)
         x = self.decoder_1(adaconv_out['r4_1'])
         x = self.upsample(x)
