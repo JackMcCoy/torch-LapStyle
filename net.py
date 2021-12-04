a74ea74714091c7e41af39fbcb8f8788132146dc
JackMcCoy <fdrdavegill@gmail.com>
2021-12-04 15:10:27 -0500

    leakyrelu

--- net.py
+++ net.py
@@ -313,7 +313,7 @@ class DecoderAdaConv(nn.Module):
         )
         self.s_d = 512
         self.style_projection = nn.Sequential(
-            nn.Linear(8192, self.s_d*16)
+            nn.Linear(8192, self.s_d*25)
         )
         self.kernel_1 = AdaConv(512, 8, s_d = self.s_d)
         self.decoder_1 = nn.Sequential(
@@ -342,7 +342,7 @@ class DecoderAdaConv(nn.Module):
         adaconv_out = {}
         style = self.style_encoding(sF['r4_1'].detach())
         style = self.style_projection(style.flatten(1))
-        style = style.reshape(b, self.s_d, 4, 4)
+        style = style.reshape(b, self.s_d, 5, 5)
         adaconv_out['r4_1'] = self.kernel_1(style, cF['r4_1'], norm=True)
         x = self.decoder_1(adaconv_out['r4_1'])
         x = self.upsample(x)
