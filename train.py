1f14c861020f4a6c97accbea53580a15d8b51c51
JackMcCoy <fdrdavegill@gmail.com>
2021-12-05 16:00:20 -0500

    jit dec_

--- train.py
+++ train.py
@@ -278,8 +278,8 @@ elif args.train_model=='revision':
 
     random_crop = transforms.RandomCrop(512)
     with autocast(enabled=ac_enabled):
-        enc_ = torch.jit.trace(build_enc(vgg),(torch.rand((args.batch_size,3,128,128))), strict=False)
-        dec_ = net.DecoderAdaConv()
+        enc_ = torch.jit.trace(build_enc(vgg),(torch.rand((args.batch_size,3,256,256))), strict=False)
+        dec_ = torch.jit.trace(net.DecoderAdaConv(), (torch.rand((args.batch_size,3,256,256)), torch.rand((args.batch_size,3,256,256)), torch.rand((args.batch_size,128,4,4))))
         dec_.load_state_dict(torch.load(args.load_model))
         disc_quant = True if args.disc_quantization == 1 else False
         set_requires_grad(dec_, False)
@@ -300,11 +300,11 @@ elif args.train_model=='revision':
         else:
             rev_state = None
         rev_ = build_rev(args.revision_depth, rev_state)#,(torch.rand(args.batch_size,3,128,128).to(device),torch.rand(args.batch_size,3,args.crop_size,args.crop_size).to(device),torch.rand(args.batch_size,320,4,4).to(device)), check_trace=False)
-        disc_inputs = {'forward': (
-        torch.rand(args.batch_size, 3, 256, 256).to(device), torch.rand(args.batch_size, 128, 4, 4).to(device)),
-        'losses': (torch.rand(args.batch_size, 3, 512, 512).to(device), torch.rand(args.batch_size, 3, 256, 256).to(device), torch.rand(args.batch_size,128,4,4).to(device)),
-        'get_ganloss': (torch.rand(args.batch_size,1,256,256).to(device),torch.Tensor([True]).to(device))}
-        disc_ = torch.jit.trace_module(build_disc(disc_state, disc_quant), disc_inputs)
+        #disc_inputs = {'forward': (
+        #torch.rand(args.batch_size, 3, 256, 256).to(device), torch.rand(args.batch_size, 320, 4, 4).to(device)),
+        #'losses': (torch.rand(args.batch_size, 3, 512, 512).to(device), torch.rand(args.batch_size, 3, 256, 256).to(device), torch.rand(args.batch_size,320,4,4).to(device)),
+        #'get_ganloss': (torch.rand(args.batch_size,1,256,256).to(device),torch.Tensor([True]).to(device))}
+        disc_ = build_disc(disc_state, disc_quant)#, disc_inputs)
         disc_.train()
         rev_.train()
         dec_.eval()
