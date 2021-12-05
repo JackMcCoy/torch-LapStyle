831d032dfc4b1116f28445473bf5cc5e9ed44a66
JackMcCoy <fdrdavegill@gmail.com>
2021-12-05 14:30:57 -0500

    patch for 256x256 decoder output

--- net.py
+++ net.py
@@ -335,7 +335,7 @@ class DecoderAdaConv(nn.Module):
         adaconv_out = {}
         style = self.style_encoding(sF['r4_1'].detach())
         style = self.style_projection(style.flatten(1))
-        style = style.reshape(b, self.s_d, 4, 4)
+        style = style.reshape(b, self.s_d, 5, 5)
         adaconv_out['r4_1'] = self.kernel_1(style, cF['r4_1'], norm=True)
         x = self.decoder_1(adaconv_out['r4_1'])
         x = self.upsample(x)
