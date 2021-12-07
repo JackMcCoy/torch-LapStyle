33b4c79c7230ae676ba9d6a47ab356d06b3e06c3
JackMcCoy <fdrdavegill@gmail.com>
2021-12-07 09:08:59 -0500

    loss on max

--- net.py
+++ net.py
@@ -662,6 +662,7 @@ class SpectralDiscriminator(nn.Module):
     def losses(self, real, fake):
         idx = 0
         pred_fake = self(fake)
+        loss_Ds = []
         if self.relgan:
             pred_fake = pred_fake.view(-1)
         else:
@@ -671,24 +672,17 @@ class SpectralDiscriminator(nn.Module):
                 pred_real = self(j)
                 if self.relgan:
                     pred_real = pred_real.view(-1)
-                    if idx == 0:
-                        loss_D = (
-                                torch.mean((pred_real - torch.mean(pred_fake) - 1) ** 2) +
-                                torch.mean((pred_fake - torch.mean(pred_real) + 1) ** 2)
-                        )
-                    else:
-                        loss_D += (
-                                torch.mean((pred_real - torch.mean(pred_fake) - 1) ** 2) +
-                                torch.mean((pred_fake - torch.mean(pred_real) + 1) ** 2)
-                        ).data
+                    loss_Ds.append((
+                            torch.mean((pred_real - torch.mean(pred_fake) - 1) ** 2) +
+                            torch.mean((pred_fake - torch.mean(pred_real) + 1) ** 2)).unsqueeze(0)
+                    )
+
                 else:
                     loss_D_real = self.ganloss(pred_real, self.true)
-                    if idx == 0:
-                        loss_D = ((loss_D_real + loss_D_fake) * 0.5)
-                    else:
-                        loss_D += ((loss_D_real + loss_D_fake) * 0.5).data
+                    loss_Ds.append(((loss_D_real + loss_D_fake) * 0.5).unsqueeze(0))
                 idx += 1
-        return loss_D
+        loss_Ds, ind = torch.cat(loss_Ds, dim=0).max(dim=0)
+        return loss_Ds
 
     def forward(self, x):
         x = self.head(x)
