00ca5323b19a725b8ce96df4e46554b275b1e2c7
JackMcCoy <fdrdavegill@gmail.com>
2021-12-10 04:39:50 -0500

    no_grad for encoding

--- train.py
+++ train.py
@@ -124,10 +124,10 @@ parser.add_argument('--patch_loss', type=float, default=1)
 parser.add_argument('--gan_loss', type=float, default=2.5)
 parser.add_argument('--fp16', type=int, default=0)
 
+args = parser.parse_args()
+
 if args.fp16 ==1:
     ac_enabled=True
-
-args = parser.parse_args()
 
 device = torch.device('cuda')
 save_dir = Path(args.save_dir)
