6aefc16f9cc36f54e8293aa9ea116147d7d1c5bf
JackMcCoy <fdrdavegill@gmail.com>
2021-12-10 22:00:26 -0500

    remove loss calc from spectral gan and jit trace discriminator

--- net.py
+++ net.py
@@ -656,7 +656,7 @@ class SpectralDiscriminator(nn.Module):
     def __init__(self, depth=5, num_channels=64, relgan=True, batch_size=5):
         super(SpectralDiscriminator, self).__init__()
         self.spectral_gan = nn.Sequential(OptimizedBlock(3, num_channels, 3, 1, downsample=True),
-                                          *[SpectralResBlock(ch*2**i, ch*2**(i+1), 5, 2, downsample=True) for i, ch in range(depth-2)],
+                                          *[SpectralResBlock(ch*2**i, ch*2**(i+1), 5, 2, downsample=True) for i in range(depth-2)],
                                           SpectralResBlock(ch, ch, 3, 1, downsample=False),
                                           nn.ReLU())
         self.relgan = relgan
