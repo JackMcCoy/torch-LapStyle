eb995277232c01ed31a09b6599694b5ecd15cc51
JackMcCoy <fdrdavegill@gmail.com>
2021-12-10 20:49:39 -0500

    clone si_cropped

--- train.py
+++ train.py
@@ -277,7 +277,7 @@ elif args.train_model=='revision':
                 i.train(False)
         return rev
     def build_disc(disc_state, disc_quant):
-        disc=net.SpectralDiscriminator(depth=args.disc_depth, num_channels=args.disc_channels, relgan=False).to(device)
+        disc=net.SpectralDiscriminator(depth=args.disc_depth, num_channels=args.disc_channels, relgan=False, batch_size = args.batch_size).to(device)
         if not disc_state is None:
             disc.load_state_dict(torch.load(new_path_func('discriminator_')), strict=False)
         else:
