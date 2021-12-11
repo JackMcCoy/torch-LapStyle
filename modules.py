33a92419b34c9f1d699e16600e30f321d514eb37
JackMcCoy <fdrdavegill@gmail.com>
2021-12-11 18:10:30 -0500

    normal for riemann noise

--- modules.py
+++ modules.py
@@ -45,7 +45,7 @@ class RiemannNoise(nn.Module):
         sd = self.A * s + self.b
         s = self.alpha*sd + (1 - self.alpha) + 1
         sigma = s / torch.linalg.vector_norm(s)
-        out = self.r * sigma * x + self.r * sigma * torch.rand(x.shape).to(torch.device('cuda'))
+        out = self.r * sigma * x + self.r * sigma * torch.normal(torch.zeros(x.shape),torch.ones(x.shape)).to(torch.device('cuda'))
         return out
 
 
