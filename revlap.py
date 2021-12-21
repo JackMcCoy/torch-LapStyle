32805c9b09d9a6adbc6968d18e24784ed7303287
JackMcCoy <fdrdavegill@gmail.com>
2021-12-21 18:07:56 -0500

    debugging

--- revlap.py
+++ revlap.py
@@ -125,7 +125,7 @@ class RevisionNet(nn.Module):
         for i, c in zip(x.chunk(2,dim=2), ci.chunk(2,dim=2)):
             for j, c2 in zip(i.chunk(2,dim=3), c.chunk(2,dim=3)):
                 if not base_case:
-                    holder.append(self.recursive_controller(j, c2, x, enc_))
+                    holder.append(self.recursive_controller(j, c2, thumbnail, enc_))
                 else:
                     holder.append(self.generator(j, c2, thumbnail_style))
         holder = torch.cat((torch.cat([holder[0],holder[2]],dim=2),
