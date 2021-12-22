df3996a23ed1ed8b0a243f05e0322e713d9fe14a
JackMcCoy <fdrdavegill@gmail.com>
2021-12-22 05:41:26 -0500

    identity loss on first net

--- train.py
+++ train.py
@@ -525,11 +525,11 @@ elif args.train_model == 'revlap':
             scaled_stylized=F.interpolate(rev_stylized,size=256,mode='bicubic')
 
             loss_small = calc_losses(stylized, ci[0], si[0], cF, enc_, dec_, None, disc_,
-                                 calc_content_style=args.content_style_loss, calc_identity=False, disc_loss=False,
+                                 calc_content_style=args.content_style_loss, calc_identity=True, disc_loss=False,
                                  mdog_losses=args.mdog_loss, content_all_layers=False, remd_loss=remd_loss,
                                  patch_loss=False, GANLoss=False, sF=sF, split_style=args.split_style)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = loss_small
-            loss_small = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
+            loss_small = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog + l_identity1 *50 + l_identity2 + l_identity3*50 + l_identity4
 
             losses_scaled = calc_losses(scaled_stylized, ci[0], si[0], cF, enc_, dec_, None, disc_,
                                  calc_content_style=args.content_style_loss, calc_identity=False, disc_loss=False,
