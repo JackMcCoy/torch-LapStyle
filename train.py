b8522cd35c67d7efbe8988007942e22147490d09
JackMcCoy <fdrdavegill@gmail.com>
2021-12-22 00:10:22 -0500

    lower closeup loss weight

--- train.py
+++ train.py
@@ -486,7 +486,7 @@ elif args.train_model == 'revlap':
     opt_D = torch.optim.AdamW(disc_.parameters(), lr=args.disc_lr)
     for i in tqdm(range(args.max_iter)):
         adjust_learning_rate(optimizer, i//4, args)
-        adjust_learning_rate(opt_D, i//4, args, disc=True)
+        adjust_learning_rate(opt_D, i, args, disc=True)
         with autocast(enabled=ac_enabled):
             ci = next(content_iter).to(device)
             si = next(style_iter).to(device)
@@ -509,12 +509,12 @@ elif args.train_model == 'revlap':
             loss_D = calc_GAN_loss(si_cropped.detach(), stylized_crop.clone().detach(), disc_, ganloss)
         if ac_enabled:
             d_scaler.scale(loss_D).backward()
-            if i + 1 % 4 == 0:
+            if i + 1 % 1 == 0:
                 d_scaler.step(opt_D)
                 d_scaler.update()
         else:
             loss_D.backward()
-            if i + 1 % 4 == 0:
+            if i + 1 % 1 == 0:
                 opt_D.step()
         set_requires_grad(disc_, False)
 
