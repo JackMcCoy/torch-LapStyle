c3cc4298040bf22b3a3d81fb7e67e191d505e330
JackMcCoy <fdrdavegill@gmail.com>
2021-12-22 05:07:38 -0500

    accumulate 10 steps grad

--- train.py
+++ train.py
@@ -486,9 +486,9 @@ elif args.train_model == 'revlap':
     optimizer = torch.optim.AdamW(rev_.parameters(), lr=args.lr)
     opt_D = torch.optim.AdamW(disc_.parameters(), lr=args.disc_lr)
     for i in tqdm(range(args.max_iter)):
-        adjust_learning_rate(optimizer, i//4, args)
-        adjust_learning_rate(dec_optimizer, i // 4, args)
-        adjust_learning_rate(opt_D, i, args, disc=True)
+        adjust_learning_rate(optimizer, i//10, args)
+        adjust_learning_rate(dec_optimizer, i // 10, args)
+        adjust_learning_rate(opt_D, i//10, args, disc=True)
         with autocast(enabled=ac_enabled):
             ci = next(content_iter).to(device)
             si = next(style_iter).to(device)
@@ -496,11 +496,10 @@ elif args.train_model == 'revlap':
             si = [F.interpolate(si, size=256, mode='bicubic', align_corners=True), si]
             cF = enc_(ci[0])
             sF = enc_(si[0])
-            opt_D.zero_grad(set_to_none=True)
-            dec_optimizer.zero_grad(set_to_none=True)
+
             stylized, style = dec_(sF, cF)
 
-            optimizer.zero_grad(set_to_none=True)
+
             rev_stylized = rev_(stylized, enc_, ci[-1].detach())
             si_cropped = random_crop(si[-1])
             stylized_crop = rev_stylized[:,:,-256:,-256:]
@@ -511,13 +510,15 @@ elif args.train_model == 'revlap':
             loss_D = calc_GAN_loss(si_cropped.detach(), stylized_crop.clone().detach(), disc_, ganloss)
         if ac_enabled:
             d_scaler.scale(loss_D).backward()
-            if i + 1 % 1 == 0:
+            if i + 1 % 10 == 0:
                 d_scaler.step(opt_D)
                 d_scaler.update()
+                opt_D.zero_grad(set_to_none=True)
         else:
             loss_D.backward()
-            if i + 1 % 1 == 0:
+            if i + 1 % 10 == 0:
                 opt_D.step()
+                opt_D.zero_grad(set_to_none=True)
         set_requires_grad(disc_, False)
 
         with autocast(enabled=ac_enabled):
@@ -552,15 +553,19 @@ elif args.train_model == 'revlap':
 
         if ac_enabled:
             scaler.scale(loss).backward()
-            if i + 1 % 4 == 0:
+            if i + 1 % 10 == 0:
                 scaler.step(dec_optimizer)
                 scaler.step(optimizer)
                 scaler.update()
+                optimizer.zero_grad(set_to_none=True)
+                dec_optimizer.zero_grad(set_to_none=True)
         else:
             loss.backward()
-            if i + 1 % 4 == 0:
+            if i + 1 % 10 == 0:
                 dec_optimizer.step()
                 optimizer.step()
+                optimizer.zero_grad(set_to_none=True)
+                dec_optimizer.zero_grad(set_to_none=True)
 
         if (i + 1) % 10 == 0:
             loss_dict = {}
