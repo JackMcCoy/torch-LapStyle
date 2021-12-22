edd8380196f8f89b7eb50b8cf39fdccd2fe0db4a
JackMcCoy <fdrdavegill@gmail.com>
2021-12-21 22:59:36 -0500

    lower closeup loss weight

--- train.py
+++ train.py
@@ -539,7 +539,7 @@ elif args.train_model == 'revlap':
                                  patch_loss=True, GANLoss=ganloss, sF=sF, split_style=args.split_style)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
-            loss = loss*.5 + loss_small
+            loss = loss*(1/16) + loss_small
 
         if ac_enabled:
             scaler.scale(loss).backward()
