4195d0dca9785bd0058f959688b6e6c6baca7560
JackMcCoy <fdrdavegill@gmail.com>
2021-12-23 02:47:21 -0500

    emd c code

--- include/cuda_helper.h
+++
@@ -1,4 +0,0 @@
-#ifndef CUDA_HELPER_H_
-#define CUDA_HELPER_H_
-
-#endif
--- setup_emd.py
+++ setup_emd.py
@@ -1,29 +1,27 @@
+"""Setup extension
+
+Notes:
+    If extra_compile_args is provided, you need to provide different instances for different extensions.
+    Refer to https://github.com/pytorch/pytorch/issues/20169
+
+"""
+
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 
 setup(
-	name='PyTorch EMD',
-	version='0.0',
-	author='Marc Eder',
-	author_email='meder@cs.unc.edu',
-	description='A PyTorch module for the earth mover\'s distance loss',
-	ext_package='_emd_ext',
-	ext_modules=[
-		CUDAExtension(
-			name='_emd',
-			sources=[
-				'src/emd.cpp',
-				'src/cuda/emd.cu',
-			],
-			include_dirs=['include'],
-		),
-	],
-	packages=[
-		'emd',
-	],
-	package_dir={
-		'emd' : 'emd_layer'
-	},
-	cmdclass={'build_ext': BuildExtension},
-)
+    name='emd_ext',
+    ext_modules=[
+        CUDAExtension(
+            name='emd_cuda',
+            sources=[
+                'cuda/emd.cpp',
+                'cuda/emd_kernel.cu',
+            ],
+            extra_compile_args={'cxx': ['-g'], 'nvcc': ['-O2']}
+        ),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
