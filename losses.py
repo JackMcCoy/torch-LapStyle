aea09503f00f2d068491d6f04ad31ccd216705ae
JackMcCoy <fdrdavegill@gmail.com>
2021-12-23 17:17:59 -0500

    no linear layer in disc

--- losses.py
+++ losses.py
@@ -130,7 +130,7 @@ class GANLoss(nn.Module):
         #c = int(conv_ch*2**(depth-2))
         #h = int(256/2**(depth-1))
         c = 3
-        h = 256
+        h = 16
         self.target_real = torch.ones(batch_size,c,h,h).to(torch.device('cuda'))
         self.target_fake = torch.zeros(batch_size,c,h,h).to(torch.device('cuda'))
         self.loss_weight = loss_weight
