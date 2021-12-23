4d0161dabbc39e30341e35c94fdac4f04483a9f4
JackMcCoy <fdrdavegill@gmail.com>
2021-12-23 13:03:41 -0500

    no loss on

--- train.py
+++ train.py
@@ -546,15 +546,7 @@ elif args.train_model == 'revlap':
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
             loss = loss + losses_scaled + loss_small*.5
-            elif loss_c <= .6:
-                rev_start = True
-                print('=========== REV START =============')
-                optimizer.zero_grad()
-                content_iter.batch_size =2
-                style_iter.batch_size = 2
-                loss = loss_small
-            else:
-                loss = loss_small
+
         if ac_enabled:
             scaler.scale(loss).backward()
             if i+1 % 3 == 0:
