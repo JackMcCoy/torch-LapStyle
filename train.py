72aac3b60ac612ee9c84a21593cd910ff40dd89a
JackMcCoy <fdrdavegill@gmail.com>
2021-12-23 04:11:07 -0500

    no accumulation

--- train.py
+++ train.py
@@ -513,8 +513,6 @@ elif args.train_model == 'revlap':
                 loss_D = calc_GAN_loss(si_cropped.detach(), stylized_crop.clone().detach(), disc_, ganloss)
             if ac_enabled:
                 d_scaler.scale(loss_D).backward()
-                d_scaler.unscale_(opt_D)
-                torch.nn.utils.clip_grad_norm_(opt_D.parameters(), 1.0, error_if_nonfinite=True)
                 d_scaler.step(opt_D)
                 d_scaler.update()
             else:
