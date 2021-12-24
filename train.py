3742d089047b58528ad6355eda8c45222a572630
JackMcCoy <fdrdavegill@gmail.com>
2021-12-23 21:17:15 -0500

    debug grad

--- train.py
+++ train.py
@@ -552,8 +552,8 @@ elif args.train_model == 'revlap':
             if i+1 % args.accumulation_steps == 0:
                 scaler.unscale_(optimizer)
                 torch.nn.utils.clip_grad_norm_(rev_.parameters(), 1.0, error_if_nonfinite=False)
-                for name, i in rev_.named_parameters():
-                    print(f'{name} {str(i.grad)}')
+                for i in rev_.parameters():
+                    print(f'{str(i.grad)}')
 
                 scaler.step(optimizer)
                 scaler.update()
