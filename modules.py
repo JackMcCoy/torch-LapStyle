7683b3ab840587e2aa301f320846e92b3347601f
JackMcCoy <fdrdavegill@gmail.com>
2021-12-25 02:48:14 -0500

    style encoding size=256, kernel size = 3,return some riemann noise layers

--- modules.py
+++ modules.py
@@ -29,9 +29,9 @@ class ResBlock(nn.Module):
 
 
 class RiemannNoise(nn.Module):
-    def __init__(self, size):
+    def __init__(self, size:int):
         super(RiemannNoise, self).__init__()
-        wn = torch.empty(1,int(size.item()),int(size.item())).to(torch.device('cuda'))
+        wn = torch.empty(1,size,size).to(torch.device('cuda'))
         w = torch.empty(1, ).to(torch.device('cuda'))
         self.params = nn.ParameterList([nn.Parameter(nn.init.normal_(wn)).to(torch.device('cuda')),
             nn.Parameter(nn.init.uniform_(w)).to(torch.device('cuda')),
