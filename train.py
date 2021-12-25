9ca31f961e4d7c8e86117c587e8673ece61fbfe5
JackMcCoy <fdrdavegill@gmail.com>
2021-12-25 04:36:49 -0500

    style encoding size=256, kernel size = 3,return some riemann noise layers

--- train.py
+++ train.py
@@ -552,7 +552,7 @@ def revlap_train():
             losses_scaled = calc_losses(scale_stylized, ci[0], si[0], cF, enc_, dec_, None, disc_,
                                  calc_identity=False, disc_loss=False,
                                  mdog_losses=args.mdog_loss, content_all_layers=False, remd_loss=remd_loss,
-                                 patch_loss=False, GANLoss=None, sF=sF, split_style=args.split_style)
+                                 patch_loss=False, sF=sF, split_style=args.split_style)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses_scaled
             losses_scaled = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss + mdog
 
@@ -562,9 +562,9 @@ def revlap_train():
             patch_feats = enc_(F.interpolate(stylized[:,:,-128:,-128:],size=256,mode='bicubic'))
 
             losses = calc_losses(stylized_crop, ci_patch, si_cropped, cF2, enc_, dec_, patch_feats, disc_,
-                                 calc_content_style=args.content_style_loss, calc_identity=False, disc_loss=True,
+                                 calc_identity=False, disc_loss=True,
                                  mdog_losses=args.mdog_loss, content_all_layers=False, remd_loss=remd_loss,
-                                 patch_loss=True, GANLoss=ganloss, sF=sF2, split_style=args.split_style)
+                                 patch_loss=True, sF=sF2, split_style=args.split_style)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
             loss = loss + losses_scaled
