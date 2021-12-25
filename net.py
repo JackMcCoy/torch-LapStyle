e106b46cbf38bb9d3896da9d70c591ef3574455b
JackMcCoy <fdrdavegill@gmail.com>
2021-12-25 18:23:54 -0500

    no noise layer

--- net.py
+++ net.py
@@ -368,15 +368,12 @@ class DecoderAdaConv(nn.Module):
         self.style_projection = nn.Sequential(
             nn.Linear(8192, self.s_d*16)
         )
-        self.style_noise = RiemannNoise(4)
         self.kernel_1 = AdaConv(512, 8, batch_size, s_d = self.s_d)
         self.decoder_1 = nn.Sequential(
-            RiemannNoise(32),
             ResBlock(512),
             ConvBlock(512, 256))
         self.kernel_2 = AdaConv(256, 4, batch_size, s_d = self.s_d)
         self.decoder_2 = nn.Sequential(
-            RiemannNoise(64),
             ResBlock(256),
             ConvBlock(256, 128)
         )
