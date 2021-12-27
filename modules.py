3274ef5892b9e4d7cea4f5023e44ab8d648fa476
JackMcCoy <fdrdavegill@gmail.com>
2021-12-26 21:19:39 -0500

    float32 for net weights in drafting

--- modules.py
+++ modules.py
@@ -51,7 +51,7 @@ class RiemannNoise(nn.Module):
         mu = x.sum(1, keepdim=True)
         mu_mean = mu.sum(dim=(2,3),keepdim=True)/(h*w)
         s = mu - mu_mean
-        s, _ = s / torch.abs(s).max(dim=1)
+        s = s / torch.abs(s).max(dim=1)[0]
         sd = A * s + b
         s = alpha*sd + (1 - alpha)*self.all_one
         sigma = s / torch.linalg.vector_norm(s, dim=1)
