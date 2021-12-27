fcf6f61a39ab8ff048ce79751a0d0efc30dac583
JackMcCoy <fdrdavegill@gmail.com>
2021-12-26 23:21:03 -0500

    matmul in reinmann noise

--- modules.py
+++ modules.py
@@ -50,10 +50,10 @@ class RiemannNoise(nn.Module):
         mu = x.sum(1, keepdim=True)
         mu_mean = mu.sum(dim=(2,3),keepdim=True)/(h*w)
         s = mu - mu_mean
-        s = s / torch.abs(s).max(dim=1)[0]
+        s = s / torch.abs(s).max()
         sd = A * s + b
         s = alpha*sd + (1 - alpha)*self.all_one
-        sigma = s / torch.linalg.vector_norm(s,dim=1)
+        sigma = s / torch.linalg.vector_norm(s)
         out = torch.matmul(r,sigma) * x + torch.matmul(r,sigma) * self.noise
         return out
 
