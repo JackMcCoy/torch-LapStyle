d71a18aefc88a3b1fd578da26b4066d9209afdff
JackMcCoy <fdrdavegill@gmail.com>
2021-12-27 12:25:00 -0500

    no direct style noise

--- modules.py
+++ modules.py
@@ -39,12 +39,9 @@ class RiemannNoise(nn.Module):
             nn.Parameter(nn.init.uniform_(w)).to(torch.device('cuda')),
             nn.Parameter(nn.init.uniform_(w)).to(torch.device('cuda')),
             nn.Parameter(nn.init.uniform_(wn)).to(torch.device('cuda'))])
-        self.zero_holder = torch.zeros(1,channels,size,size,device=torch.device('cuda:0'))
+        self.noise = torch.zeros(1,channels,size,size,device=torch.device('cuda:0')).normal_()
         self.all_one = torch.ones(1, size,size,device=torch.device('cuda:0'))
         self.size=size
-
-    def set_random(self):
-        self.noise = self.zero_holder.normal_()
 
     @torch.jit.ignore
     def forward(self, x):
