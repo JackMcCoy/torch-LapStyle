91026dd3373095b585c81d8a80bd3130ce80d10e
JackMcCoy <fdrdavegill@gmail.com>
2021-12-26 21:30:29 -0500

    float32 for net weights in drafting

--- net.py
+++ net.py
@@ -388,7 +388,7 @@ class DecoderAdaConv(nn.Module):
         )
         self.kernel_4 = AdaConv(64, 1, batch_size, s_d = self.s_d)
         self.decoder_4 = nn.Sequential(
-            RiemannNoise(256)),
+            RiemannNoise(256),
             ConvBlock(64, 64),
             nn.ReflectionPad2d((1, 1, 1, 1)),
             nn.Conv2d(64, 3, kernel_size=3)
