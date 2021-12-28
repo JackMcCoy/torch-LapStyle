8d29783caa65fb0527f103747463340398ad33c2
JackMcCoy <fdrdavegill@gmail.com>
2021-12-28 11:37:48 -0500

    debugging

--- losses.py
+++ losses.py
@@ -218,7 +218,7 @@ def calc_mean_std(feat, eps=1e-8):
     assert (len(size) == 4)
     N, C = size[:2]
     feat_var = feat.reshape([N, C, -1])
-    feat_var = torch.var(feat_var, dim = 2) + eps
+    feat_var = torch.var(feat_var, dim = 2, unbiased=True) + eps
     feat_std = torch.sqrt(feat_var)
     feat_std = feat_std.reshape([N, C, 1, 1])
     feat_mean = feat.reshape([N, C, -1])
