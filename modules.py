417c5db60d156adadd41101d4d30188c8ae505f7
JackMcCoy <fdrdavegill@gmail.com>
2021-12-28 22:39:05 -0500

    identity losses

--- modules.py
+++ modules.py
@@ -40,6 +40,7 @@ class RiemannNoise(nn.Module):
             nn.Parameter(nn.init.normal_(torch.ones(size,size))),
             nn.Parameter(nn.init.constant_(torch.ones(1, ),.5)),
             nn.Parameter(nn.init.constant_(torch.ones(1,), .5)),
+            nn.Parameter(nn.init.constant_(torch.ones(1, ), .5)),
             nn.Parameter(nn.init.constant_(torch.ones(channels,1,1), 0))])
         self.noise = torch.zeros(1,device=torch.device('cuda:0'))
         self.size=size
@@ -52,7 +53,7 @@ class RiemannNoise(nn.Module):
     def forward(self, x):
         #self.cuda_states = torch.utils.checkpoint.get_device_states(x)
         N, c, h, w = x.shape
-        A, b, alpha,r,b2 = self.params
+        A, b, alpha,r, w, b2 = self.params
         s,_ = torch.max(-x, dim=1, keepdim=True)
         s = s - s.mean(dim=(2,3),keepdim=True)
         s_max = torch.abs(s).amax(dim=(2,3), keepdim=True)
@@ -65,7 +66,7 @@ class RiemannNoise(nn.Module):
             torch.mean(torch.square(sp_att_mask), axis=(2, 3), keepdims=True) + 1e-8)
         sp_att_mask = r * sp_att_mask
         x2 = x * sp_att_mask
-        x2 = x2 + (sp_att_mask * self.noise.repeat(*x.size()).normal_())
+        x2 = x2 + (sp_att_mask * (self.noise.repeat(*x.size()).normal_()*w))
         # bias and activation
         x = x + x2
         x = x + b2
