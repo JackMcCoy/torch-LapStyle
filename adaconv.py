e77831dc17b02fa7b4f292a5dd27a2cda0f15929
JackMcCoy <fdrdavegill@gmail.com>
2021-12-30 23:54:25 -0500

    leakyrelu in adaconv kernel predictor

--- adaconv.py
+++ adaconv.py
@@ -14,14 +14,14 @@ class AdaConv(nn.Module):
         self.pad = nn.ReflectionPad2d((1, 1, 1, 1))
         self.depthwise_kernel_conv = nn.Sequential(
             nn.Conv2d(s_d, self.c_out * (self.c_in//self.n_groups), kernel_size=2),
-            nn.ReLU())
+            nn.LeakyReLU())
         self.pointwise_avg_pool = nn.AvgPool2d(4)
         self.pw_cn_kn = nn.Sequential(
             nn.Conv2d(s_d, self.c_out*(self.c_out//self.n_groups), kernel_size=1),
-            nn.ReLU())
+            nn.LeakyReLU())
         self.pw_cn_bias = nn.Sequential(
             nn.Conv2d(s_d, self.c_out, kernel_size=1),
-            nn.ReLU())
+            nn.LeakyReLU())
         self.apply(self._init_weights)
 
     @staticmethod
