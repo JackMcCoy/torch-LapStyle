bf0b401f21414582fbd75313225e428bc272e409
JackMcCoy <fdrdavegill@gmail.com>
2021-12-31 07:11:11 -0500

    noise in conv block, with seperate bias layer

--- modules.py
+++ modules.py
@@ -104,7 +104,7 @@ class SpectralResBlock(nn.Module):
 class Bias(nn.Module):
     def __init__(self, channels):
         super(Bias, self).__init__()
-        self.bias = nn.Parameter(nn.init.normal_(torch.ones(channels,),.5))
+        self.bias = nn.Parameter(nn.init.normal_(torch.ones(channels,1,1),.5))
     def forward(self, x):
         x = x+self.bias
         return x
