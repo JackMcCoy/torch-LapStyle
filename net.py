b1b2e39e37df43f5ea7649c4ea58960c2154a9c0
JackMcCoy <fdrdavegill@gmail.com>
2021-12-31 07:20:03 -0500

    noise in conv block, with seperate bias layer

--- net.py
+++ net.py
@@ -373,7 +373,7 @@ class DecoderAdaConv(nn.Module):
         self.kernel_1 = AdaConv(512, 8, batch_size, s_d = self.s_d)
         self.decoder_1 = nn.Sequential(
             ResBlock(512),
-            ConvBlock(512, 256),
+            ConvBlock(512, 256, noise=32),
             nn.ConvTranspose2d(256, 256, 2, 2, 0, 0)
             )
         self.kernel_2 = AdaConv(256, 4, batch_size, s_d = self.s_d)
