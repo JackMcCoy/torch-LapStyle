6d559b78ae3629dc2745b6998758bbedb2d7a3a0
JackMcCoy <fdrdavegill@gmail.com>
2022-01-01 04:51:19 -0500

    debugging

--- train.py
+++ train.py
@@ -440,7 +440,7 @@ def revision_train():
                 if idx != 0:
                     loss = loss + (loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog)
                 else:
-                    loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
+                    loss = (loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog)*2
 
         if ac_enabled:
             scaler.scale(loss).backward()
