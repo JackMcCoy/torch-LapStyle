7accd94dc8e9c7f874ef1f2fdd24f52a51433839
JackMcCoy <fdrdavegill@gmail.com>
2022-01-01 03:19:32 -0500

    debugging

--- train.py
+++ train.py
@@ -364,11 +364,11 @@ def revision_train():
     remd_loss = True if args.remd_loss==1 else False
     scaler = GradScaler(init_scale=128)
     d_scaler = GradScaler(init_scale=128)
-    optimizers = []
-    #for i in rev_.layers:
-    #    optimizers.append(torch.optim.AdamW(list(i.parameters()), lr=args.lr))
-    optimizers.append(torch.optim.AdamW(list(rev_.parameters())+list(dec_.parameters()), lr=args.lr))
-    opt_D = torch.optim.SGD(disc_.parameters(), lr=args.disc_lr, momentum = .9)
+    optimizers = [torch.optim.AdamW(list(dec_.parameters()), lr=args.lr)]
+    for i in rev_.layers:
+        optimizers.append(torch.optim.AdamW(list(i.parameters()), lr=args.lr))
+    #optimizers.append(torch.optim.AdamW(list(rev_.parameters())+list(dec_.parameters()), lr=args.lr))
+    opt_D = torch.optim.SGD(disc_.parameters(), lr=args.disc_lr, momentum = .5)
     for i in tqdm(range(args.max_iter)):
         for optimizer in optimizers:
             adjust_learning_rate(optimizer, i, args)
