1c449201495dff678ea3a3a0090e8d5ad5fc84ad
JackMcCoy <fdrdavegill@gmail.com>
2021-12-31 20:26:56 -0500

    different clip method

--- modules.py
+++ modules.py
@@ -51,10 +51,11 @@ class RiemannNoise(nn.Module):
         A, b, alpha, r = self.spatial_params
 
 
-        s = torch.sum(self.relu(-x), dim=1, keepdim=True)
+        s, _ = torch.max(-x, dim=1, keepdim=True)
         s = s - s.mean(dim=(2, 3), keepdim=True)
         s_max = torch.abs(s).amax(dim=(2, 3), keepdim=True)
         s = s / (s_max + 1e-8)
+        s = (s + 1) / 2
         s = s * A + b
         s = torch.tile(s, (1, c, 1, 1))
         sp_att_mask = (1 - alpha) + alpha * s
