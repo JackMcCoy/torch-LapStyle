f84388f5f397b9c656f33e17209268966aeb888f
JackMcCoy <fdrdavegill@gmail.com>
2022-01-02 14:51:00 -0500

    debugging

--- net.py
+++ net.py
@@ -719,6 +719,7 @@ class ResDiscriminator(nn.Module):
         self.spectral_gan = nn.ModuleList([*[nn.Sequential(
             OptimizedBlock(3, num_channels, 3, 1, downsample=False),
             SpectralResBlock(num_channels, num_channels, 3, 1, downsample=False),
+            SpectralResBlock(num_channels, num_channels, 3, 1, downsample=False),
             SpectralResBlock(num_channels, num_channels, 3, 1, downsample=False)) for i in range(depth+1)]
                                           ])
 
