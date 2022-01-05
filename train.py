f9135adf1bd0b7102cc0c80299fc0ace2cf7b808
JackMcCoy <fdrdavegill@gmail.com>
2022-01-05 04:09:07 -0500

    experiment with loss on final output only

--- train.py
+++ train.py
@@ -467,10 +467,7 @@ def revision_train():
                                      sF=sF, split_style = args.split_style if idx != 0 else False,
                                      rev_depth=idx)
                 loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
-                if idx != 0:
-                    loss = loss + (loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog)
-                else:
-                    loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * 16 + style_remd * 10 + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
+                loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * 16 + style_remd * 10 + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
         pred_fake = disc_(rev_outputs.float(), crop_marks)
         loss_D_fake = calc_GAN_loss_from_pred(pred_fake, True)
 
