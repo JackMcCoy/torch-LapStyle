9ab43237ed250df6ff65a1d753adf531fe899b5a
JackMcCoy <fdrdavegill@gmail.com>
2022-01-05 02:03:19 -0500

    revlib test 1

--- net.py
+++ net.py
@@ -406,7 +406,7 @@ def style_encoder_block(ch):
         nn.ReflectionPad2d((1, 1, 1, 1)),
         nn.Conv2d(ch, ch, kernel_size=3),
         nn.ReLU(),
-        nn.AvgPool2d(3, count_include_pad=True,padding=1, stride=2)
+        nn.AvgPool2d(3, count_include_pad=True,padding=1, stride=2),
         nn.Conv2d(ch, ch, kernel_size=1),
         nn.ReLU()
     ]
