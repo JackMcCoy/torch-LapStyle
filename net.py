3e1ef4c970ae7288288b1de47daaaae9d1ed3892
JackMcCoy <fdrdavegill@gmail.com>
2022-01-04 22:58:29 -0500

    patch style encodings

--- net.py
+++ net.py
@@ -252,7 +252,7 @@ class Revisors(nn.Module):
             *style_encoder_block(self.s_d)) for i in range(levels-1)]
         )
         self.style_projection = nn.ModuleList([nn.Sequential(
-            nn.Linear(512, self.s_d*16),
+            nn.Linear(1024, self.s_d*16),
             nn.ReLU()) for i in range(levels-1)])
 
     def load_states(self, state_string):
