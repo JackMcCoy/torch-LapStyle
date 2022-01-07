5a3e5dbd0f11e40a3736140ca25e71e9dee4f5a8
JackMcCoy <fdrdavegill@gmail.com>
2022-01-06 21:47:31 -0500

    debuggins

--- revlap.py
+++ revlap.py
@@ -70,7 +70,7 @@ class Sequential_Worker(nn.Module):
         out = self.crop_to_working_area(x, row, col)
         lap = self.crop_to_working_area(ci, row, col)
         lap = F.conv2d(F.pad(lap, (1,1,1,1), mode='reflect'), weight = self.lap_weight, groups = 3)
-        out = torch.cat([out, lap], dim=0)
+        out = torch.cat([out, lap], dim=1)
 
         out = self.downblock(out)
         for ada, learnable in zip(self.adaconvs, self.upblock):
