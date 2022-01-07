c66c041ddd5b1578a2d90f92a62b5a89918db274
JackMcCoy <fdrdavegill@gmail.com>
2022-01-06 22:42:18 -0500

    debuggins

--- revlap.py
+++ revlap.py
@@ -77,7 +77,6 @@ class Sequential_Worker(nn.Module):
             out = learnable(out)
         print(out.shape)
         out = self.reinsert_work(x, out, row, col)
-        out = out.repeat(1,2,1,1)
         print(out.shape)
         return out
 
@@ -100,7 +99,7 @@ class LayerHolders(nn.Module):
         return F.interpolate(x, self.max_res, mode='nearest')
 
     def forward(self, x, ci, style):
-        out = self.resize_to_res(x).repeat(1,2,1,1).data
+        out = self.resize_to_res(x).data
         out.requires_grad=True
         ci = self.resize_to_res(ci).to(torch.device('cuda:0'))
 
