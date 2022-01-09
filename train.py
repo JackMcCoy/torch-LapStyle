60c2aab97b398918b46537e62f5efa8860e60005
JackMcCoy <fdrdavegill@gmail.com>
2022-01-09 06:36:53 -0500

    try modulelist

--- train.py
+++ train.py
@@ -626,13 +626,13 @@ def revlap_train():
                 d_scaler.step(opt_D)
                 d_scaler.update()
         else:
-            loss_D.backward(retain_graph=True)
+            loss_D.backward()
             opt_D.step()
             opt_D.zero_grad()
         set_requires_grad(disc_, False)
 
         with autocast(enabled=ac_enabled):
-            '''
+
             losses_small = calc_losses(stylized, ci[0], si[0], cF, enc_, dec_, None, disc_,
                                         calc_identity=False, disc_loss=False,
                                         mdog_losses=args.mdog_loss, content_all_layers=False,
@@ -640,7 +640,7 @@ def revlap_train():
                                         patch_loss=False, sF=sF, split_style=args.split_style)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses_small
             losses_small = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss + mdog
-            '''
+
 
             losses_scaled = calc_losses(scale_stylized, ci[0], si[0], cF, enc_, dec_, None, disc_,
                                  calc_identity=False, disc_loss=False,
@@ -660,7 +660,7 @@ def revlap_train():
                                  patch_loss=True, sF=sF2, split_style=args.split_style)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog
-            loss = loss*args.thumbnail_loss + losses_scaled # + losses_small
+            loss = loss*args.thumbnail_loss + losses_scaled + losses_small
 
         if ac_enabled:
             scaler.scale(loss).backward()
