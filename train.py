36902094e7ad2e1b42718e027acef2d6e9d2f5a6
JackMcCoy <fdrdavegill@gmail.com>
2022-01-09 10:53:14 -0500

    try modulelist

--- train.py
+++ train.py
@@ -683,13 +683,11 @@ def revlap_train():
 
             loss_dict = {}
             for l, s in zip(
-                    [loss, loss_c, loss_s, style_remd, content_relt, loss_Gp_GAN, loss_D, rev_stylized, patch_loss,
+                    [loss, loss_c, loss_s, style_remd, content_relt, loss_Gp_GAN, loss_D, patch_loss,
                      mdog],
                     ['Loss', 'Content Loss', 'Style Loss', 'Style REMD', 'Content RELT',
                      'Revision Disc. Loss', 'Discriminator Loss', 'Patch Loss', 'MXDOG Loss']):
                 if type(l) == torch.Tensor:
-                    print(s)
-                    print(l)
                     loss_dict[s] = l.item()
             if(i +1) % 10 ==0:
                 loss_dict['example'] = wandb.Image(rev_stylized[0].transpose(2, 0).transpose(1, 0).detach().cpu().numpy())
