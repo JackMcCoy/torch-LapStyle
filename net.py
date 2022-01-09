a8b463d21891fa4dd8f045fb6767fb0be795cff0
JackMcCoy <fdrdavegill@gmail.com>
2022-01-09 12:28:05 -0500

    try modulelist

--- net.py
+++ net.py
@@ -827,9 +827,9 @@ def calc_GAN_loss_from_pred(prediction: torch.Tensor,
     return loss
 
 def calc_GAN_loss(real: torch.Tensor, fake:torch.Tensor, crop_marks, disc_:torch.nn.Module):
-    pred_fake = disc_(fake)
+    pred_fake = torch.clamp(disc_(fake),0,1)
     loss_D_fake = calc_GAN_loss_from_pred(pred_fake, False)
-    pred_real = disc_(real)
+    pred_real = torch.clamp(disc_(real),0,1)
     loss_D_real = calc_GAN_loss_from_pred(pred_real, True)
     loss_D = ((loss_D_real + loss_D_fake) * 0.5)
     return loss_D
