7b99547afe8086ff4f0fc7f1a75b48a79b3e6684
JackMcCoy <fdrdavegill@gmail.com>
2022-01-09 23:28:05 -0500

    stylegan2 style noise addition in decoder

--- net.py
+++ net.py
@@ -376,22 +376,24 @@ class DecoderAdaConv(nn.Module):
             nn.LeakyReLU()
         )
         self.kernel_1 = AdaConv(512, 8, batch_size, s_d = self.s_d)
-        self.decoder_1 = FusedConvNoiseBias(512, 256, 32, 'none')
+        self.decoder_1 = nn.Sequential(
+            FusedConvNoiseBias(512, 256, 32, 'none'),
+            FusedConvNoiseBias(256, 256, 64, 'up')
+        )
         self.kernel_2 = AdaConv(256, 4, batch_size, s_d = self.s_d)
         self.decoder_2 = nn.Sequential(
-            FusedConvNoiseBias(256, 256, 64, 'up'),
             FusedConvNoiseBias(256, 256, 64, 'none'),
             FusedConvNoiseBias(256, 256, 64, 'none'),
-        )
-        self.kernel_3 = AdaConv(256, 4, batch_size, s_d = self.s_d)
-        self.decoder_3 = nn.Sequential(
             FusedConvNoiseBias(256, 128, 128, 'up'),
-            FusedConvNoiseBias(128, 128, 128, 'none'),
-            FusedConvNoiseBias(128, 64, 128, 'none'),
         )
-        self.kernel_4 = AdaConv(128, 2, batch_size, s_d = self.s_d)
+        self.kernel_3 = AdaConv(128, 2, batch_size, s_d = self.s_d)
+        self.decoder_3 = nn.Sequential(
+            FusedConvNoiseBias(128, 128, 128, 'none'),
+            FusedConvNoiseBias(128, 64, 256, 'up'),
+        )
+        self.kernel_4 = AdaConv(64, 1, batch_size, s_d = self.s_d)
         self.decoder_4 = nn.Sequential(
-            FusedConvNoiseBias(64, 64, 256, 'up'),
+            FusedConvNoiseBias(64, 64, 256, 'none'),
             FusedConvNoiseBias(64, 3, 256, 'none'),
             nn.Conv2d(3, 3, kernel_size=1)
         )
