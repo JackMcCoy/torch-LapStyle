af48cc5b7771f912ab2761e4bb7de42663733a31
JackMcCoy <fdrdavegill@gmail.com>
2022-01-09 19:48:59 -0500

    share weights

--- revlap.py
+++ revlap.py
@@ -1,6 +1,6 @@
 from adaconv import AdaConv
 from net import style_encoder_block, ResBlock
-from modules import RiemannNoise, PixelShuffleUp, Upblock, Downblock, adaconvs, StyleEncoderBlock
+from modules import RiemannNoise, PixelShuffleUp, StyleEncoderBlock
 import torch
 from torch import nn
 import torch.nn.functional as F
@@ -16,31 +16,72 @@ def additive_coupling_forward(other_stream: torch.Tensor, fn_out: torch.Tensor) 
 def additive_coupling_inverse(output: torch.Tensor, fn_out: torch.Tensor) -> torch.Tensor:
     return output - downsample(fn_out)
 
+def style_encoder_block(s_d):
+    return nn.Sequential(StyleEncoderBlock(512),
+            StyleEncoderBlock(512),
+            StyleEncoderBlock(512)), nn.Sequential(
+            nn.Linear(8192, s_d * 16),
+            nn.ReLU()
+        )
+
+def Down_and_Up():
+    return nn.ModuleList(nn.Sequential(  # Downblock
+        nn.Conv2d(6, 128, kernel_size=1),
+        nn.LeakyReLU()),
+        nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 128, kernel_size=3, stride=1),
+        nn.LeakyReLU()),
+        nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(128, 64, kernel_size=3, stride=1),
+        nn.LeakyReLU()),
+        nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
+        nn.Conv2d(64, 64, kernel_size=3, stride=2),
+        nn.LeakyReLU()),
+        # Resblock Middle
+        ResBlock(64),
+        nn.Sequential(nn.Conv2d(64, 256, kernel_size=1),
+                      nn.LeakyReLU(),
+                      nn.PixelShuffle(2),
+                      nn.Conv2d(64, 64, kernel_size=1),
+                      nn.LeakyReLU(),
+                      nn.ReflectionPad2d((1, 1, 1, 1)),
+                      nn.Conv2d(64, 64, kernel_size=3),
+                      nn.LeakyReLU()),
+        nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
+                      nn.Conv2d(64, 128, kernel_size=3),
+                      nn.LeakyReLU()),
+        nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
+                      nn.Conv2d(128, 128, kernel_size=3),
+                      nn.LeakyReLU()),
+        nn.Sequential(nn.Conv2d(128, 3, kernel_size=1)
+                      )
+    )
+
+def adaconvs(batch_size,s_d):
+    return nn.ModuleList([
+            nn.Identity(),
+            AdaConv(128, 1, batch_size, s_d=s_d),
+            AdaConv(128, 2, batch_size, s_d=s_d),
+            AdaConv(64, 1, batch_size, s_d=s_d),
+            AdaConv(64, 1, batch_size, s_d=s_d),
+            AdaConv(64, 1, batch_size, s_d=s_d),
+            AdaConv(64, 1, batch_size, s_d=s_d),
+            AdaConv(128, 2, batch_size, s_d=s_d),
+            AdaConv(128, 2, batch_size, s_d=s_d)])
 
 blank_canvas = torch.zeros(4,3,512,512)
 class Sequential_Worker(nn.Module):
-    def __init__(self, max_res,working_res, batch_size,s_d):
+    def __init__(self, params, max_res,working_res, batch_size,s_d):
         super(Sequential_Worker, self).__init__()
         self.working_res = working_res
         self.s_d = 512
         self.max_res =max_res
-        self.style_encoding = nn.Sequential(
-            StyleEncoderBlock(512),
-            StyleEncoderBlock(512),
-            StyleEncoderBlock(512),
-        )
-        self.style_projection = nn.Sequential(
-            nn.Linear(8192, self.s_d * 16),
-            nn.ReLU()
-        )
-        self.downblock = nn.Sequential(*Downblock())
+        self.style_encoding = params[0]
+        self.style_projection = params[1]
+        self.down_and_up = params[3]
+        self.adaconvs = params[4]
+        self.lap_weight = params[5]
 
-
-        self.adaconvs = nn.ModuleList(adaconvs(batch_size, s_d=self.s_d))
-        self.upblock = nn.ModuleList(Upblock())
-        self.lap_weight = np.repeat(np.array([[[[-8, -8, -8], [-8, 1, -8], [-8, -8, -8]]]]), 3, axis=0)
-        self.lap_weight = torch.Tensor(self.lap_weight).to(torch.device('cuda:0'))
-        self.lap_weight.requires_grad = False
         # row_num == col_num, as these are squares
 
     def get_layer_rows(self, layer_num, layer_res):
@@ -87,7 +128,6 @@ class Sequential_Worker(nn.Module):
 
         lap = F.conv2d(F.pad(lap, (1,1,1,1), mode='reflect'), weight = self.lap_weight, groups = 3)
         out = torch.cat([out, lap], dim=1)
-        out = self.downblock(out)
 
         N,C,h,w = thumb.shape
         style = self.style_encoding(thumb)
@@ -95,40 +135,11 @@ class Sequential_Worker(nn.Module):
         style = self.style_projection(style)
         style = style.reshape(N, self.s_d, 4, 4)
 
-        for ada, learnable in zip(self.adaconvs, self.upblock):
+        for ada, learnable in zip(self.adaconvs, self.down_and_up):
             out = ada(style, out, norm=True)
             out = learnable(out)
         out = self.reinsert_work(x, out, row, col)
         out = self.return_to_full_res(out)
-        return out
-
-
-class LayerHolders(nn.Module):
-    def __init__(self, max_res: int, working_res: int, layer_num: int, batch_size: int, s_d: int):
-        """Uses square-valued resolutions"""
-        super(LayerHolders, self).__init__()
-        self.max_res = max_res
-        self.working_res = working_res
-        self.layer_num = layer_num
-        self.internal_layer_res = 512*2**layer_num
-        self.num_layers_per_side = self.internal_layer_res // 256
-        self.module_patches = Sequential_Worker(256, self.internal_layer_res, batch_size,s_d)
-
-    def resize_to_res(self, x):
-        return F.interpolate(x, self.internal_layer_res, mode='nearest')
-
-    def return_to_full_res(self, x):
-        return F.interpolate(x, self.max_res, mode='nearest')
-
-    def forward(self, x, ci, style):
-
-        #out = self.resize_to_res(x).repeat(1,2,1,1).data
-        out = self.resize_to_res(x)
-        ci = self.resize_to_res(ci)
-        for i in range(self.num_layers_per_side**2):
-            out = self.module_patches(out, ci, style, i)
-        out = self.return_to_full_res(out)
-        print(out.shape)
         return out
 
 
@@ -138,8 +149,12 @@ class LapRev(nn.Module):
         self.max_res = max_res
         self.working_res = working_res
         height = max_res//working_res
+        self.lap_weight = np.repeat(np.array([[[[-8, -8, -8], [-8, 1, -8], [-8, -8, -8]]]]), 3, axis=0)
+        self.lap_weight = torch.Tensor(self.lap_weight).to(torch.device('cuda:0'))
+        self.lap_weight.requires_grad = False
         self.num_layers = [(h,i) for h in range(height) for i in range(int((2**h)/.25))]
-        self.layers = module_list_to_momentum_net(nn.ModuleList([Sequential_Worker(self.max_res,256, batch_size, s_d) for i in self.num_layers]),target_device='cuda:0')
+        self.params = [[*style_encoder_block(s_d), Down_and_Up(),adaconvs(batch_size, s_d), self.lap_weight] for h in range(height)]
+        self.layers = module_list_to_momentum_net(nn.ModuleList([Sequential_Worker(params[i[0]],self.max_res,256, batch_size, s_d) for i in self.num_layers]),target_device='cuda:0')
 
     def forward(self, input:torch.Tensor, ci:torch.Tensor, enc_:torch.nn.Module):
         """
