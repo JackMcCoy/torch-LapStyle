6a3979cc9c5d688407003c6417fff7504823d3ce
JackMcCoy <fdrdavegill@gmail.com>
2022-01-11 12:44:29 -0500

    debugging

--- revlap.py
+++ revlap.py
@@ -252,7 +252,7 @@ class LapRev(nn.Module):
         #input.requires_grad = True
         out = F.interpolate(input, self.max_res, mode='nearest')
         out = out.repeat(2,1,1,1)
-        out = self.momentumnet(out,([(self.params[0],ci, style.data)] * len(self.momentumnet.stem)))
+        out = self.momentumnet(out,(self.params[0],ci, style.data))
 
         out = out[N:,:, :,:]
         return out
