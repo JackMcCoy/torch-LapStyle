ed43871d0e8f386c9e9410801337d7946a46892c
JackMcCoy <fdrdavegill@gmail.com>
2022-01-12 02:27:08 -0500

    shared weights

--- weights.py
+++ weights.py
@@ -55,7 +55,7 @@ def resblock_weight(ch):
 
 def fused_conv_noise_weights(ch_in, ch_out, noise=False, noise_size=256):
     params = []
-    params.append(nn.ParameterList([conv_weight(ch_in, ch_out, 3, 1, activation_std)])
+    params.append(nn.ParameterList([conv_weight(ch_in, ch_out, 3, 1, activation_std)]))
     if noise:
         params.append(rnoise_weight(noise_size))
     params.append(nn.ParameterList([bias()])
