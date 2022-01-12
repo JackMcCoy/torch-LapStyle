a88e3b7dc8027c39407005137c3c52a187775ee8
JackMcCoy <fdrdavegill@gmail.com>
2022-01-12 03:35:06 -0500

    shared weights

--- weights.py
+++ weights.py
@@ -19,21 +19,21 @@ def rnoise_weight(size):
                                         nn.Parameter(nn.init.constant_(torch.ones(1, ), .5)),
                                         nn.Parameter(nn.init.constant_(torch.ones(1, ), .5))])
 
-def bias():
-    return nn.Parameter(nn.init.constant_(torch.ones(1, ), 0))
+def bias(dim):
+    return nn.Parameter(nn.init.constant_(torch.ones(dim, ), 0))
 
 def adaconv_weight(s_d, channels, n_groups,):
     params = []
-    params.append(nn.ParameterList([conv_weight(s_d, channels * (channels // n_groups), 2, 1, activation_std), bias()]))
-    params.append(nn.ParameterList([conv_weight(s_d, channels * (channels // n_groups), 1, 1, activation_std), bias()]))
-    params.append(nn.ParameterList([conv_weight(s_d, channels, 1, 1, activation_std),bias()]))
+    params.append(nn.ParameterList([conv_weight(s_d, channels * (channels // n_groups), 2, 1, activation_std), bias(channels * (channels // n_groups))]))
+    params.append(nn.ParameterList([conv_weight(s_d, channels * (channels // n_groups), 1, 1, activation_std), bias(channels * (channels // n_groups))]))
+    params.append(nn.ParameterList([conv_weight(s_d, channels, 1, 1, activation_std),bias(channels)]))
     params.append(nn.ParameterList([bias()]))
     return nn.ModuleList(params)
 
 def resblock_weight(ch):
     params = []
-    params.append(nn.ParameterList([conv_weight(ch, ch, 3, 1, activation_std), bias()]))
-    params.append(nn.ParameterList([conv_weight(ch, ch, 1, 1, activation_std), bias()]))
+    params.append(nn.ParameterList([conv_weight(ch, ch, 3, 1, activation_std), bias(ch)]))
+    params.append(nn.ParameterList([conv_weight(ch, ch, 1, 1, activation_std), bias(ch)]))
     return nn.ModuleList(params)
 
 def fused_conv_noise_weights(ch_in, ch_out, noise=False, noise_size=256):
@@ -41,9 +41,9 @@ def fused_conv_noise_weights(ch_in, ch_out, noise=False, noise_size=256):
     params.append(nn.ParameterList([conv_weight(ch_in, ch_out, 3, 1, activation_std)]))
     if noise:
         params.append(rnoise_weight(noise_size))
-    params.append(nn.ParameterList([bias()]))
+    params.append(nn.ParameterList([bias(ch_out)]))
     if ch_in != ch_out:
-        params.append(nn.ParameterList([conv_weight(ch_in, ch_out, 3, 1, activation_std),bias()]))
+        params.append(nn.ParameterList([conv_weight(ch_in, ch_out, 3, 1, activation_std),bias(ch_out)]))
     return nn.ModuleList(params)
 
 def downblock_weights():
@@ -57,7 +57,7 @@ def upblock_weights():
     for i, j, k, l in [(64, 64, False, 0), (64, 128, False, 0), (128, 128, False, 0), (128, 3, False, 0),
                        (64, 64, False, 0)]:
         params.append(fused_conv_noise_weights(i, j, noise=k, noise_size=l))
-    params.append(nn.ParameterList([conv_weight(3, 3, 1, 1, activation_std),bias()]))
+    params.append(nn.ParameterList([conv_weight(3, 3, 1, 1, activation_std),bias(3)]))
     return nn.ModuleList(params)
 
 def up_adaconv_weights(s_d):
@@ -71,5 +71,5 @@ def style_encoder_weights(s_d, dim=16):
     params = []
     params.append(torch.nn.Parameter(torch.normal(torch.zeros(s_d * dim, s_d * dim),
                                                           torch.ones(s_d * dim, s_d * dim))))
-    params.append(bias())
+    params.append(bias(s_d * dim))
     return nn.ParameterList(params)
