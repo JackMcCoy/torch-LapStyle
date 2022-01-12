41493f0ca47a8493590b2fbc6478911f3c3609db
JackMcCoy <fdrdavegill@gmail.com>
2022-01-12 03:05:26 -0500

    shared weights

--- shared_modules.py
+++ shared_modules.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 
 def conv(inp, weight, groups, use_pad=True,bias=None):
     if use_pad:
-        inp = F.pad(inp, (1, 1, 1, 1), stride=1, mode='reflect')
-    return F.conv2d(inp, weight, groups=groups, bias=bias)
+        inp = F.pad(inp, (1, 1, 1, 1), mode='reflect')
+    return F.conv2d(inp, weight, groups=groups, stride=1, bias=bias)
 
 def conv1d(inp, weight, groups, use_pad=True,bias=None):
     if use_pad:
