d3bd56375291801047e996555dc9e3c477268913
JackMcCoy <fdrdavegill@gmail.com>
2022-01-13 11:59:43 -0500

    variable thumbnail loss + scaled_loss

--- revlap.py
+++ revlap.py
@@ -170,7 +170,7 @@ class LapRev(nn.Module):
         coupling_inverse = [partial(cropped_coupling_inverse, h, i) for h, i in self.num_layers]
 
         cell = Sequential_Worker(1., 0, 0, self.max_res,256, batch_size, s_d)
-        self.layers = revlib.ReversibleSequential(*[cell.copy(layer_num) for height, layer_num in self.num_layers],split_dim=0,coupling_forward=coupling_forward,coupling_inverse=coupling_inverse, memory_mode=revlib.core.MemoryModes.autograd_function)
+        self.layers = revlib.ReversibleSequential(*[cell.copy(layer_num) for height, layer_num in self.num_layers],split_dim=0,coupling_forward=coupling_forward,coupling_inverse=coupling_inverse, memory_mode=revlib.core.MemoryModes.autograd_function, target_device='cuda')
 
     def forward(self, input:torch.Tensor, ci:torch.Tensor, style:torch.Tensor, enc_:nn.Module):
         """
