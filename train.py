e7048679aadd899cba396cef7fb5da1c672bd31e
JackMcCoy <fdrdavegill@gmail.com>
2022-01-14 00:15:10 -0500

    clip gradients

--- train.py
+++ train.py
@@ -668,10 +668,11 @@ def revlap_train():
                                              patch_loss=True, sF=sF, split_style=args.split_style)
                         loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
                         loss = loss + (loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss + patch_loss * args.patch_loss + mdog)*.25
-            for mod in [dec_,disc_,rev_]:
-                _clip_gradient(mod)
+
             loss.backward()
             loss_D.backward()
+            for mod in [dec_,disc_,rev_]:
+                _clip_gradient(mod)
             if idx == 0:
                 opt_D.first_step(zero_grad=True)
                 dec_optimizer.first_step(zero_grad=True)
