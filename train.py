99bc528c8028773a7131335893a3702e5995e651
JackMcCoy <fdrdavegill@gmail.com>
2022-01-14 04:25:42 -0500

    sgd + jit for decoder and disc

--- train.py
+++ train.py
@@ -622,7 +622,7 @@ def revlap_train():
 
                 stylized, style = dec_(sF, cF)
 
-                rev_stylized = rev_(stylized, ci[-1], si[-1], enc_)
+                rev_stylized = rev_(stylized, ci[-1], si[-1])
                 si_cropped = random_crop(si[-1])
                 stylized_crop = rev_stylized[:,:,-384:-128, -256:]
 
