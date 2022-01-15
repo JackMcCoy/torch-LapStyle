3f82f1cf269b14cd56def86de2f42f5cf680f38f
JackMcCoy <fdrdavegill@gmail.com>
2022-01-15 04:51:16 -0500

    try rolling it

--- train.py
+++ train.py
@@ -626,7 +626,7 @@ def revlap_train():
 
                 rev_stylized = rev_(stylized, ci[-1], si[-1])
                 si_cropped = random_crop(si[-1])
-                stylized_crop = rev_stylized[:,:,-256:, -256:]
+                stylized_crop = rev_stylized[:,:,:256, :256]
 
             with autocast(enabled=ac_enabled):
 
