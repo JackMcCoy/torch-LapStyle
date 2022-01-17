3f6a888d17cd4f3a16614ec145df671a8baed854
JackMcCoy <fdrdavegill@gmail.com>
2022-01-16 21:38:20 -0500

    quantize first layer

--- train.py
+++ train.py
@@ -759,7 +759,7 @@ def adaconv_thumb_train():
                                        remd_loss=remd_loss,
                                        patch_loss=True, upscaled_patch = patch_stylized, sF=sF, split_style=False)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
-            loss = cb_loss + cb_loss2 + loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss + mdog
+            loss = cb_loss + loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss + mdog
 
             loss.backward()
             dec_optimizer.step()
