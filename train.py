523e6a90f8bf0ded412be209823f702657147e42
JackMcCoy <fdrdavegill@gmail.com>
2022-01-17 01:49:39 -0500

    no patch test

--- train.py
+++ train.py
@@ -757,7 +757,7 @@ def adaconv_thumb_train():
                                        calc_identity=args.identity_loss==1, disc_loss=False,
                                        mdog_losses=args.mdog_loss, content_all_layers=False,
                                        remd_loss=remd_loss,
-                                       patch_loss=False, upscaled_patch = patch_stylized, sF=sF, split_style=False)
+                                       patch_loss=False, upscaled_patch = None, sF=sF, split_style=False)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss + mdog
 
