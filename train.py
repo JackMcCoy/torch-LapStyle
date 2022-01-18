224a32496bdc8d99d00626fb3bb21000797be3e1
JackMcCoy <fdrdavegill@gmail.com>
2022-01-18 01:18:38 -0500

    multiple patches

--- train.py
+++ train.py
@@ -810,10 +810,10 @@ def adaconv_thumb_train():
                      'Patch Disc. Loss']):
                 if type(l) == torch.Tensor:
                     loss_dict[s] = l.item()
-            if(i +1) % 10 ==0:
+            if(n +1) % 10 ==0:
                 loss_dict['example'] = wandb.Image(stylized[0].transpose(2, 0).transpose(1, 0).detach().cpu().numpy())
             print('\n')
-            print(str(i)+'/'+str(args.max_iter)+': '+'\t'.join([str(k) + ': ' + str(v) for k, v in loss_dict.items()]))
+            print(str(n)+'/'+str(args.max_iter)+': '+'\t'.join([str(k) + ': ' + str(v) for k, v in loss_dict.items()]))
 
             wandb.log(loss_dict, step=i)
 
