9bc1f6af3dbdaf54602883019e83cda0617ceae0
JackMcCoy <fdrdavegill@gmail.com>
2022-01-18 01:12:59 -0500

    multiple patches

--- train.py
+++ train.py
@@ -789,7 +789,7 @@ def adaconv_thumb_train():
                                        mdog_losses=args.mdog_loss, content_all_layers=False,
                                        remd_loss=remd_loss,
                                        patch_loss=True, patch_stylized = patches, sF=sF, split_style=False)
-            loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, _, patch_disc_loss = losses
+            loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss, patch_disc_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss +loss_Gp_GAN*args.gan_loss + patch_disc_loss*args.gan_loss +mdog + l_identity1*50 + l_identity2 + l_identity3*50 + l_identity4
 
             loss.backward()
