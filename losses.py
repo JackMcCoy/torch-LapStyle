67f5432b0b25aa6ddd6b206f8827a3b6ba30c00d
JackMcCoy <fdrdavegill@gmail.com>
2022-01-20 08:50:59 -0500

    flatten mean/var

--- losses.py
+++ losses.py
@@ -216,6 +216,8 @@ def calc_mean_std(feat, eps=1e-5):
     """
     size = feat.shape
     assert (len(size) == 4)
-    feat_std = torch.std(feat, dim = (2,3), keepdim=True, unbiased=True)
-    feat_mean = feat.mean(dim= (2,3), keepdim=True)
+    N, C = size[:2]
+    feat_var = feat.view(N, C, -1).var(dim=2) + eps
+    feat_std = feat_var.sqrt().view(N, C, 1, 1)
+    feat_mean = feat.view(N, C, -1).mean(dim=2).view(N, C, 1, 1)
     return feat_mean, feat_std
