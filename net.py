2ca074391ec3415ddccd93c3b55169d12d70d9cb
JackMcCoy <fdrdavegill@gmail.com>
2022-01-21 15:18:22 -0500

    style contrastive only

--- net.py
+++ net.py
@@ -438,7 +438,8 @@ class StyleReprojection(nn.Module):
         self.style_reprojection = nn.Sequential(
             nn.Linear(self.s_d * 16, self.s_d * 16),
             nn.LeakyReLU(),
-            nn.Linear(self.s_d * 16, self.s_d * 16)
+            nn.Linear(self.s_d * 16, self.s_d * 16),
+            nn.LeakyReLU(),
         )
     def forward(self,x):
         b = x.shape[0]
