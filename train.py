6ceeafd64480024cc1b52d95f72262130df46618
JackMcCoy <fdrdavegill@gmail.com>
2022-01-24 08:03:11 -0500

    no contrastive loss

--- train.py
+++ train.py
@@ -788,7 +788,7 @@ def adaconv_thumb_train():
             losses = calc_losses(stylized, ci[0], si[0], cF, enc_, dec_, None, disc_,
                                        calc_identity=args.identity_loss==1, disc_loss=False,
                                        mdog_losses=args.mdog_loss, content_all_layers=args.content_all_layers,
-                                       remd_loss=remd_loss, contrastive_loss = True,
+                                       remd_loss=remd_loss, contrastive_loss = False,
                                        patch_loss=True, patch_stylized = patches, top_level_patch = original, sF=sF, split_style=False)
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss, style_contrastive_loss, content_contrastive_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss +\
@@ -800,7 +800,7 @@ def adaconv_thumb_train():
             patch_losses = calc_losses(patch_stylized, ci[-1], si[-1], patch_cF, enc_, dec_, None, disc_,
                                        calc_identity=False, disc_loss=True,
                                        mdog_losses=args.mdog_loss, content_all_layers=args.content_all_layers,
-                                       remd_loss=remd_loss, contrastive_loss=True,
+                                       remd_loss=remd_loss, contrastive_loss=False,
                                        patch_loss=False, sF=patch_sF,
                                        split_style=False)
             loss_cp, loss_sp, content_reltp, style_remdp, l_identity1p, l_identity2p, l_identity3p, l_identity4p, mdogp, loss_Gp_GAN, patch_loss, style_contrastive_lossp, content_contrastive_lossp = patch_losses
