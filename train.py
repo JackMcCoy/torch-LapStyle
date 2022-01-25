353d8ece924b4c402440d2598dc0dc5f3891aa50
JackMcCoy <fdrdavegill@gmail.com>
2022-01-25 17:44:07 -0500

    log patch loss

--- train.py
+++ train.py
@@ -798,8 +798,8 @@ def adaconv_thumb_train():
                                  remd_loss=remd_loss, contrastive_loss=True,
                                  patch_loss=False, patch_stylized=patches, top_level_patch=original,
                                  sF=patch_sF, split_style=False)
-            loss_cp, loss_sp, content_reltp, style_remdp, l_identity1p, l_identity2p, l_identity3p, l_identity4p, mdogp, loss_Gp_GAN, patch_loss, style_contrastive_lossp, content_contrastive_lossp = p_losses
-            loss = loss + (loss_cp * args.content_weight + args.style_weight * loss_sp + content_reltp * args.content_relt + style_remdp * args.style_remd + patch_loss * args.patch_loss + \
+            loss_cp, loss_sp, content_reltp, style_remdp, l_identity1p, l_identity2p, l_identity3p, l_identity4p, mdogp, loss_Gp_GAN, patch_lossp, style_contrastive_lossp, content_contrastive_lossp = p_losses
+            loss = loss + (loss_cp * args.content_weight + args.style_weight * loss_sp + content_reltp * args.content_relt + style_remdp * args.style_remd + patch_lossp * args.patch_loss + \
                    loss_Gp_GAN * args.gan_loss + mdog + l_identity1 * 50 + l_identity2 + l_identity3 * 50 + l_identity4 + \
                    style_contrastive_lossp * 0.8 + content_contrastive_lossp * 0.3)
 
