d9167ee988f064824a4c32ea8994416539d34c52
JackMcCoy <fdrdavegill@gmail.com>
2022-01-26 10:40:26 -0500

    drafting sanity-check

--- train.py
+++ train.py
@@ -277,7 +277,7 @@ def drafting_train():
                                         mdog_losses=args.mdog_loss, content_all_layers=args.content_all_layers,
                                         remd_loss=remd_loss,
                                         patch_loss=False, sF=sF, split_style=args.split_style)
-            loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss = losses
+            loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss, style_contrastive_loss, content_contrastive_loss = losses
             loss = loss_c * args.content_weight + args.style_weight * loss_s + l_identity1*50+ l_identity2+ l_identity3*50+ l_identity4+content_relt * args.content_relt + style_remd * args.style_remd + loss_Gp_GAN * args.gan_loss
 
         if ac_enabled:
