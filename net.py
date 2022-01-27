b1fe4d06b1c0bdc072ff6cd07ed8b8bc14739f19
JackMcCoy <fdrdavegill@gmail.com>
2022-01-27 15:21:32 -0500

    no downsampling in spectral_disc

--- net.py
+++ net.py
@@ -816,9 +816,9 @@ class SpectralDiscriminator(nn.Module):
     def __init__(self, depth:int=5, num_channels: int=64):
         super(SpectralDiscriminator, self).__init__()
         ch = num_channels
-        self.spectral_gan = nn.ModuleList([OptimizedBlock(3, num_channels, 3, 1, downsample=True),
-                                          *[SpectralResBlock(ch*2**i, ch*2**(i+1), 3, 1, downsample=True) for i in range(depth-2)],
-                                          SpectralResBlock(ch*2**(depth-2), 3, 3, 1, downsample=False)])
+        self.spectral_gan = nn.ModuleList([OptimizedBlock(3, num_channels, 3, 1, downsample=False),
+                                          *[SpectralResBlock(ch*2**i, ch*2**(i+1), 3, 1, downsample=False) for i in range(depth-2)],
+                                          SpectralResBlock(ch*2**(depth-2), 1, 3, 1, downsample=False)])
 
     def init_spectral_norm(self):
         for layer in self.spectral_gan:
@@ -884,8 +884,8 @@ gan_first=True
 def calc_GAN_loss_from_pred(prediction: torch.Tensor,
               target_is_real: bool):
     batch_size = prediction.shape[0]
-    c = 3
-    h = 64
+    c = 1
+    h = 256
     if target_is_real:
         target_tensor = torch.ones(batch_size, c, h, h, device=torch.device('cuda:0'))
     else:
