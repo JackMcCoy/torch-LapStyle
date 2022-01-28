eea346a4ae55e9cb45ab77f9ea5720b269f51829
JackMcCoy <fdrdavegill@gmail.com>
2022-01-28 01:35:07 -0500

    remove cuda device refs in target code

--- net.py
+++ net.py
@@ -19,8 +19,6 @@ from linear_attention_transformer import LinearAttentionTransformer as Transform
 from adaconv import AdaConv
 from vector_quantize_pytorch import VectorQuantize
 
-gaus_1, gaus_2, morph = make_gaussians(torch.device('cuda'))
-
 device = torch.device('cuda')
 
 unfold = torch.nn.Unfold(256,stride=256)
@@ -791,9 +789,9 @@ class OptimizedBlock(nn.Module):
         self.downsample = nn.AvgPool2d(2) if downsample else nn.Identity()
 
     def init_spectral_norm(self):
-        self.conv_1 = spectral_norm(self.conv_1).to(device)
-        self.conv_2 = spectral_norm(self.conv_2).to(device)
-        self.c_sc = spectral_norm(self.c_sc).to(device)
+        self.conv_1 = spectral_norm(self.conv_1)
+        self.conv_2 = spectral_norm(self.conv_2)
+        self.c_sc = spectral_norm(self.c_sc)
 
 
     def forward(self, in_feat):
