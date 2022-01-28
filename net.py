c2ac058633c1f9998550647b772919f6e8914cc1
JackMcCoy <fdrdavegill@gmail.com>
2022-01-28 06:25:12 -0500

    detach decoder style for gan loss

--- net.py
+++ net.py
@@ -1012,7 +1012,7 @@ def calc_losses(stylized: torch.Tensor,
         mxdog_losses = 0
 
     if disc_loss:
-        style_embedding = disc_.style_encoding(style_embedding)
+        style_embedding = disc_.style_encoding(style_embedding.clone().detach())
         style_embedding = disc_.style_projection(style_embedding.flatten(1)).reshape(-1, 128, 4, 4)
         fake_loss = disc_(stylized, style_embedding)
         loss_Gp_GAN = disc_.ganloss(fake_loss, True)
