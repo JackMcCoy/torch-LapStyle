f848dc69fa68020eff523ec245ca0e2a389dc560
JackMcCoy <fdrdavegill@gmail.com>
2022-01-28 05:10:25 -0500

    style guided disc

--- net.py
+++ net.py
@@ -1012,6 +1012,8 @@ def calc_losses(stylized: torch.Tensor,
         mxdog_losses = 0
 
     if disc_loss:
+        style_embedding = self.style_encoding(style_embedding)
+        style_embedding = self.style_projection(style_embedding).reshape(-1, 128, 4, 4)
         fake_loss = disc_(stylized, style_embedding)
         loss_Gp_GAN = disc_.ganloss(fake_loss, True)
     else:
