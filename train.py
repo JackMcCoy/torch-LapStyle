fd2eeccdf3e200f4da6719f42c5ec3047590c034
JackMcCoy <fdrdavegill@gmail.com>
2022-01-28 04:34:16 -0500

    let disc have first update. add tqdm

--- train.py
+++ train.py
@@ -802,7 +802,9 @@ def adaconv_thumb_train():
                     param.grad = None
             patch_stylized = rev_(original[0], ci[-1])
             patches.append(patch_stylized)
-            if n>0:
+
+        if n>0:
+            with autocast(enabled=ac_enabled):
                 losses = calc_losses(stylized, ci[0], si[0], cF, enc_, dec_, None, disc_,
                                      calc_identity=args.identity_loss == 1, disc_loss=True,
                                      mdog_losses=args.mdog_loss, content_all_layers=args.content_all_layers,
@@ -830,24 +832,24 @@ def adaconv_thumb_train():
                             loss_Gp_GANp * args.gan_loss + mdog + l_identity1 * 50 + l_identity2 + l_identity3 * 50 + l_identity4 + \
                             style_contrastive_lossp * 0.8 + content_contrastive_lossp * 0.3)
 
-        if ac_enabled:
-            scaler.scale(loss).backward(retain_graph=True)
-        else:
-            loss.backward()
-        if n % args.accumulation_steps == 0:
             if ac_enabled:
-                scaler.step(dec_optimizer)
-                scaler.step(rev_optimizer)
-                scaler.update()
+                scaler.scale(loss).backward(retain_graph=True)
             else:
-                rev_optimizer.step()
-                dec_optimizer.step()
-
+                loss.backward()
             if n % args.accumulation_steps == 0:
-                for param in disc_.parameters():
-                    param.grad = None
-                for param in disc2_.parameters():
-                    param.grad = None
+                if ac_enabled:
+                    scaler.step(dec_optimizer)
+                    scaler.step(rev_optimizer)
+                    scaler.update()
+                else:
+                    rev_optimizer.step()
+                    dec_optimizer.step()
+
+                if n % args.accumulation_steps == 0:
+                    for param in disc_.parameters():
+                        param.grad = None
+                    for param in disc2_.parameters():
+                        param.grad = None
 
 
             set_requires_grad(disc_, True)
