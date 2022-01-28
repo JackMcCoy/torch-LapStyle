808a941c65a47538f0966ccea60cc7a3a62882ef
JackMcCoy <fdrdavegill@gmail.com>
2022-01-27 22:37:10 -0500

    sum reduction for cross_entropy contrastive loss

--- net.py
+++ net.py
@@ -924,7 +924,7 @@ def content_feature_contrastive(input, decoder):
 def compute_contrastive_loss(feat_q, feat_k, tau, index):
     out = torch.mm(feat_q, feat_k.transpose(1, 0)) / tau
     #loss = self.cross_entropy_loss(out, torch.zeros(out.size(0), dtype=torch.long, device=feat_q.device))
-    loss = F.cross_entropy(out.to(torch.float32), torch.tensor([index], device=feat_q.device), reduction='sum')
+    loss = F.cross_entropy(out.to(torch.float32)+1e-3, torch.tensor([index], device=feat_q.device), reduction='sum')
     return loss
 
 
