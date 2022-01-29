fada3b4c335669958809fae3fa4dd60f23e6c456
JackMcCoy <fdrdavegill@gmail.com>
2022-01-28 22:38:26 -0500

    adaconv in revisionnet

--- adaconv.py
+++ adaconv.py
@@ -47,10 +47,11 @@ class AdaConv(nn.Module):
 
     def forward(self, style_encoding: torch.Tensor, predicted: torch.Tensor):
         N = style_encoding.shape[0]
-        depthwise = self.depthwise_kernel_conv(style_encoding).view(N,self.c_out, self.c_in // self.n_groups)
+        depthwise = self.depthwise_kernel_conv(style_encoding)
+        depthwise = depthwise.view(N, self.c_out, self.c_in // self.n_groups, 3, 3)
         s_d = self.pointwise_avg_pool(style_encoding)
-        pointwise_kn = self.pw_cn_kn(s_d).view(N,self.c_out, self.c_out//self.n_groups)
-        pointwise_bias = self.pw_cn_bias(s_d).view(N,self.c_out)
+        pointwise_kn = self.pw_cn_kn(s_d).view(N, self.c_out, self.c_out // self.n_groups, 1, 1)
+        pointwise_bias = self.pw_cn_bias(s_d).view(N, self.c_out)
 
         a, b, c, d = predicted.size()
         if self.norm:
