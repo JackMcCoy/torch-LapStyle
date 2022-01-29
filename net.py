18054056dee8f134be44cb1db74dbe4afb25149d
JackMcCoy <fdrdavegill@gmail.com>
2022-01-28 21:53:55 -0500

    mo batchnorm

--- net.py
+++ net.py
@@ -753,7 +753,7 @@ class Discriminator(nn.Module):
 
     def forward(self, x, style):
         x = self.head(x)
-        x = self.norm(x)
+        x = self.norms(x)
         x = self.tail(x)
         return x
 
