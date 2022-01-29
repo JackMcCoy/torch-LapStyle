9bdd49fee8b85c1339e4b8ead4a4111e0ecd39ff
JackMcCoy <fdrdavegill@gmail.com>
2022-01-28 22:22:38 -0500

    adaconv in revisionnet

--- net.py
+++ net.py
@@ -148,7 +148,6 @@ class RevisionNet(nn.Module):
         self.adaconvs = nn.ModuleList([
             AdaConv(64, 8, s_d=s_d),
             AdaConv(64, 8, s_d=s_d),
-            AdaConv(128, 4, s_d=s_d),
             AdaConv(128, 4, s_d=s_d)])
 
         self.UpBlock = nn.ModuleList([nn.Sequential(nn.ReflectionPad2d((1, 1, 1, 1)),
