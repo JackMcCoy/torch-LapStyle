96d2a9d373089243662357aa6675acb1f090a01c
JackMcCoy <fdrdavegill@gmail.com>
2022-01-29 17:51:16 -0500

    gradient checkpointing

--- train.py
+++ train.py
@@ -785,11 +785,12 @@ def adaconv_thumb_train():
 
         for param in dec_.parameters():
             param.grad = None
+        print('dec')
         stylized, style_embedding = torch.utils.checkpoint.checkpoint(dec_,cF,sF['r4_1'])
 
         patches = []
         original = []
-
+        print('rev')
         original.append(F.interpolate(stylized[:,:,0:128,0:128],256))
         for param in rev_.parameters():
             param.grad = None
