95e319d048cd463a023b245a69794276a674f343
JackMcCoy <fdrdavegill@gmail.com>
2022-01-31 00:28:40 -0500

    blurpool for aliasing

--- modules.py
+++ modules.py
@@ -6,6 +6,59 @@ from function import normalized_feat
 from adaconv import AdaConv
 
 
+class BlurPool(nn.Module):
+    def __init__(self, channels, pad_type='reflect', filt_size=4, stride=2, pad_off=0):
+        super(BlurPool, self).__init__()
+        self.filt_size = filt_size
+        self.pad_off = pad_off
+        self.pad_sizes = [int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2)), int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2))]
+        self.pad_sizes = [pad_size+pad_off for pad_size in self.pad_sizes]
+        self.stride = stride
+        self.off = int((self.stride-1)/2.)
+        self.channels = channels
+
+        if(self.filt_size==1):
+            a = np.array([1.,])
+        elif(self.filt_size==2):
+            a = np.array([1., 1.])
+        elif(self.filt_size==3):
+            a = np.array([1., 2., 1.])
+        elif(self.filt_size==4):
+            a = np.array([1., 3., 3., 1.])
+        elif(self.filt_size==5):
+            a = np.array([1., 4., 6., 4., 1.])
+        elif(self.filt_size==6):
+            a = np.array([1., 5., 10., 10., 5., 1.])
+        elif(self.filt_size==7):
+            a = np.array([1., 6., 15., 20., 15., 6., 1.])
+
+        filt = torch.Tensor(a[:,None]*a[None,:])
+        filt = filt/torch.sum(filt)
+        self.register_buffer('filt', filt[None,None,:,:].repeat((self.channels,1,1,1)))
+
+        self.pad = get_pad_layer(pad_type)(self.pad_sizes)
+
+    def forward(self, inp):
+        if(self.filt_size==1):
+            if(self.pad_off==0):
+                return inp[:,:,::self.stride,::self.stride]
+            else:
+                return self.pad(inp)[:,:,::self.stride,::self.stride]
+        else:
+            return F.conv2d(self.pad(inp), self.filt, stride=self.stride, groups=inp.shape[1])
+
+def get_pad_layer(pad_type):
+    if(pad_type in ['refl','reflect']):
+        PadLayer = nn.ReflectionPad2d
+    elif(pad_type in ['repl','replicate']):
+        PadLayer = nn.ReplicationPad2d
+    elif(pad_type=='zero'):
+        PadLayer = nn.ZeroPad2d
+    else:
+        print('Pad type [%s] not recognized'%pad_type)
+    return PadLayer
+
+
 class ThumbInstanceNorm(nn.Module):
     def __init__(self, out_channels=None, affine=True):
         super(ThumbInstanceNorm, self).__init__()
@@ -185,9 +238,12 @@ class ConvBlock(nn.Module):
         super(ConvBlock, self).__init__()
         self.resize=nn.Identity()
         self.skip = nn.Identity()
+        self.blurpool = nn.Identity()
         if scale_change == 'up':
+            self.blurpool = BlurPool(dim2, pad_type='reflect', filt_size=4, stride=1, pad_off=0)
             self.resize = nn.Upsample(scale_factor=2, mode='nearest')
         elif scale_change == 'down':
+            self.blurpool = BlurPool(dim2, pad_type='reflect', filt_size=4, stride=1, pad_off=0)
             self.resize = nn.Upsample(scale_factor=.5, mode='nearest')
         if dim2 != dim1:
             self.skip = nn.Conv2d(dim1, dim2, kernel_size=1)
@@ -196,8 +252,10 @@ class ConvBlock(nn.Module):
             nn.BatchNorm2d(dim2, momentum=.2),
             nn.LeakyReLU(),
             nn.Conv2d(dim2, dim2, kernel_size = 3,padding=1),
-            nn.BatchNorm2d(dim2, momentum=.2)
+            nn.BatchNorm2d(dim2, momentum=.2),
+            self.blurpool
             )
+        self.skip = nn.Sequential(self.skip,self.blurpool)
         self.relu = nn.LeakyReLU()
         self.apply(self._init_weights)
 
