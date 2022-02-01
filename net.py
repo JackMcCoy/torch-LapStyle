2442973a84cc3a2fc9b8cc45567e01b68cf26944
JackMcCoy <fdrdavegill@gmail.com>
2022-02-01 13:42:52 -0500

    momentumnet convmixer

--- net.py
+++ net.py
@@ -201,6 +201,7 @@ class ConvMixer(nn.Module):
             nn.BatchNorm2d(dim),
             nn.Conv2d(dim,3,kernel_size=kernel_size,padding='same'),
             nn.GELU(),
+            nn.BatchNorm2d(3),
             nn.Conv2d(3,3,kernel_size=3,padding=1)
         )
 
