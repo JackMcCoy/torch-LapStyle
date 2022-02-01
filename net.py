3dd03f2220679e952a167fd296c0f93361761eaa
JackMcCoy <fdrdavegill@gmail.com>
2022-02-01 14:06:05 -0500

    momentumnet convmixer

--- net.py
+++ net.py
@@ -198,8 +198,9 @@ class ConvMixer(nn.Module):
             nn.BatchNorm2d(dim),
             nn.ConvTranspose2d(dim, dim, kernel_size=patch_size, stride=patch_size),
             nn.GELU(),
-            nn.BatchNorm2d(dim),
             nn.Conv2d(dim,3,kernel_size=kernel_size,padding='same'),
+            nn.GELU(),
+            nn.Conv2d(3, 3, kernel_size=3, padding=1)
         )
 
     def forward(self, x):
