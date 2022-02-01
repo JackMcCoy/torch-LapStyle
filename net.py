ccbcfe2bc8269e4755198d6e689d21bc1e8584a5
JackMcCoy <fdrdavegill@gmail.com>
2022-02-01 13:16:06 -0500

    momentumnet convmixer

--- net.py
+++ net.py
@@ -194,8 +194,9 @@ class ConvMixer(nn.Module):
         self.body = momentum_net(*[cell for i in range(depth)],target_device='cuda')
         self.tail = nn.Sequential(
             nn.Conv2d(dim, dim, kernel_size=1),
-            nn.ConvTranspose2d(dim, 3, kernel_size=patch_size, stride=patch_size, groups=3),
+            nn.BatchNorm2d(dim),
             nn.GELU(),
+            nn.ConvTranspose2d(dim, 3, kernel_size=patch_size, stride=patch_size, groups=3),
             nn.Conv2d(3,3,kernel_size=3,padding=1))
 
     def forward(self, x):
