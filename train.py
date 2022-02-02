e0d63acb8b3f7850b2638166ef8f7066d2225e5c
JackMcCoy <fdrdavegill@gmail.com>
2022-02-02 00:28:17 -0500

    leaky relu on xdog

--- train.py
+++ train.py
@@ -849,7 +849,7 @@ def adaconv_thumb_train():
         loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, mdog, loss_Gp_GAN, patch_loss, style_contrastive_loss, content_contrastive_loss = losses
         fake_loss = disc2_(patch_stylized)
         loss_patch_disc = disc2_.ganloss(fake_loss, True)
-        loss = loss_patch_disc * args.gan2_loss + loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss + \
+        loss = loss_patch_disc * args.gan_loss2 + loss_c * args.content_weight + args.style_weight * loss_s + content_relt * args.content_relt + style_remd * args.style_remd + patch_loss * args.patch_loss + \
                loss_Gp_GAN * args.gan_loss + mdog * args.mdog_weight + l_identity1 * 50 + l_identity2 + l_identity3 * 50 + l_identity4 + \
                style_contrastive_loss * 0.6 + content_contrastive_loss * 0.5
 
