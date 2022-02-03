523e358637699ab7b3faf125aff924741982f623
JackMcCoy <fdrdavegill@gmail.com>
2022-02-03 00:59:55 -0500

    normalize and postprocess images

--- train.py
+++ train.py
@@ -39,13 +39,12 @@ invTrans = transforms.Compose(transforms.Normalize(
     std=[1/0.229, 1/0.224, 1/0.225]
 ))
 
-invTrans = nn.Identity()
+#invTrans = nn.Identity()
 def train_transform(load_size, crop_size):
     transform_list = [
         transforms.Resize(size=(load_size, load_size)),
         transforms.RandomCrop(crop_size),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
     return transforms.Compose(transform_list)
 
