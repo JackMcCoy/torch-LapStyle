d0f0f371f3be2c982b0f6360893fc3be66bee858
JackMcCoy <fdrdavegill@gmail.com>
2022-02-02 23:50:42 -0500

    normalize and postprocess images

--- vgg.py
+++ vgg.py
@@ -122,7 +122,7 @@ def make_layers(cfg, batch_norm: bool = False) -> nn.Sequential:
         if v == "M":
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
         else:
-            v = cast(int, v)
+            v = int(v)
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
             if batch_norm:
                 layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
