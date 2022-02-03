742fe80a44cc14c3d9338b1d05bece879115b471
JackMcCoy <fdrdavegill@gmail.com>
2022-02-03 03:08:03 -0500

    with bn

--- modules.py
+++ modules.py
@@ -234,20 +234,13 @@ class Bias(nn.Module):
         x = x+self.bias
         return x
 
-'''
 def ConvMixer(h, depth, kernel_size=9, patch_size=7):
     Seq, ActBn = nn.Sequential, lambda x: Seq(x, nn.GELU(), nn.BatchNorm2d(h))
     Residual = type('Residual', (Seq,), {'forward': lambda self, x: self[0](x) + x})
     return Seq(ActBn(nn.Conv2d(h, h, patch_size, stride=patch_size)),
     [Seq(Residual(ActBn(nn.Conv2d(h, h, kernel_size, groups=h, padding="same"))),
     ActBn(nn.Conv2d(h, h, 1))) for i in range(depth)])
-'''
-def ConvMixer(h, depth, kernel_size=9, patch_size=7):
-    Seq, ActBn = nn.Sequential, lambda x: Seq(x, nn.GELU())
-    Residual = type('Residual', (Seq,), {'forward': lambda self, x: self[0](x) + x})
-    return Seq(ActBn(nn.Conv2d(h, h, patch_size, stride=patch_size)),
-    [Seq(Residual(ActBn(nn.Conv2d(h, h, kernel_size, groups=h, padding="same"))),
-    ActBn(nn.Conv2d(h, h, 1))) for i in range(depth)])
+
 
 class ConvBlock(nn.Module):
 
@@ -268,7 +261,9 @@ class ConvBlock(nn.Module):
             self.skip = nn.Conv2d(dim1, dim2, kernel_size=1)
         self.conv_block = nn.Sequential(
             nn.Conv2d(dim1, dim2, kernel_size=3,padding=1, padding_mode=padding_mode),
+            nn.BatchNorm2d(dim2),
             nn.LeakyReLU(),
+            nn.BatchNorm2d(dim2),
             nn.Conv2d(dim2, dim2, kernel_size = 3,padding=1, padding_mode=padding_mode),
             self.blurpool
             )
