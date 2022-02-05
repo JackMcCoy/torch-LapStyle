6cc4aea93dbe8dd835cf45b90e884b07e5ab632b
JackMcCoy <fdrdavegill@gmail.com>
2022-02-05 00:01:02 -0500

    rev patch size = 8

--- train.py
+++ train.py
@@ -229,7 +229,7 @@ mdog_loss = True if args.mdog_loss==1 else 0
 
 
 def build_rev(depth, state):
-    rev = net.ConvMixer(64, 16, kernel_size=5, patch_size=16).to(device)
+    rev = net.ConvMixer(64, 16, kernel_size=5, patch_size=8).to(device)
     #if not state is None:
     #    state = torch.load(state)
     #    rev.load_state_dict(state, strict=False)
