f2d9ad98d707eab63d8b82e6b500de224c2c1764
JackMcCoy <fdrdavegill@gmail.com>
2022-02-05 16:10:36 -0500

    strotts loss

--- losses.py
+++ losses.py
@@ -16,11 +16,13 @@ def pairwise_distances_sq_l2(x, y):
     dist = x_norm + y_norm - 2.0 * torch.mm(x, y_t)
     return torch.clamp(dist, 1e-5, 1e5)/x.size(1)
 
-def distmat(x, y, cos_d=True):
-    if cos_d:
-        M = pairwise_distances_cos(x, y)
-    else:
-        M = torch.sqrt(pairwise_distances_sq_l2(x, y))
+
+def cosd_dist(x,y):
+    M = pairwise_distances_cos(x, y)
+    return M
+
+def euc_dist(x,y):
+    M = torch.sqrt(pairwise_distances_sq_l2(x, y))
     return M
 
 def rgb_to_yuv(rgb):
@@ -45,17 +47,11 @@ class CalcStyleEmdLoss():
         """
         d = X.shape[1]
 
-        if d == 3:
-            X = rgb_to_yuv(X.transpose(0, 1).contiguous().view(d, -1)).transpose(0, 1)
-            Y = rgb_to_yuv(Y.transpose(0, 1).contiguous().view(d, -1)).transpose(0, 1)
-        else:
-            X = X.transpose(0, 1).contiguous().view(d, -1).transpose(0, 1)
-            Y = Y.transpose(0, 1).contiguous().view(d, -1).transpose(0, 1)
+        X = X.transpose(0, 1).contiguous().view(d, -1).transpose(0, 1)
+        Y = Y.transpose(0, 1).contiguous().view(d, -1).transpose(0, 1)
 
         # Relaxed EMD
-        CX_M = distmat(X, Y, cos_d=True)
-
-        if d == 3: CX_M = CX_M + distmat(X, Y, cos_d=False)
+        CX_M = cosd_dist(X, Y)
 
         m1, m1_inds = CX_M.min(1)
         m2, m2_inds = CX_M.min(0)
