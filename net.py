a3cabfe9963d271fcbabda16461d593992c29981
JackMcCoy <fdrdavegill@gmail.com>
2022-02-07 00:39:58 -0500

    no seperate style weights

--- net.py
+++ net.py
@@ -207,9 +207,12 @@ class ConvMixer(nn.Module):
             nn.Conv2d(dim, dim, kernel_size=1),
             nn.LeakyReLU(),
             nn.InstanceNorm2d(dim),
-            nn.ConvTranspose2d(dim, out_dim, kernel_size=trans_kernel_size, stride=trans_kernel_size) if upscale else \
+            nn.ConvTranspose2d(dim, dim, kernel_size=trans_kernel_size, stride=trans_kernel_size),
+            nn.LeakyReLU(),
+            nn.InstanceNorm2d(dim),
             nn.Conv2d(dim, out_dim, kernel_size=kernel_size, padding='same', padding_mode='reflect'),
             nn.LeakyReLU(),
+            nn.InstanceNorm2d(out_dim),
             nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1, padding_mode='reflect', bias=final_bias)
         )
 
