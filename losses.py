354a8923fd405fd685954030e4c2489e74a92151
JackMcCoy <fdrdavegill@gmail.com>
2022-02-07 14:26:43 -0500

    transpose and calc emd distance between points

--- losses.py
+++ losses.py
@@ -90,8 +90,9 @@ def CalcContentReltLoss(X,Y, eps=1e-5):
     return d
 
 def pixel_loss(pred, target):
-    pred = rgb_to_yuv(pred.flatten(2)).transpose(1,2)[:,:,:-2]
-    target = rgb_to_yuv(target.flatten(2)).transpose(1,2)[:,:,:-2]
+    with torch.no_grad():
+        pred = rgb_to_yuv(pred.flatten(2)).transpose(1,2)[:,:,:-2]
+        target = rgb_to_yuv(target.flatten(2)).transpose(1,2)[:,:,:-2]
     remd = remd_loss(pred,target)
     return remd
 
