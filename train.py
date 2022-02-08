c4e5c42598bfd19bb789948608f26a5a87d953a5
JackMcCoy <fdrdavegill@gmail.com>
2022-02-08 15:20:34 -0500

    decrease content weight at higher resolutions

--- train.py
+++ train.py
@@ -884,7 +884,7 @@ def adaconv_thumb_train():
 
         patch_stylized = rev_(res_in)
         #disc_.eval()
-        losses = calc_losses(stylized, ci[0], si[0], cF, enc_, dec_, None, None, args.content_weight,
+        losses = calc_losses(stylized, ci[0], si[0], cF, enc_, dec_, None, args.content_weight,
                              calc_identity=args.identity_loss == 1, disc_loss=False,
                              mdog_losses=args.mdog_loss, style_contrastive_loss=args.style_contrastive_loss == 1,
                              content_contrastive_loss=args.content_contrastive_loss == 1,
