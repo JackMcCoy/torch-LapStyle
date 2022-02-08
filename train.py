d385661d9308db1e461eedccfcf76fe3727ed1dc
JackMcCoy <fdrdavegill@gmail.com>
2022-02-07 20:58:22 -0500

    jit decoder

--- train.py
+++ train.py
@@ -754,16 +754,7 @@ def adaconv_thumb_train():
     enc_ = torch.jit.trace(build_enc(vgg), (torch.rand((args.batch_size, 3, 256, 256))), strict=False)
     #dec_ = net.ThumbAdaConv(style_contrastive_loss=args.style_contrastive_loss==1,content_contrastive_loss=args.content_contrastive_loss==1,batch_size=args.batch_size,s_d=args.s_d).to(device)
 
-    dec_ = torch.jit.trace(net.ThumbAdaConv(batch_size=args.batch_size,s_d=args.s_d).to(device),
-                           ({k: v for k, v in zip(['r1_1', 'r2_1', 'r3_1', 'r4_1','r5_1'],
-                                                  [torch.rand(args.batch_size, 64, 256, 256).to(torch.device('cuda')),
-                                                   torch.rand(args.batch_size, 128, 128, 128).to(torch.device('cuda')),
-                                                   torch.rand(args.batch_size, 256, 64, 64).to(torch.device('cuda')),
-                                                   torch.rand(args.batch_size, 512, 32, 32).to(torch.device('cuda')),
-                                                   torch.rand(args.batch_size, 512, 16, 16).to(torch.device('cuda'))])},
-                            torch.rand(args.batch_size, 512, 32, 32).to(
-                                                       torch.device('cuda'))),
-                           strict=False, check_trace=False)
+    dec_ = torch.jit.script(net.ThumbAdaConv(batch_size=args.batch_size,s_d=args.s_d).to(device),strict=False)
 
     rev_ = build_rev(args.revision_depth, None)
     random_crop = transforms.RandomCrop(256)
