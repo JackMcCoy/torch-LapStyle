2536aabaa1454d00459a67b105457cf5a9e38900
JackMcCoy <fdrdavegill@gmail.com>
2022-02-11 09:47:40 -0500

    change skip placement

--- net.py
+++ net.py
@@ -641,13 +641,13 @@ class ThumbAdaConv(nn.Module):
         x = cF['r4_1']
 
         for idx, (ada, learnable, mixin) in enumerate(zip(self.adaconvs, self.learnable, self.content_injection_layer)):
-            x = self.relu(ada(style_enc, x))
-            if idx > 0:
-                x = x + self.relu(res)
-                x = self.res_norm[idx-1](x)
             if idx != len(self.learnable)-1:
                 res = self.res[idx](x)
+            x = self.relu(ada(style_enc, x))
             x = learnable(x)
+            if idx != len(self.learnable)-1:
+                x = self.relu(x+res)
+                x = self.res_norm[idx](x)
         return x
 
 
