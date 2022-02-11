ecc83c48792aee3bbade06aed8c831d5a0fcc601
JackMcCoy <fdrdavegill@gmail.com>
2022-02-10 20:07:27 -0500

    no convmixer

--- net.py
+++ net.py
@@ -614,7 +614,7 @@ class ThumbAdaConv(nn.Module):
 
         x = cF['r4_1']
         for idx, (ada, learnable, mixin) in enumerate(zip(self.adaconvs, self.learnable, self.content_injection_layer)):
-            x = x + ada(style_enc, cF[mixin])
+            x = x + ada(style_enc, x)
             x = learnable(x)
         return x
 
