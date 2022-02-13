bd1d09c7cc99d24c4d9d5855fb171d4aca90f6f0
JackMcCoy <fdrdavegill@gmail.com>
2022-02-13 12:32:51 -0500

    99 problems

--- net.py
+++ net.py
@@ -614,8 +614,7 @@ class ThumbAdaConv(nn.Module):
                 nn.Conv2d(64, 64, (3, 3)),
                 nn.ReLU(),
                 nn.ReflectionPad2d((1, 1, 1, 1)),
-                nn.Conv2d(64, 64, (3, 3)),
-                nn.ReLU()
+                nn.Conv2d(64, 3, (3, 3))
             )
         ])
         self.outfeature_shift = nn.ModuleList([
