6b3af545b76cf7005a7058df639daad2d1801cd4
JackMcCoy <fdrdavegill@gmail.com>
2022-02-22 07:00:50 -0500

    Six adaconv layers, switch to 3x3 kernel at lower levels; increase revisionnet depth, non-shuffled style remd sampling

--- adaconv.py
+++ adaconv.py
@@ -9,6 +9,7 @@ class AdaConv(nn.Module):
     def __init__(self, c_in:int, p:int, batch_size:int = 8, s_d: int = 512, norm:bool=True, c_out=None, kernel_size=5):
         super(AdaConv, self).__init__()
         self.n_groups = (c_in//p)
+        self.kernel_size = kernel_size
         self.batch_groups = batch_size * (c_in // p)
         self.pointwise_groups = s_d//p
         self.c_out = c_out if not c_out is None else c_in
@@ -47,7 +48,7 @@ class AdaConv(nn.Module):
     def forward(self, style_encoding: torch.Tensor, predicted: torch.Tensor):
         N = style_encoding.shape[0]
         depthwise = self.depthwise_kernel_conv(style_encoding)
-        depthwise = depthwise.view(N*self.c_out, self.c_in // self.n_groups, 5, 5)
+        depthwise = depthwise.view(N*self.c_out, self.c_in // self.n_groups, self.kernel_size, self.kernel_size)
         s_d = self.pointwise_avg_pool(style_encoding)
         pointwise_kn = self.pw_cn_kn(s_d).view(N*self.c_out, self.c_out // self.n_groups, 1, 1)
         pointwise_bias = self.pw_cn_bias(s_d).view(N*self.c_out)
