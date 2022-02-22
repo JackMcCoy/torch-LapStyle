f8ebcd66f76615bb70ff2c6bd78b1928d61fd4c4
JackMcCoy <fdrdavegill@gmail.com>
2022-02-21 22:43:49 -0500

    gelu nonlinearity

--- adaconv.py
+++ adaconv.py
@@ -18,16 +18,16 @@ class AdaConv(nn.Module):
         self.norm = norm
         self.depthwise_kernel_conv = nn.Sequential(
             nn.Conv2d(s_d, self.c_out * (self.c_in//self.n_groups), kernel_size=3, padding=1, padding_mode='reflect'),
-            nn.ReLU())
+            nn.GELU())
 
         self.pointwise_avg_pool = nn.Sequential(
             nn.AdaptiveAvgPool2d(1))
         self.pw_cn_kn = nn.Sequential(
             nn.Conv2d(s_d, self.c_out*(self.c_out//self.n_groups), kernel_size=1),
-            nn.ReLU())
+            nn.GELU())
         self.pw_cn_bias = nn.Sequential(
             nn.Conv2d(s_d, self.c_out, kernel_size=1),
-            nn.ReLU())
+            nn.GELU())
         self.relu=nn.LeakyReLU()
         self.apply(self._init_weights)
 
