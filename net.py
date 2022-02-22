e102f248cdef25e615647c861ecd4be0454e7316
JackMcCoy <fdrdavegill@gmail.com>
2022-02-21 23:38:01 -0500

    whiten content

--- net.py
+++ net.py
@@ -747,8 +747,7 @@ class ThumbAdaConv(nn.Module):
         whitening = []
         for i in range(x.shape[0]):
             whitening.append(whiten(x[i]).unsqueeze(0))
-        x = torch.cat(whitening,0)
-        print(x.shape)
+        x = torch.cat(whitening,0).view(x.shape[0],512,32,32)
         for idx, (ada, learnable, mixin) in enumerate(zip(self.adaconvs, self.learnable, self.content_injection_layer)):
             if idx > 0:
                 x = torch.cat([x,self.relu(ada(style_enc, x))],1)
