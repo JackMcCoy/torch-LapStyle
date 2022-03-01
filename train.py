c5dad035c4e04c7c48f9a2b3be437347f082ba88
JackMcCoy <fdrdavegill@gmail.com>
2022-03-01 12:40:04 -0500

    debugging

--- train.py
+++ train.py
@@ -953,10 +953,12 @@ def adaconv_thumb_train():
         with torch.no_grad():
             if (n + 1) % 50 == 0:
                 draft_img_grid = make_grid(invStyleTrans(stylized), nrow=4, scale_each=True)
-                styled_img_grid = make_grid(invStyleTrans(patch_stylized), nrow=4, scale_each=True)
                 style_source_grid = make_grid(si[0], nrow=4, scale_each=True)
-                content_img_grid = make_grid(ci[-1], nrow=4, scale_each=True)
-                save_image(styled_img_grid, args.save_dir + '/drafting_revision_iter' + str(n + 1) + '.jpg')
+                content_img_grid = make_grid(ci[0], nrow=4, scale_each=True)
+                for idx, patch_stylized in enumerate(stylized_patches):
+                    styled_img_grid = make_grid(invStyleTrans(patch_stylized), nrow=4, scale_each=True)
+                    version = '' if idx ==0 else str(idx)+'_'
+                    save_image(styled_img_grid, args.save_dir + '/drafting_revision_'+version+'iter' + str(n + 1) + '.jpg')
                 save_image(draft_img_grid,
                            args.save_dir + '/drafting_draft_iter' + str(n + 1) + '.jpg')
                 save_image(invTrans(content_img_grid),
