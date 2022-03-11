7872f57a159f31919ccfeb1ac82a675f44018ed3
JackMcCoy <fdrdavegill@gmail.com>
2022-03-11 12:54:52 -0500

    relu instead of leakyrelu in kernel prediction

--- adaconv.py
+++ adaconv.py
@@ -20,17 +20,17 @@ class AdaConv(nn.Module):
         self.norm = norm
         self.depthwise_kernel_conv = nn.Sequential(
             nn.Conv2d(s_d, self.c_out * (self.c_in//self.n_groups), kernel_size=3, padding=1 if kernel_size==5 else 0, padding_mode='reflect'),
-            nn.LeakyReLU())
+            nn.ReLU())
 
         self.pointwise_avg_pool = nn.Sequential(
             nn.AdaptiveAvgPool2d(1))
         self.pw_cn_kn = nn.Sequential(
             nn.Conv2d(s_d, self.c_out*(self.c_out//self.n_groups), kernel_size=1),
-            nn.LeakyReLU())
+            nn.ReLU())
         self.pw_cn_bias = nn.Sequential(
             nn.Conv2d(s_d, self.c_out, kernel_size=1),
-            nn.LeakyReLU())
-        self.relu=nn.LeakyReLU()
+            nn.ReLU())
+        self.relu=nn.ReLU()
         self.apply(self._init_weights)
 
     @staticmethod
