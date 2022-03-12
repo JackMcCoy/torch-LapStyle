4ef42f985b8a290a08607ccf09e4fabb85af2783
JackMcCoy <fdrdavegill@gmail.com>
2022-03-12 00:08:46 -0500

    remd revision

--- train.py
+++ train.py
@@ -944,8 +944,8 @@ def adaconv_thumb_train():
             loss_c, loss_s, content_relt, style_remd, l_identity1, l_identity2, l_identity3, l_identity4, \
             mdog, loss_Gp_GAN, patch_loss, style_contrastive_loss, content_contrastive_loss, pixel_loss = patch_losses
 
-            loss = loss + loss_s * args.style_weight + content_reltp * args.content_relt + \
-                   style_remdp * args.style_remd + patch_loss * args.patch_loss + \
+            loss = loss + loss_s * args.style_weight + content_relt * args.content_relt + \
+                   style_remd * args.style_remd + patch_loss * args.patch_loss + \
                    loss_Gp_GAN * args.gan_loss + mdog * args.mdog_weight + l_identity1 * 50 \
                    + l_identity2 + l_identity3 * 50 + l_identity4 + \
                    style_contrastive_loss * 0.6 + content_contrastive_loss * 0.6 + pixel_loss / args.content_relt
