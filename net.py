b2f1ee7447450d11f53c58bc1058598429924b1c
JackMcCoy <fdrdavegill@gmail.com>
2022-03-14 18:49:07 -0400

    attention

--- net.py
+++ net.py
@@ -568,7 +568,7 @@ class ResidualConvAttention(nn.Module):
         self.to_kv = nn.Conv2d(chan, key_dim * heads * 2, kernel_size, **conv_kwargs)
 
         self.to_out = nn.Conv2d(value_dim * heads, chan_out, 1)
-        self.out_norm = nn.GroupNorm(16,chan_out)
+        self.out_norm = nn.GroupNorm(16,chan_out*2)
 
     def forward(self, x, context=None):
         b, c, h, w, k_dim, heads = *x.shape, self.key_dim, self.heads
