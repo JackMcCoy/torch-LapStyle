bdf9a8e4c07fa51659b8edf14b278671375e7524
JackMcCoy <fdrdavegill@gmail.com>
2022-03-17 13:01:39 -0400

    1x1 kernel conv in ada

--- adaconv.py
+++ adaconv.py
@@ -19,16 +19,22 @@ class AdaConv(nn.Module):
         self.pad = nn.ReflectionPad2d((pad, pad, pad, pad))
         self.norm = norm
         self.depthwise_kernel_conv = nn.Sequential(
-            nn.Conv2d(s_d, self.c_out * (self.c_in//self.n_groups), kernel_size=3, padding=1 if kernel_size==5 else 0, padding_mode='reflect'),
+            nn.Conv2d(s_d,self.c_in,kernel_size=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(self.c_in, self.c_out * (self.c_in//self.n_groups), kernel_size=3, padding=1 if kernel_size==5 else 0, padding_mode='reflect'),
             nn.ReLU())
 
         self.pointwise_avg_pool = nn.Sequential(
             nn.AdaptiveAvgPool2d(1))
         self.pw_cn_kn = nn.Sequential(
-            nn.Conv2d(s_d, self.c_out*(self.c_out//self.n_groups), kernel_size=1),
+            nn.Conv2d(s_d, self.c_in, kernel_size=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(self.c_in, self.c_out*(self.c_out//self.n_groups), kernel_size=1),
             nn.ReLU())
         self.pw_cn_bias = nn.Sequential(
-            nn.Conv2d(s_d, self.c_out, kernel_size=1),
+            nn.Conv2d(s_d, self.c_in, kernel_size=1),
+            nn.LeakyReLU(),
+            nn.Conv2d(self.c_in, self.c_out, kernel_size=1),
             nn.ReLU())
         self.relu=nn.ReLU()
         self.apply(self._init_weights)
