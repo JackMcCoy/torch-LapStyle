bc98e562ce2c22fff78816b56a74ed86ec88ef47
JackMcCoy <fdrdavegill@gmail.com>
2022-03-17 07:08:31 -0400

    inject r4_1

--- net.py
+++ net.py
@@ -672,7 +672,7 @@ class ThumbAdaConv(nn.Module):
             StyleEncoderBlock(512)
         )
         self.projection = nn.Linear(8192, self.s_d*25)
-        self.content_injection_layer = ['r4_1','r4_1','r3_1','r3_1','r2_1','r2_1','r1_1']
+        self.content_injection_layer = ['r4_1',None,None,None,None,None,None]
 
         self.learnable = nn.ModuleList([
             nn.Sequential(
@@ -770,22 +770,21 @@ class ThumbAdaConv(nn.Module):
             style_enc = self.relu(style_enc).view(b,self.s_d,5,5)
         for idx, (ada, learnable, injection) in enumerate(
                 zip(self.adaconvs, self.learnable, self.content_injection_layer)):
-            if not injection is None:
+            '''if not injection is None:
                 whitening = []
                 N, C, h, w = cF[injection].shape
                 for i in range(N):
                     whitening.append(whiten(cF[injection][i]).unsqueeze(0))
                 whitening = torch.cat(whitening, 0).view(N, C, h, w)
             else:
-                whitening = x
+                whitening = x'''
             if idx > 0:
-                r = torch.rand(1,device='cuda')
-                if r[0]>.03334:
+                if injection is None:
                     x = x + self.relu(ada(style_enc, x))
                 else:
                     x = x + self.relu(ada(style_enc, cF[injection]))
             else:
-                x = self.relu(ada(style_enc, whitening))
+                x = self.relu(ada(style_enc, cF[injection]))
             x = learnable(x)
         return x, style_enc
 
