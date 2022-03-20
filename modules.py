ad141cf87ad027a1b5e87d8137ac364fd9635f7b
JackMcCoy <fdrdavegill@gmail.com>
2022-03-20 19:03:27 -0400

    reflectionpad on stylenerf

--- modules.py
+++ modules.py
@@ -487,10 +487,9 @@ class StyleNERFUpsample(nn.Module):
             nn.Conv2d(dim,dim,kernel_size=3,padding=1,padding_mode='reflect'),
             nn.LeakyReLU()
         )
+        self.pad = nn.ReflectionPad2d((2,2,2,2))
     def forward(self, x):
-        resolution = x.size()[-1]*2
-        x_pad = x.new_zeros(*x.size()[:2], x.size(-2) + 2, x.size(-1) + 2)
-        x_pad[..., 1:-1, 1:-1] = x
+        x_pad = self.pad(x)
         xl, xu, xd, xr = x_pad[..., 1:-1, :-2], x_pad[..., :-2, 1:-1], x_pad[..., 2:, 1:-1], x_pad[..., 1:-1, 2:]
         x1, x2, x3, x4 = xl + xu, xu + xr, xl + xd, xr + xd
         xb = torch.stack([x1, x2, x3, x4], 2) / 2
