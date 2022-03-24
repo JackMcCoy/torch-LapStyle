c742f65a39fd29f6aa309e30b1f52338d80c3de7
JackMcCoy <fdrdavegill@gmail.com>
2022-03-23 22:28:09 -0400

    debugging

--- adaconv.py
+++ adaconv.py
@@ -12,7 +12,7 @@ class AdaConv(nn.Module):
         self.c_in = c_in
         self.n_groups = (self.c_in // p)
         self.kernel_size = kernel_size
-        self.batch_groups = batch_size * (self.c_in // p)
+        self.batch_groups = batch_size * (self.c_out // p)
         self.pointwise_groups = s_d // p
         self.style_groups = (s_d//p)
         pad = 2 if kernel_size==5 else 1
