1ca6b86959eeb35deb5e9d2f68213631a7014765
JackMcCoy <fdrdavegill@gmail.com>
2022-03-23 22:26:12 -0400

    debugging

--- adaconv.py
+++ adaconv.py
@@ -62,7 +62,7 @@ class AdaConv(nn.Module):
         print(depthwise.shape)
         s_d = self.pointwise_avg_pool(style_encoding)
         print(s_d.shape)
-        pointwise_kn = self.pw_cn_kn(s_d).view(N*self.c_out, self.c_out // self.n_groups, 1, 1)
+        pointwise_kn = self.pw_cn_kn(s_d).view(N*self.c_out, self.c_in // self.n_groups, 1, 1)
         pointwise_bias = self.pw_cn_bias(s_d).view(N*self.c_out)
 
         a, b, c, d = predicted.size()
