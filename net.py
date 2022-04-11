8a82886bc786cb74bae908da234e6c7c1ded3590
JackMcCoy <fdrdavegill@gmail.com>
2022-04-11 14:53:55 -0400

    debugging

--- net.py
+++ net.py
@@ -789,7 +789,7 @@ class ThumbAdaConv(nn.Module):
             nn.init.constant_(m.bias.data, 0.01)
 
     def forward(self, cF: torch.Tensor, sF, calc_style=True, style_norm= None):
-        b, C, h, w = cF['r4_1'].shape[0]
+        b = cF['r4_1'].shape[0]
         if calc_style:
             style_enc = self.style_encoding(sF).flatten(1)
             style_enc = self.projection(style_enc).view(b,self.s_d,16)
