a8b29c9a90b2f6ddfc96eb6b8504277679ac1d37
JackMcCoy <fdrdavegill@gmail.com>
2022-04-16 16:23:22 -0400

    try sanet MHSA with style encoding

--- net.py
+++ net.py
@@ -604,23 +604,23 @@ class ResidualConvAttention(nn.Module):
 
 
 class MHSA(nn.Module):
-    def __init__(self, n_dims, width=16, height=16):
+    def __init__(self, n_dims, width=16, height=16, s_d=64, batch_size=):
         super(MHSA, self).__init__()
 
-        self.query = nn.Conv2d(n_dims, n_dims, kernel_size=1)
-        self.key = nn.Conv2d(n_dims, n_dims, kernel_size=1)
-        self.value = nn.Conv2d(n_dims, n_dims, kernel_size=1)
+        self.query = AdaConv(n_dims, n_dims//s_d, s_d=s_d, batch_size=batch_size, c_out=n_dims, norm=False)
+        self.key = AdaConv(n_dims, n_dims//s_d, s_d=s_d, batch_size=batch_size, c_out=n_dims, norm=False)
+        self.value = AdaConv(n_dims, n_dims//s_d, s_d=s_d, batch_size=batch_size, c_out=n_dims, norm=False)
 
         self.rel_h = nn.Parameter(torch.randn([1, n_dims, 1, height]), requires_grad=True)
         self.rel_w = nn.Parameter(torch.randn([1, n_dims, width, 1]), requires_grad=True)
 
         self.softmax = nn.Softmax(dim=-1)
 
-    def forward(self, x):  # b, c, 16,16
+    def forward(self, style, x):  # b, c, 16,16
         n_batch, C, width, height = x.size()
-        q = self.query(x).view(n_batch, C, -1)
-        k = self.key(x).view(n_batch, C, -1)
-        v = self.value(x).view(n_batch, C, -1)
+        q = self.query(style, x).view(n_batch, C, -1)
+        k = self.key(style, x).view(n_batch, C, -1)
+        v = self.value(style, x).view(n_batch, C, -1)
 
         content_content = torch.bmm(q.permute(0, 2, 1), k)
 
@@ -789,11 +789,11 @@ class ThumbAdaConv(nn.Module):
         #self.vector_quantize = VectorQuantize(dim=25, codebook_size = 512, decay = 0.8)
 
         self.attention_block = nn.ModuleList([
-            MHSA(512, width = size//8, height = size//8),
+            MHSA(512, width = size//8, height = size//8, s_d = s_d, batch_size=batch_size),
             nn.Identity(),
-            MHSA(256, width = size//4, height = size//4),
+            MHSA(256, width = size//4, height = size//4, s_d = s_d, batch_size=batch_size),
             nn.Identity(),
-            MHSA(128, width = size//2, height = size//2),
+            MHSA(128, width = size//2, height = size//2, s_d = s_d, batch_size=batch_size),
         ])
         #self.attention_conv = nn.Sequential(nn.Conv2d(512,512,kernel_size=3,padding=1,padding_mode='reflect'),
         #                                    nn.LeakyReLU())
@@ -846,9 +846,9 @@ class ThumbAdaConv(nn.Module):
                 '''
                 whitening = cF[injection]
                 if idx==0:
-                    x = checkpoint(self.attention_block[idx],whitening, preserve_rng_state=False)
+                    x = checkpoint(self.attention_block[idx],style_enc, whitening, preserve_rng_state=False)
                 else:
-                    x = checkpoint(self.attention_block[idx],whitening, preserve_rng_state=False)
+                    x = checkpoint(self.attention_block[idx],style_enc, whitening, preserve_rng_state=False)
             elif not injection is None:
                 x = self.relu(checkpoint(ada,style_enc, cF[injection], preserve_rng_state=False))
             elif type(ada) != nn.Identity:
