aad2e1d08e67b78a5a5e3701ac1aa6e07612b8f2
JackMcCoy <fdrdavegill@gmail.com>
2022-04-21 22:25:40 -0400

    no control flow in thumbadaconv

--- net.py
+++ net.py
@@ -850,7 +850,7 @@ class ThumbAdaConv(nn.Module):
         style_enc = self.style_encoding(sF).flatten(1)
         style_enc = self.projection(style_enc).view(b,self.s_d,16)
         style_enc = self.relu(style_enc).view(b,self.s_d,4,4)
-        x = checkpoint(self.attention_block[0], style_enc, cF['r4_1'], preserve_rng_state=False)
+        x = self.relu(checkpoint(self.adaconv[0], style_enc, cF['r4_1'], preserve_rng_state=False))
         x = checkpoint(self.learnable[0], x, preserve_rng_state=False)
         x = self.relu(checkpoint(self.adaconv[1], style_enc, x, preserve_rng_state=False))
         x = checkpoint(self.learnable[1], x, preserve_rng_state=False)
