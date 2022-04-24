fd6deea7c70f726ee28c207623e20f2910466f46
JackMcCoy <fdrdavegill@gmail.com>
2022-04-24 02:05:51 -0400

    batch whitening

--- function.py
+++ function.py
@@ -52,28 +52,33 @@ def demean(feature_map, dim=-1):
     return demeaned, mu
 
 def whiten(cf):
-    c_channels, c_width, c_height = cf.size(0), cf.size(1), cf.size(2)
-    cfv = cf.view(c_channels, -1)  # c x (h x w)
+    whitening = []
+    N, C, h, w = cf.shape
+    for i in range(N):
+        c_channels, c_width, c_height = C, h, w
+        cfv = cf[i].view(c_channels, -1)  # c x (h x w)
 
-    c_mean = torch.mean(cfv, 1) # perform mean for each row
-    c_mean = c_mean.unsqueeze(1).expand_as(cfv) # add dim and replicate mean on rows
-    cfv = cfv - c_mean # subtract mean element-wise
+        c_mean = torch.mean(cfv, 1) # perform mean for each row
+        c_mean = c_mean.unsqueeze(1).expand_as(cfv) # add dim and replicate mean on rows
+        cfv = cfv - c_mean # subtract mean element-wise
 
-    c_covm = torch.mm(cfv, cfv.t()).div((c_width * c_height) - 1)  # construct covariance matrix
-    c_u, c_e, c_v = torch.svd(c_covm, some=False) # singular value decomposition
+        c_covm = torch.mm(cfv, cfv.t()).div((c_width * c_height) - 1)  # construct covariance matrix
+        c_u, c_e, c_v = torch.svd(c_covm, some=False) # singular value decomposition
 
-    k_c = c_channels
+        k_c = c_channels
 
-    for i in range(c_channels):
-        if c_e[i] < 0.00001:
-            k_c = i
-            break
-    c_d = (c_e[0:k_c]).pow(-0.5)
+        for i in range(c_channels):
+            if c_e[i] < 0.00001:
+                k_c = i
+                break
+        c_d = (c_e[0:k_c]).pow(-0.5)
 
-    w_step1 = torch.mm(c_v[:, 0:k_c], torch.diag(c_d))
-    w_step2 = torch.mm(w_step1, (c_v[:, 0:k_c].t()))
-    whitened = torch.mm(w_step2, cfv)
-    return whitened
+        w_step1 = torch.mm(c_v[:, 0:k_c], torch.diag(c_d))
+        w_step2 = torch.mm(w_step1, (c_v[:, 0:k_c].t()))
+        whitened = torch.mm(w_step2, cfv)
+        whitening.append(whitened.unsqueeze(0))
+    whitening = torch.cat(whitening, 0).view(N, C, h, w)
+    return whitening
 
 def color(sF, whiten_cF):
     sFSize = sF.size()
