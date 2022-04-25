cf9667c24a700ee6f2cf998810adeded154b56db
JackMcCoy <fdrdavegill@gmail.com>
2022-04-25 05:55:41 -0400

    instance norm together

--- net.py
+++ net.py
@@ -713,9 +713,9 @@ class ThumbAdaConv(nn.Module):
         self.adaconvs = nn.ModuleList([
             AdaConv(512, 1, s_d=self.s_d, batch_size=batch_size),
             AdaConv(512, 1, s_d=self.s_d, batch_size=batch_size),
-            AdaConv(256, 2, s_d=self.s_d, batch_size=batch_size, norm=False),
             AdaConv(256, 2, s_d=self.s_d, batch_size=batch_size),
-            AdaConv(128, 4, s_d=self.s_d, batch_size=batch_size, norm=False),
+            AdaConv(256, 2, s_d=self.s_d, batch_size=batch_size),
+            AdaConv(128, 4, s_d=self.s_d, batch_size=batch_size),
             AdaConv(128, 4, s_d=self.s_d, batch_size=batch_size),
             AdaConv(64, 8, s_d=self.s_d, batch_size=batch_size),
         ])
@@ -862,12 +862,12 @@ class ThumbAdaConv(nn.Module):
         x = self.learnable[0](x)
         x = self.relu(self.adaconvs[1](style_enc, x))
         x = self.learnable[1](x)
-        whitened = whiten(cF['r3_1']) + F.instance_norm(x)
+        whitened = whiten(cF['r3_1']) + x
         x = x + self.gelu(self.adaconvs[2](style_enc, whitened))
         x = self.learnable[2](x)
         x = self.relu(self.adaconvs[3](style_enc, x))
         x = self.learnable[3](x)
-        whitened = whiten(cF['r2_1']) + F.instance_norm(x)
+        whitened = whiten(cF['r2_1']) + x
         x = x + self.gelu(self.adaconvs[4](style_enc, whitened))
         x = self.learnable[4](x)
         x = self.relu(self.adaconvs[5](style_enc, x))
