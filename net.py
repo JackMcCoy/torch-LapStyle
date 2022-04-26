fd15831d279f4bf65eca6723da559314144d9f58
JackMcCoy <fdrdavegill@gmail.com>
2022-04-25 23:25:28 -0400

    content injection on 3_1

--- net.py
+++ net.py
@@ -864,7 +864,7 @@ class ThumbAdaConv(nn.Module):
         x = self.learnable[1](x)
         x = x + res
         res = self.residual[2](x)
-        x = self.relu(self.adaconvs[2](style_enc, cF['r3_1']))
+        x = x + self.relu(self.adaconvs[2](style_enc, cF['r3_1']))
         x = self.learnable[2](x)
         x = x + res
         res = self.residual[3](x)
