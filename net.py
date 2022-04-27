ecd9e0d57f0fb7f8d3714c9261e4a28ee021eae8
JackMcCoy <fdrdavegill@gmail.com>
2022-04-27 06:37:02 -0400

    consolidation; more checkpointing

--- net.py
+++ net.py
@@ -831,10 +831,6 @@ class ThumbAdaConv(nn.Module):
                 nn.Conv2d(512, 128, kernel_size=1),
                 nn.LeakyReLU(),
                 nn.Upsample(scale_factor=4, mode='bilinear')),
-            nn.Sequential(
-                nn.Conv2d(128,64, kernel_size=1),
-                nn.LeakyReLU(),
-                nn.Upsample(scale_factor=2, mode='bilinear')),
         ])
 
         #ks = 7 if size==256 else 3
@@ -966,26 +962,27 @@ class ThumbAdaConv(nn.Module):
         x = checkpoint(self.learnable[0],x,preserve_rng_state=True)
         res = checkpoint(self.residual[1],x,preserve_rng_state=False)
         half_res = checkpoint(self.half_residual[0],x,preserve_rng_state=False)
-        full_res = checkpoint(self.full_res[0],x,preserve_rng_state=False)
+        out_res = checkpoint(self.full_res[0],x,preserve_rng_state=False)
         # quarter res
         x = self.relu(checkpoint(self.adaconvs[1],style_enc, x,preserve_rng_state=False))
         x = checkpoint(self.learnable[1],x,preserve_rng_state=True)
-        x = x + res
+        x = checkpoint(torch.add, x, res)
         # in = 256 ch
         res = x
         x = x + self.relu(checkpoint(self.attention_block[2],style_enc, cF['r3_1'], self.layer_norm[2](x),preserve_rng_state=False))
         x = x + checkpoint(self.learnable[2],x,preserve_rng_state=False)
-        x = x + res
+        x = checkpoint(torch.add,x, res)
         #####
-        out_res = full_res + checkpoint(self.full_res[1],x,preserve_rng_state=False)
+        out_res = checkpoint(torch.add,out_res, checkpoint(self.full_res[1],x,preserve_rng_state=False))
         res = x
         x = checkpoint(self.learnable[3], x, preserve_rng_state=True)
-        x = x + res
+        x = checkpoint(torch.add,x,res)
         res = checkpoint(self.residual[4], x, preserve_rng_state=False)
         x = self.relu(checkpoint(self.adaconvs[4], style_enc, x, preserve_rng_state=False))
         x = checkpoint(self.learnable[4],x,preserve_rng_state=True)
-        x = x + res + half_res
-        half_res = checkpoint(self.half_residual[1], x, preserve_rng_state=False)
+        x = checkpoint(torch.add,x, res)
+        x = checkpoint(torch.add,x,half_res)
+        half_res = checkpoint(self.self.residual[6], x, preserve_rng_state=False)
         #####
         x = x + self.relu(checkpoint(self.attention_block[5],style_enc, cF['r2_1'], self.layer_norm[5](x),preserve_rng_state=False))
         x = x + checkpoint(self.learnable[5],x,preserve_rng_state=False)
@@ -994,14 +991,15 @@ class ThumbAdaConv(nn.Module):
         res = checkpoint(self.residual[6],x,preserve_rng_state=False)
         x = self.relu(checkpoint(self.adaconvs[6],style_enc, x,preserve_rng_state=False))
         x = checkpoint(self.learnable[6],x,preserve_rng_state=True)
-        x = x + res
+        x = checkpoint(torch.add,x,res)
         ######
         # in = 64 ch
         res = x
         x = self.relu(checkpoint(self.adaconvs[7], style_enc, x, preserve_rng_state=False))
         x = checkpoint(self.learnable[7], x, preserve_rng_state=True)
-        out_res = out_res + x
-        x = x + res + half_res + full_res + out_res
+        x = checkpoint(torch.add,x,res)
+        x = checkpoint(torch.add,x,half_res)
+        x = checkpoint(torch.add,x,out_res)
         x = x + self.relu(checkpoint(self.out_deform,self.layer_norm[8](x),preserve_rng_state=False))
         x = x + checkpoint(self.learnable[8],x,preserve_rng_state=False)
         x = checkpoint(self.learnable[9], x, preserve_rng_state=False)
