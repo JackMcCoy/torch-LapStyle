51823fea8de70c6a7286c43ad70dd4a2233df875
JackMcCoy <fdrdavegill@gmail.com>
2022-04-28 00:23:12 -0400

    in_transform

--- net.py
+++ net.py
@@ -961,7 +961,7 @@ class ThumbAdaConv(nn.Module):
         style_enc = self.style_encoding(sF).flatten(1)
         style_enc = self.projection(style_enc).view(b,self.s_d,16)
         style_enc = self.relu(style_enc).view(b,self.s_d,4,4)
-        x = self.in_transform(cF['r4_1'])
+        x = self.in_deform(cF['r4_1'])
         x = checkpoint(self.adaconvs[0],style_enc, x,preserve_rng_state=False)
         x = checkpoint(self.learnable[0],x,preserve_rng_state=True)
         res = checkpoint(self.residual[1],x,preserve_rng_state=False)
