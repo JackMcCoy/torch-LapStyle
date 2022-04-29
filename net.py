a030b677b250a96c45b60ee273247f62fbea6594
JackMcCoy <fdrdavegill@gmail.com>
2022-04-29 12:08:40 -0400

    no in_deform

--- net.py
+++ net.py
@@ -928,7 +928,12 @@ class ThumbAdaConv(nn.Module):
             nn.GroupNorm(8, 64)
         ])
         #self.in_deform = DeformableAttention2D(512, heads=8, downsample_factor=4, offset_kernel_size=6)
-        self.in_deform = DeformableAttention2D(512)
+        self.in_deform = nn.Sequential(
+            nn.Conv2d(512,128,kernel_size=3,padding=1,padding_mode='reflect'),
+            nn.LeakyReLU(),
+            nn.Conv2d(128, 512, kernel_size=3, padding=1, padding_mode='reflect'),
+            nn.LeakyReLU(),
+        )
         self.out_deform = DeformableAttention2D(64, heads=2, downsample_factor=16, offset_kernel_size=32)
 
         #self.attention_conv = nn.Sequential(nn.Conv2d(512,512,kernel_size=3,padding=1,padding_mode='reflect'),
