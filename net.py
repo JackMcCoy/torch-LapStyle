d0839d3ca7ecc3de65e570b28246634c45bc055e
JackMcCoy <fdrdavegill@gmail.com>
2022-05-25 16:39:08 -0400

    no residual after learnable[8]

--- net.py
+++ net.py
@@ -891,7 +891,7 @@ class ThumbAdaConv(nn.Module):
                 GaussianNoise(),
                 FusedLeakyReLU(64),),
             nn.Sequential(
-                nn.GroupNorm(8, 64),
+                nn.GroupNorm(8, 128),
                 nn.ReflectionPad2d((1, 1, 1, 1)),
                 nn.Conv2d(128, 64, (3, 3), bias=False),
                 GaussianNoise(),
