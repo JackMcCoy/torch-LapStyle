95f6ea9dd01e545982016ba88cdb5fe2bda9a8a6
JackMcCoy <fdrdavegill@gmail.com>
2022-06-13 00:04:42 -0400

    normal distribution for initial x

--- net.py
+++ net.py
@@ -1000,7 +1000,7 @@ class ThumbAdaConv(nn.Module):
         mean = self.style_mean(style_enc)
         std = self.style_std(style_enc)
         style_enc = self.relu(style_enc.view(b,self.s_d,16)).view(b,self.s_d,4,4)
-        x = torch.normal(mean, std)
+        x = torch.normal(mean, std, size=(b,512,16,16))
         x = x + checkpoint(self.attention_block[0],style_enc, x, cF['r4_1'],preserve_rng_state=False)
         x = self.layer_norm_out[0](x)
         #x = self.gelu(x)
