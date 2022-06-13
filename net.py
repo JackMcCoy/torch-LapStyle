5f732a0842c3bee4c108e28ae4fef6130c9f33a7
JackMcCoy <fdrdavegill@gmail.com>
2022-06-13 03:31:21 -0400

    no instance_norm, res

--- net.py
+++ net.py
@@ -995,13 +995,13 @@ class ThumbAdaConv(nn.Module):
         x = x + res
         # in = 256 ch
         x = self.layer_norm_in[2](x)
-        #res = x
+        res = x
         #whitened = self.in_projection[1](cF['r3_1'])
         #whitened = checkpoint(self.in_deform[1], whitened,preserve_rng_state=False)
         x = x + checkpoint(self.attention_block[2], style_enc, x, cF['r3_1'], preserve_rng_state=False)
         x = self.layer_norm_out[2](x)
         x = checkpoint(self.learnable[2], x, preserve_rng_state=False)
-        #x = x + res
+        x = x + res
         #####
         res = x
         x = checkpoint(self.learnable[3], x, preserve_rng_state=False)
@@ -1011,11 +1011,11 @@ class ThumbAdaConv(nn.Module):
         x = x + res
         #####
         x = self.layer_norm_in[5](x)
-        #res = x
+        res = x
         x = x + checkpoint(self.attention_block[5], style_enc, x, cF['r2_1'], preserve_rng_state=False)
         x = self.layer_norm_out[5](x)
         x = checkpoint(self.learnable[5], x, preserve_rng_state=False)
-        #x = x + res
+        x = x + res
 
         # in = 128 ch
         res = checkpoint(self.residual[6],x,preserve_rng_state=False)
