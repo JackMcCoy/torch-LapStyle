2af642dc583e0d1079bb2e305013ce64e8889803
JackMcCoy <fdrdavegill@gmail.com>
2022-06-13 15:03:38 -0400

    grouped depthwise

--- net.py
+++ net.py
@@ -610,7 +610,9 @@ class ResidualConvAttention(nn.Module):
 class AdaConv_w_FF(nn.Module):
     def __init__(self, in_dims, out_dims, s_d, batch_size, norm=False, kernel_relu=True):
         super(AdaConv_w_FF, self).__init__()
-        p = in_dims
+        #p = in_dims
+        p = n_dims//s_d
+        print(p)
         self.ada = AdaConv(in_dims, p, s_d=s_d, batch_size=batch_size, c_out=out_dims, norm=norm, kernel_relu=kernel_relu)
         #self.conv = nn.Conv2d(n_dims, n_dims, kernel_size = 1, padding='same', padding_mode='reflect')
     def forward(self, style, x):
