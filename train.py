4c8c11ffb82a68c8d6a7fff97523e18e84aa0b46
JackMcCoy <fdrdavegill@gmail.com>
2022-06-17 13:14:39 -0400

    200000 blurred disc images

--- train.py
+++ train.py
@@ -360,7 +360,7 @@ def drafting_train():
     lowest_range = 32
     loss_D = 0
     blurpool = BlurPool(3, filt_size=5, stride=1).to(device='cuda').eval()
-    blur_iters = 100000 // args.batch_size
+    blur_iters = 200000 // args.batch_size
     print(str(blur_iters)+' blur iters')
     for n in tqdm(range(args.max_iter), position=0):
         warmup_lr_adjust(dec_optimizer, n, warmup_start=args.warmup_start, warmup_iters=args.warmup_iters, max_lr=args.lr,
