16821b2a2377ae7b7335be8013041a0ec5f300e3
JackMcCoy <fdrdavegill@gmail.com>
2022-07-01 14:57:43 -0400

    codebookdim = 16

--- net.py
+++ net.py
@@ -1047,7 +1047,8 @@ class ThumbAdaConv(nn.Module):
         b = cF['r4_1'].shape[0]
         style_enc = self.style_encoding(sF).flatten(1)
         style_enc = self.projection(style_enc).view(b, self.s_d, self.kernel_size ** 2)
-        style_enc, _, cb_loss = self.channelwise_quantize(style_enc).view(b, self.s_d, self.kernel_size, self.kernel_size)
+        style_enc, _, cb_loss = self.channelwise_quantize(style_enc)
+        style_enc = style_enc.view(b, self.s_d, self.kernel_size, self.kernel_size)
         style_enc, _, cb = self.vector_quantize(style_enc)
         cb_loss = cb_loss + cb
         x = checkpoint(self.attention_block[0], style_enc, cF['r4_1'], preserve_rng_state=False)
