97a1d79fe4f441b22ca339d203dbe4ff08c8ccad
JackMcCoy <fdrdavegill@gmail.com>
2022-07-02 16:07:40 -0400

    channelwise attention, more heads, no remd

--- net.py
+++ net.py
@@ -1501,17 +1501,17 @@ def loss_no_patch(stylized: torch.Tensor,
                 crop_size=128,
                 blur = False):
 
-    #l_identity1 = 0
-    #l_identity2 = 0
+    l_identity1 = 0
+    l_identity2 = 0
 
-    #style_remd = 0
-    #content_relt = 0
-    #l_identity3 = 0
-    #l_identity4 = 0
-    #cb_loss = 0
-    l_identity1, l_identity2, cb_loss = identity_loss(ci, cF, encoder, decoder)
-    l_identity3, l_identity4, cb = identity_loss(si, sF, encoder, decoder)
-    cb_loss = cb_loss + cb
+    style_remd = 0
+    content_relt = 0
+    l_identity3 = 0
+    l_identity4 = 0
+    cb_loss = 0
+    #l_identity1, l_identity2, cb_loss = identity_loss(ci, cF, encoder, decoder)
+    #l_identity3, l_identity4, cb = identity_loss(si, sF, encoder, decoder)
+    #cb_loss = cb_loss + cb
     stylized_feats = encoder(stylized)
     #loss_c = content_loss.no_norm(stylized_feats['r5_1'], cF['r5_1'].detach())
     loss_c = content_loss.no_norm(stylized_feats['r4_1'], cF['r4_1'].detach())
@@ -1523,12 +1523,12 @@ def loss_no_patch(stylized: torch.Tensor,
     loss_s = loss_s + style_loss(stylized_feats['r3_1'], sF['r3_1'].detach())
     loss_s = loss_s + style_loss(stylized_feats['r4_1'], sF['r4_1'].detach())
     #loss_s = loss_s + style_loss(stylized_feats['r5_1'], sF['r5_1'].detach())
-    style_remd = CalcStyleEmdNoSample(stylized_feats['r4_1'], sF['r4_1']) + \
-                 CalcStyleEmdNoSample(stylized_feats['r3_1'], sF['r3_1'])
-    content_relt = CalcContentReltNoSample(stylized_feats['r4_1'], cF['r4_1'].detach()) + \
-                   CalcContentReltNoSample(stylized_feats['r3_1'], cF['r3_1'].detach())
-    # p_loss = 0
-    p_loss = pixel_loss(stylized, si)
+    #style_remd = CalcStyleEmdNoSample(stylized_feats['r4_1'], sF['r4_1']) + \
+    #             CalcStyleEmdNoSample(stylized_feats['r3_1'], sF['r3_1'])
+    #content_relt = CalcContentReltNoSample(stylized_feats['r4_1'], cF['r4_1'].detach()) + \
+    #               CalcContentReltNoSample(stylized_feats['r3_1'], cF['r3_1'].detach())
+    p_loss = 0
+    #p_loss = pixel_loss(stylized, si)
     if disc_:
         if blur:
             fake = blur(stylized)
