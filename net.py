0f7817dcb739750f9ea4268c65756f73c9f124bb
JackMcCoy <fdrdavegill@gmail.com>
2022-07-04 17:33:28 -0400

    debugging

--- net.py
+++ net.py
@@ -1010,28 +1010,29 @@ class ThumbAdaConv(nn.Module):
         x = checkpoint(self.learnable[1], x, preserve_rng_state=False)
         # in = 256 ch
         # x = self.layer_norm_in[2](x)
-        x = checkpoint(self.layer_norm[0], x, preserve_rng_state=False)
+        x = checkpoint(self.layer_norm[1], x, preserve_rng_state=False)
         x = x + checkpoint(self.attention_block[2], style_enc, x, preserve_rng_state=False)
         x = x + checkpoint(self.learnable[2], x, preserve_rng_state=False)
         #####
         x = checkpoint(self.learnable[3], x, preserve_rng_state=False)
         x = checkpoint(self.learnable[4], x, preserve_rng_state=False)
         #####
-        x = checkpoint(self.layer_norm[1], x, preserve_rng_state=False)
+        x = checkpoint(self.layer_norm[2], x, preserve_rng_state=False)
         x = x + checkpoint(self.attention_block[5], style_enc, x, preserve_rng_state=False)
         x = x + checkpoint(self.learnable[5], x, preserve_rng_state=False)
         # in = 128 ch
         x = checkpoint(self.learnable[6], x, preserve_rng_state=False)
         ######
         # in = 64 ch
-        x = checkpoint(self.layer_norm[2], x, preserve_rng_state=False)
+        x = checkpoint(self.layer_norm[3], x, preserve_rng_state=False)
         x = x + checkpoint(self.attention_block[7], style_enc, x, preserve_rng_state=False)
         x = x + checkpoint(self.learnable[7], x, preserve_rng_state=False)
         x = checkpoint(self.learnable[8], x, preserve_rng_state=False)
-        x = checkpoint(self.layer_norm[3], x, preserve_rng_state=False)
+        x = checkpoint(self.layer_norm[4], x, preserve_rng_state=False)
         x = x + checkpoint(self.attention_block[8], style_enc, x, preserve_rng_state=False)
         x = x + checkpoint(self.learnable[9], x, preserve_rng_state=False)
         x = checkpoint(self.learnable[10], x, preserve_rng_state=False)
+        print('out')
         return x, cb_loss
 
 
