7dea7a51846c824165818556c98e9b9800664743
JackMcCoy <fdrdavegill@gmail.com>
2022-07-08 14:41:11 -0400

    relu after projection

--- net.py
+++ net.py
@@ -984,7 +984,7 @@ class ThumbAdaConv(nn.Module):
     def forward(self, cF: torch.Tensor, sF, calc_style=True, style_norm= None):
         b = cF['r4_1'].shape[0]
         style_enc = self.style_encoding(sF).flatten(1)
-        style_enc = self.projection(style_enc).view(b, self.s_d, self.kernel_size, self.kernel_size)
+        style_enc = F.relu(self.projection(style_enc)).view(b, self.s_d, self.kernel_size, self.kernel_size)
         #style_enc, _, cb_loss = self.channelwise_quantize(style_enc)
         cb_loss = 0
         #style_enc = style_enc.view(b, self.s_d, self.kernel_size, self.kernel_size)
