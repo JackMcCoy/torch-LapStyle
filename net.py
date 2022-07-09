d401bcbed85dfc7da08d6418e08ad2ff3d6c2b65
JackMcCoy <fdrdavegill@gmail.com>
2022-07-09 16:09:16 -0400

    quantize

--- net.py
+++ net.py
@@ -933,7 +933,7 @@ class ThumbAdaConv(nn.Module):
             )
         ])
 
-        #self.channelwise_quantize = VectorQuantize(dim=self.kernel_size ** 2, codebook_size=1200, decay=0.8)
+        self.channelwise_quantize = VectorQuantize(dim=self.kernel_size ** 2, codebook_size=1200, decay=0.8)
         self.attention_block = nn.ModuleList([
             StyleAttention(512, s_d=s_d, batch_size=batch_size, heads=16, size=int(size / 2 ** 3), kernel_size = self.kernel_size, adaconv_norm=False),
             #AdaConv(512, 1, s_d=self.s_d, batch_size=batch_size, norm=True, kernel_size = self.kernel_size),
@@ -984,10 +984,10 @@ class ThumbAdaConv(nn.Module):
     def forward(self, cF: torch.Tensor, sF, calc_style=True, style_norm= None):
         b = cF['r4_1'].shape[0]
         style_enc = self.style_encoding(sF).flatten(1)
-        style_enc = self.projection(style_enc).view(b, self.s_d, self.kernel_size, self.kernel_size)
-        #style_enc, _, cb_loss = self.channelwise_quantize(style_enc)
-        cb_loss = 0
-        #style_enc = style_enc.view(b, self.s_d, self.kernel_size, self.kernel_size)
+        style_enc = self.projection(style_enc).view(b, self.s_d, self.kernel_size**2)
+        style_enc, _, cb_loss = self.channelwise_quantize(style_enc)
+        #cb_loss = 0
+        style_enc = style_enc.view(b, self.s_d, self.kernel_size, self.kernel_size)
         x = checkpoint(self.attention_block[0], style_enc, cF['r4_1'], preserve_rng_state=False)
         x = x + checkpoint(self.learnable[0], x, preserve_rng_state=False)
         # quarter res
