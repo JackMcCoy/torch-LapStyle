db8fd0d18904fdf14db7d1df94702de82c098653
JackMcCoy <fdrdavegill@gmail.com>
2022-07-13 02:59:53 -0400

    norm

--- net.py
+++ net.py
@@ -967,18 +967,18 @@ class ThumbAdaConv(nn.Module):
             #StyleAttention(512, s_d=s_d, batch_size=batch_size, heads=12, size=int(size / 2 ** 3), kernel_size = self.kernel_size, adaconv_norm=False),
             AdaConv(512, 1, s_d=self.s_d, batch_size=batch_size, norm=True, kernel_size = self.kernel_size),
             nn.Identity(),
-            AdaConv(256, 2, s_d=self.s_d, batch_size=batch_size, norm=False, kernel_size = self.kernel_size),
+            AdaConv(256, 2, s_d=self.s_d, batch_size=batch_size, norm=True, kernel_size = self.kernel_size),
             #StyleAttention(256, s_d=s_d, batch_size=batch_size, heads=8, size=int(size / 2 ** 2),
             #               kernel_size=self.kernel_size, adaconv_norm=False),
 
             nn.Identity(),
             nn.Identity(),
-            AdaConv(128, 4, s_d=self.s_d, batch_size=batch_size, norm=False, kernel_size = self.kernel_size),
+            AdaConv(128, 4, s_d=self.s_d, batch_size=batch_size, norm=True, kernel_size = self.kernel_size),
             #StyleAttention(128, s_d=s_d, batch_size=batch_size, heads=4, size=int(size / 2 ** 1),
             #               kernel_size=self.kernel_size, adaconv_norm=False),
 
             nn.Identity(),
-            AdaConv(64, 8, s_d=self.s_d, batch_size=batch_size, norm=False, kernel_size = self.kernel_size),
+            AdaConv(64, 8, s_d=self.s_d, batch_size=batch_size, norm=True, kernel_size = self.kernel_size),
             #StyleAttention(64, s_d=s_d, batch_size=batch_size, heads=2, size=size,
             #               kernel_size=self.kernel_size, adaconv_norm=False),
             AdaConv(64, 8, s_d=self.s_d, batch_size=batch_size, norm=True, kernel_size = self.kernel_size)
